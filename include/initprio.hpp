ee08895cfefc0fb1cf6aa8598139cab1d0a40586
Udo Steinberg <udo@hypervisor.org>
2019-02-11 13:16:02 +0100

    BUILD: Fixed linker errors.

--- include/initprio.hpp
+++ include/initprio.hpp
@@ -4,7 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
- * Copyright (C) 2012 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2022 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -20,10 +21,11 @@
 
 #pragma once
 
-#define AFTER(X)        (X + 1)
+// Lower numbers indicate a higher priority
+#define AFTER(X)        ((X) + 1)
 
-#define PRIO_GLOBAL     100
-#define PRIO_BUDDY      AFTER (PRIO_GLOBAL)
+#define PRIO_LIMIT      100
+#define PRIO_BUDDY      AFTER (PRIO_LIMIT)
 #define PRIO_SLAB       AFTER (PRIO_BUDDY)
-#define PRIO_CONSOLE    0xfffd
-#define PRIO_LOCAL      0xfffe
+#define PRIO_CONSOLE    65533
+#define PRIO_LOCAL      65534
