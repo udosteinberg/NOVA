0c688a3a9d9f97b3c37b29b74e7b6fafa6342541
Udo Steinberg <udo@hypervisor.org>
2019-02-12 11:32:12 +0100

    ARM: Call C++ code.

---
+++ src/aarch64/init.cpp
@@ -0,0 +1,31 @@
+/*
+ * Initialization Code
+ *
+ * Copyright (C) 2019-2021 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "arch.hpp"
+#include "console.hpp"
+#include "extern.hpp"
+
+extern "C"
+void init()
+{
+    for (void (**func)() = &CTORS_S; func != &CTORS_E; (*func++)()) ;
+
+    for (void (**func)() = &CTORS_C; func != &CTORS_S; (*func++)()) ;
+
+    // Now we're ready to talk to the world
+    Console::print ("\nNOVA Microhypervisor #%07lx (%s): %s %s [%s]\n", reinterpret_cast<uintptr_t>(&GIT_VER), ARCH, __DATE__, __TIME__, COMPILER_STRING);
+}
