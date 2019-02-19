07048e5fba1d7645225df71649ed39f197bc959d
Udo Steinberg <udo@hypervisor.org>
2019-02-19 15:30:49 +0100

    ARM: Link to high offset and set up page tables accordingly.

--- inc/aarch64/memory.hpp
+++ inc/aarch64/memory.hpp
@@ -36,6 +36,30 @@
 #define PAGE_SIZE(L)    BITN (LEVL_BITS (L))
 #define OFFS_MASK(L)    (PAGE_SIZE (L) - 1)
 
-#define LINK_ADDR       LOAD_ADDR
-#define MMAP_CPU_DATA   0
+#define VIRT_ADDR(L3,L2,L1,L0)  (VALN_SHIFT (L3, LEVL_BITS (3)) | VALN_SHIFT (L2, LEVL_BITS (2)) | VALN_SHIFT (L1, LEVL_BITS (1)) | VALN_SHIFT (L0, LEVL_BITS (0)))
+
+// Global Area
+#define MMAP_GLB_CPUS   VIRT_ADDR (511, 509,   0,   0)  //   1G (262144 CPUs)
+#define MMAP_GLB_PCIE   MMAP_GLB_CPUS
+#define MMAP_GLB_PCIS   VIRT_ADDR (511, 253,   0,   0)  // 256G (1024 PCI Segment Groups)
+#define MMAP_TMP_RW1E   MMAP_GLB_PCIS
+#define MMAP_TMP_RW1S   VIRT_ADDR (511, 252,   0,   0)  //   1G (Remap Window 1)
+#define MMAP_TMP_RW0E   MMAP_TMP_RW1S
+#define MMAP_TMP_RW0S   VIRT_ADDR (511, 251,   0,   0)  //   1G (Remap Window 0)
+
+#define MMAP_GLB_MAP1   VIRT_ADDR (511,   0, 500,   0)  //   4M + gap
+#define MMAP_GLB_MAP0   VIRT_ADDR (511,   0, 496,   0)  //   4M + gap
+#define MMAP_GLB_GICD   VIRT_ADDR (511,   0, 488,  32)  //  64K
+#define MMAP_GLB_GICC   VIRT_ADDR (511,   0, 488,  16)  //  64K
+#define MMAP_GLB_GICH   VIRT_ADDR (511,   0, 488,   0)  //  64K
+#define MMAP_GLB_UART   VIRT_ADDR (511,   0, 480,   0)  //  16M
+#define MMAP_GLB_SMMU   VIRT_ADDR (511,   0, 448,   0)  //  64M
+#define LINK_ADDR       VIRT_ADDR (511,   0,   0,   0)  // 896M
+
+// CPU-Local Area
+#define MMAP_CPU_DATA   VIRT_ADDR (510, 511, 511, 511)  //   4K
+#define MMAP_CPU_DSTT   VIRT_ADDR (510, 511, 511, 510)  // Data Stack Top
+#define MMAP_CPU_DSTB   VIRT_ADDR (510, 511, 511, 509)  // Data Stack Base
+#define MMAP_CPU_GICR   VIRT_ADDR (510, 511, 511,   0)  // 256K
+
 #define OFFSET          (LINK_ADDR - LOAD_ADDR)
