1df33d029122045860159200787af67d33b9dea5
Udo Steinberg <udo@hypervisor.org>
2019-02-19 15:30:49 +0100

    ARM: Link to high offset and set up page tables accordingly.

--- inc/aarch64/memory.hpp
+++ inc/aarch64/memory.hpp
@@ -34,6 +34,14 @@
 #define PAGE_SIZE       BITN (PAGE_BITS)
 #define OFFS_MASK       (PAGE_SIZE - 1)
 
-#define LINK_ADDR       LOAD_ADDR
-#define MMAP_CPU_DATA   0
+// Global Area                [--PTE--]---      // ^39 ^30 ^21 ^12
+#define MMAP_GLB_UART   0x0000ffffffe00000      // 511 511 511 000   4K
+#define MMAP_GLB_DATA   0x0000ffffc0000000      // 511 511 000 000 256M
+#define LINK_ADDR       0x0000ff8000000000      // 511 000 000 000
+
+// CPU-Local Area             [--PTE--]---      // ^39 ^30 ^21 ^12
+#define MMAP_CPU_DATA   0x0000ff7ffffff000      // 510 511 511 511   4K
+#define MMAP_CPU_DSTK   0x0000ff7fffffd000      // 510 511 511 509   4K + gap
+#define MMAP_CPU_TMAP   0x0000ff7fffa00000      // 510 511 509 000   4M
+
 #define OFFSET          (LINK_ADDR - LOAD_ADDR)
