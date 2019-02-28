0cefa5124d348ded76ee3e49951146990f3f4aab
Udo Steinberg <udo@hypervisor.org>
2019-03-01 00:50:53 +0100

    ARM: Adapt compiler.hpp.

--- inc/compiler.hpp
+++ inc/compiler.hpp
@@ -26,59 +26,59 @@
 
 #if defined(__GNUC__)
 
-        #define COMPILER            "gcc " __VERSION__
+        #define COMPILER                "gcc " __VERSION__
 
     #if defined(__GNUC_PATCHLEVEL__)
-        #define COMPILER_STRING     "gcc " EXPAND (__GNUC__) "." EXPAND (__GNUC_MINOR__) "." EXPAND (__GNUC_PATCHLEVEL__)
-        #define COMPILER_VERSION    (__GNUC__ * 100 + __GNUC_MINOR__ * 10 + __GNUC_PATCHLEVEL__)
+        #define COMPILER_STRING         "gcc " EXPAND (__GNUC__) "." EXPAND (__GNUC_MINOR__) "." EXPAND (__GNUC_PATCHLEVEL__)
+        #define COMPILER_VERSION        (__GNUC__ * 100 + __GNUC_MINOR__ * 10 + __GNUC_PATCHLEVEL__)
     #else
-        #define COMPILER_STRING     "gcc " EXPAND (__GNUC__) "." EXPAND (__GNUC_MINOR__)
-        #define COMPILER_VERSION    (__GNUC__ * 100 + __GNUC_MINOR__ * 10)
+        #define COMPILER_STRING         "gcc " EXPAND (__GNUC__) "." EXPAND (__GNUC_MINOR__)
+        #define COMPILER_VERSION        (__GNUC__ * 100 + __GNUC_MINOR__ * 10)
     #endif
 
-    #if (COMPILER_VERSION < 430)
-        #define COLD
-        #define HOT
+    #ifndef __has_cpp_attribute
+        #define __has_cpp_attribute(X)  0
+    #endif
+
+    #if __has_cpp_attribute(fallthrough)
+        #define FALLTHROUGH             [[fallthrough]]
     #else
-        #define COLD                __attribute__((cold))
-        #define HOT                 __attribute__((hot))
-    #endif
-
-    #if (COMPILER_VERSION < 450)
-        #define UNREACHED           __builtin_trap()
-    #else
-        #define UNREACHED           __builtin_unreachable()
-    #endif
-
-    #if (COMPILER_VERSION < 460) || !defined(__GXX_EXPERIMENTAL_CXX0X__)
-        #define nullptr             0
-    #endif
-
-    #if (COMPILER_VERSION < 700)
         #define FALLTHROUGH
-    #else
-        #define FALLTHROUGH         __attribute__((fallthrough))
     #endif
 
-        #define ALIGNED(X)          __attribute__((aligned(X)))
-        #define ALWAYS_INLINE       __attribute__((always_inline))
-        #define CPULOCAL            __attribute__((section (".cpulocal,\"w\",@nobits#")))
-        #define CPULOCAL_HOT        __attribute__((section (".cpulocal.hot,\"w\",@nobits#")))
-        #define FORMAT(X,Y)         __attribute__((format (printf, (X),(Y))))
-        #define INIT_PRIORITY(X)    __attribute__((init_priority((X))))
-        #define NOINLINE            __attribute__((noinline))
-        #define NONNULL             __attribute__((nonnull))
-        #define NORETURN            __attribute__((noreturn))
-        #define PACKED              __attribute__((packed))
-        #define REGPARM(X)          __attribute__((regparm(X)))
-        #define WARN_UNUSED_RESULT  __attribute__((warn_unused_result))
+    #if __has_cpp_attribute(noreturn)
+        #define NORETURN                [[noreturn]]
+    #else
+        #define NORETURN                __attribute__((noreturn))
+    #endif
 
-        #define EXPECT_FALSE(X)     __builtin_expect(!!(X), 0)
-        #define EXPECT_TRUE(X)      __builtin_expect(!!(X), 1)
+        #define COLD                    __attribute__((cold))
+        #define HOT                     __attribute__((hot))
 
-        #define ACCESS_ONCE(x)      (*static_cast<volatile typeof(x) *>(&(x)))
+        #define ALIGNED(X)              __attribute__((aligned(X)))
+        #define ALWAYS_INLINE           __attribute__((always_inline))
+    #if defined(__x86_64__) || defined(__i386__)
+        #define CPULOCAL                __attribute__((section (".cpulocal,\"w\",@nobits#")))
+        #define CPULOCAL_HOT            __attribute__((section (".cpulocal.hot,\"w\",@nobits#")))
+    #elif defined (__aarch64__)
+        #define CPULOCAL                __attribute__((section (".cpulocal,\"w\",@nobits//")))
+        #define CPULOCAL_HOT            __attribute__((section (".cpulocal.hot,\"w\",@nobits//")))
+    #endif
+        #define FORMAT(X,Y)             __attribute__((format (printf, (X),(Y))))
+        #define INIT_PRIORITY(X)        __attribute__((init_priority((X))))
+        #define NOINLINE                __attribute__((noinline))
+        #define NONNULL                 __attribute__((nonnull))
+        #define PACKED                  __attribute__((packed))
+        #define REGPARM(X)              __attribute__((regparm(X)))
+        #define WARN_UNUSED_RESULT      __attribute__((warn_unused_result))
+
+        #define EXPECT_FALSE(X)         __builtin_expect(!!(X), 0)
+        #define EXPECT_TRUE(X)          __builtin_expect(!!(X), 1)
+        #define UNREACHED               __builtin_unreachable()
+
+        #define ACCESS_ONCE(x)          (*static_cast<volatile decltype(x) *>(&(x)))
 
 #else
-        #define COMPILER            "unknown compiler"
-        #define COMPILER_VERSION    0
+        #define COMPILER                "unknown compiler"
+        #define COMPILER_VERSION        0
 #endif
