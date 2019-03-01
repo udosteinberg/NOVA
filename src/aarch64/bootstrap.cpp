cbb64398b525c6d4eefca05ad72a3fce13cab52f
Udo Steinberg <udo@hypervisor.org>
2019-03-01 01:06:58 +0100

    ARM: Activate CPU-local infrastructure.

---
+++ src/aarch64/bootstrap.cpp
@@ -0,0 +1,27 @@
+/*
+ * Bootstrap Code
+ *
+ * Copyright (C) 2019-2022 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "compiler.hpp"
+#include "cpu.hpp"
+
+extern "C" [[noreturn]]
+void bootstrap()
+{
+    Cpu::init();
+
+    for (;;) {}
+}
