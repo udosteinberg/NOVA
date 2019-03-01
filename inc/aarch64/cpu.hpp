dc5c98c7360d46fbfb54de381f1b8d4e597efd91
Udo Steinberg <udo@hypervisor.org>
2019-03-01 01:05:47 +0100

    ARM: Added CPU class.

--- inc/aarch64/cpu.hpp
+++ inc/aarch64/cpu.hpp
@@ -1,7 +1,7 @@
 /*
  * Central Processing Unit (CPU)
  *
- * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ * Copyright (C) 2019-2021 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -16,3 +16,41 @@
  */
 
 #pragma once
+
+#include "arch.hpp"
+#include "compiler.hpp"
+#include "types.hpp"
+
+class Cpu
+{
+    private:
+        static uint64   midr                CPULOCAL;           // Main ID Register
+        static uint64   mpidr               CPULOCAL;           // Multiprocessor Affinity Register
+
+    public:
+        static inline unsigned          boot_cpu    { 0 };
+        static inline unsigned          count       { 0 };
+
+        // Returns affinity in Aff3[31:24] Aff2[23:16] Aff1[15:8] Aff0[7:0] format
+        ALWAYS_INLINE
+        static inline auto affinity() { return static_cast<uint32>((mpidr >> 8 & BIT_RANGE (31, 24)) | (mpidr & BIT_RANGE (23, 0))); }
+
+        // Returns affinity in Aff3[39:32] Aff2[23:16] Aff1[15:8] Aff0[7:0] format
+        ALWAYS_INLINE
+        static inline auto affinity_bits (uint64 v) { return v & (BIT64_RANGE (39, 32) | BIT64_RANGE (23, 0)); }
+
+        ALWAYS_INLINE
+        static inline void preemption_disable() { asm volatile ("msr daifset, #0xf" : : : "memory"); }
+
+        ALWAYS_INLINE
+        static inline void preemption_enable() { asm volatile ("msr daifclr, #0xf" : : : "memory"); }
+
+        ALWAYS_INLINE
+        static inline void preemption_point() { asm volatile ("msr daifclr, #0xf; msr daifset, #0xf" : : : "memory"); }
+
+        ALWAYS_INLINE
+        static inline void halt() { asm volatile ("wfi; msr daifclr, #0xf; msr daifset, #0xf" : : : "memory"); }
+
+        static void init();
+        static void fini();
+};
