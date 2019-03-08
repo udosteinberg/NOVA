3db09b9ab4c7272eeff09644bb723791eacb01a1
Udo Steinberg <udo@hypervisor.org>
2019-03-08 15:41:06 +0100

    ARM: Added exception entry/exit.

---
+++ inc/aarch64/regs.hpp
@@ -0,0 +1,49 @@
+/*
+ * Register File
+ *
+ * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "arch.hpp"
+#include "compiler.hpp"
+#include "types.hpp"
+
+class Sys_regs
+{
+    public:
+        mword   r[31]       { 0 };
+        mword   el0_sp      { 0 };
+        mword   el0_tpidr   { 0 };
+        mword   el0_tpidrro { 0 };
+        mword   el2_elr     { 0 };
+        mword   el2_spsr    { PSTATE_A64_EL0 };
+
+        inline unsigned emode() const { return el2_spsr & (PSTATE_ALL_nRW | PSTATE_ALL_M); }
+
+        inline void set_sp (mword val) { el0_sp  = val; }
+        inline void set_ip (mword val) { el2_elr = val; }
+};
+
+class Exc_regs : public Sys_regs
+{
+    public:
+        mword   el2_esr     { 0 };
+        mword   el2_far     { 0 };
+
+        inline mword ep() const { return el2_esr >> 26; }
+
+        inline void set_ep (mword val) { el2_esr = val << 26; }
+};
