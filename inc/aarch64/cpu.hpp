4d05af306639049b9d2f64913bc95fb3f9f130e0
Udo Steinberg <udo@hypervisor.org>
2019-03-13 23:41:28 +0100

    ARM: Determine CPU features.

--- inc/aarch64/cpu.hpp
+++ inc/aarch64/cpu.hpp
@@ -24,11 +24,163 @@
 
 class Cpu
 {
+    private:
+        static void enumerate_features();
+
     public:
+        enum class Cpu_feature  // Appendix K13
+        {
+            EL0         =  0,   // EL0 Support AArch64 only (1), AArch64+AArch32 (2)
+            EL1         =  1,   // EL1 Support AArch64 only (1), AArch64+AArch32 (2)
+            EL2         =  2,   // EL2 Support AArch64 only (1), AArch64+AArch32 (2)
+            EL3         =  3,   // EL3 Support AArch64 only (1), AArch64+AArch32 (2)
+            FP          =  4,   // Floating Point (0), FEAT_FP16 (1), Unsupported (15)
+            ADVSIMD     =  5,   // Advanced SIMD (0), FEAT_FP16 (1), Unsupported (15)
+            GIC         =  6,   // GIC System Registers v3.0/4.0 (1), v4.1 (3)
+            RAS         =  7,   // FEAT_RASv1 (1), FEAT_RASv1p1 (2)
+            SVE         =  8,   // FEAT_SVE (1)
+            SEL2        =  9,   // FEAT_SEL2 (1)
+            MPAM        = 10,   // FEAT_MPAM (1)
+            AMU         = 11,   // FEAT_AMUv1 (1), FEAT_AMUv1p1 (2)
+            DIT         = 12,   // FEAT_DIT (1)
+            CSV2        = 14,   // FEAT_CSV2 (1,2)
+            CSV3        = 15,   // FEAT_CSV3 (1)
+            BT          = 16,   // FEAT_BTI (1)
+            SSBS        = 17,   // FEAT_SSBS (1,2)
+            MTE         = 18,   // FEAT_MTE (1,2)
+            RAS_FRAC    = 19,   // FEAT_RAS_frac (1)
+            MPAM_FRAC   = 20,   // FRAM_MPAM_frac (1)
+        };
+
+        enum class Dbg_feature  // Appendix K13
+        {
+            DEBUGVER    =  0,   // FEAT_Debugv8p0 (6), FEAT_Debugv8p1 (7), FEAT_Debugv8p2 (8), FEAT_Debugv8p4 (9)
+            TRACEVER    =  1,   // Trace Support
+            PMUVER      =  2,   // FEAT_PMUv3 (1), FEAT_PMUv3p1 (4), FEAT_PMUv3p4 (5), FEAT_PMUv3p5 (6), FEAT_PMUv3p7 (7)
+            BRPS        =  3,   // Number of Breakpoints - 1
+            WRPS        =  5,   // Number of Watchpoints - 1
+            CTXCMPS     =  7,   // Number of Context-Aware Breakpoints - 1
+            PMSVER      =  8,   // FEAT_SPE (1), FEAT_SPEv1p1 (2), FEAT_SPEv1p2 (3)
+            DOUBLELOCK  =  9,   // FEAT_DoubleLock (0), Unsupported (15)
+            TRACEFILT   = 10,   // FEAT_TRF (1)
+            TRACEBUFFER = 11,   // FEAT_TRBE (1)
+            MTPMU       = 12,   // FEAT_MTPMU (1)
+            BRBE        = 13,   // FEAT_BRBE (1)
+            CSRE        = 14,   // FEAT_CSRE (1)
+        };
+
+        enum class Isa_feature  // Appendix K13
+        {
+            AES         =  1,   // FEAT_AES (1), FEAT_PMULL (2)
+            SHA1        =  2,   // FEAT_SHA1 (1)
+            SHA2        =  3,   // FEAT_SHA256 (1), FEAT_SHA512 (2)
+            CRC32       =  4,   // FEAT_CRC (1)
+            ATOMIC      =  5,   // FEAT_LSE (2)
+            TME         =  6,   // FEAT_TME (1)
+            RDM         =  7,   // FEAT_RDM (1)
+            SHA3        =  8,   // FEAT_SHA3 (1)
+            SM3         =  9,   // FEAT_SM3 (1)
+            SM4         = 10,   // FEAT_SM4 (1)
+            DP          = 11,   // FEAT_DotProd (1)
+            FHM         = 12,   // FEAT_FHM (1)
+            TS          = 13,   // FEAT_FlagM (1), FEAT_FlagM2 (2)
+            TLB         = 14,   // FEAT_TLBIOS (1,2), FEAT_TLBIRANGE (2)
+            RNDR        = 15,   // FEAT_RNG (1)
+            DPB         = 16,   // FEAT_DPB (1), FEAT_DPB2 (2)
+            APA         = 17,   // FEAT_PAuth (0,1,2), FEAT_PAuth2 (3), FEAT_FPAC (4,5)
+            API         = 18,   // FEAT_PAuth (0,1,2), FEAT_PAuth2 (3), FEAT_FPAC (4,5)
+            JSCVT       = 19,   // FEAT_JSCVT (1)
+            FCMA        = 20,   // FEAT_FCMA (1)
+            LRCPC       = 21,   // FEAT_LRCPC (1), FEAT_LRCPC2 (2)
+            GPA         = 22,   // FEAT_PAuth
+            GPI         = 23,   // FEAT_PAuth
+            FRINTTS     = 24,   // FEAT_FRINTTS (1)
+            SB          = 25,   // FEAT_SB (1)
+            SPECRES     = 26,   // FEAT_SPECRES (1)
+            BF16        = 27,   // FEAT_BF16 (1)
+            DGH         = 28,   // FEAT_DGH (1)
+            I8MM        = 29,   // FEAT_I8MM (1)
+            XS          = 30,   // FEAT_XS (1)
+            LS64        = 31,   // FEAT_LS64 (1), FEAT_LS64_V (2), FEAT_LS64_ACCDATA (3)
+            WFXT        = 32,   // FEAT_WFXT (1)
+            RPRES       = 33,   // FEAT_RPRES (1)
+        };
+
+        enum class Mem_feature  // Appendix K13
+        {
+            PARANGE     =  0,   // FEAT_LPA (6)
+            ASIDBITS    =  1,   // Number of ASID Bits
+            BIGEND      =  2,   // Mixed Endian Support
+            SNSMEM      =  3,   // Secure/Non-Secure Memory Distinction
+            BIGEND_EL0  =  4,   // Mixed Endian Support at EL0
+            TGRAN16     =  5,   // Stage-1 Granule 16KB
+            TGRAN64     =  6,   // Stage-1 Granule 64KB
+            TGRAN4      =  7,   // Stage-1 Granule  4KB
+            TGRAN16_2   =  8,   // FEAT_GTG Stage-2 Granule 16KB
+            TGRAN64_2   =  9,   // FEAT_GTG Stage-2 Granule 64KB
+            TGRAN4_2    = 10,   // FEAT_GTG Stage-2 Granule  4KB
+            EXS         = 11,   // FEAT_ExS (1)
+            FGT         = 14,   // FEAT_FGT (1)
+            ECV         = 15,   // FEAT_ECV (1,2)
+            HAFDBS      = 16,   // FEAT_HAFDBS (1,2)
+            VMIDBITS    = 17,   // FEAT_VMID16 (2)
+            VH          = 18,   // FEAT_VHE (1)
+            HPDS        = 19,   // FEAT_HPDS (1), FEAT_HPDS2 (2)
+            LO          = 20,   // FEAT_LOR (1)
+            PAN         = 21,   // FEAT_PAN (1), FEAT_PAN2 (2), FEAT_PAN3 (3)
+            SPECSEI     = 22,   // RAS SError on speculative reads/fetches
+            XNX         = 23,   // FEAT_XNX (1)
+            TWED        = 24,   // FEAT_TWED (1)
+            ETS         = 25,   // FEAT_ETS (1)
+            HCX         = 26,   // FEAT_HCX (1)
+            AFP         = 27,   // FEAT_AFP (1)
+            CNP         = 32,   // FEAT_TTCNP (1)
+            UAO         = 33,   // FEAT_UAO (1)
+            LSM         = 34,   // FEAT_LSMAOC (1)
+            IESB        = 35,   // FEAT_IESB (1)
+            VARANGE     = 36,   // FEAT_LVA (1)
+            CCIDX       = 37,   // FEAT_CCIDX (1)
+            NV          = 38,   // FEAT_NV (1), FEAT_NV2 (2)
+            ST          = 39,   // FEAT_TTST (1)
+            AT          = 40,   // FEAT_LSE2 (1)
+            IDS         = 41,   // FEAT_IDST (1)
+            FWB         = 42,   // FEAT_S2FWB (1)
+            TTL         = 44,   // FEAT_TTL (1)
+            BBM         = 45,   // FEAT_BBM (0,1,2)
+            EVT         = 46,   // FEAT_EVT (1,2)
+            E0PD        = 47,   // FEAT_E0PD (1)
+        };
+
         static unsigned id              CPULOCAL;
         static unsigned hazard          CPULOCAL;
         static bool     bsp             CPULOCAL;
         static uint32   affinity        CPULOCAL;
+        static uint64   hcr_res0        CPULOCAL;
+        static uint64   sctlr32_res0    CPULOCAL;
+        static uint64   sctlr32_res1    CPULOCAL;
+        static uint64   sctlr64_res0    CPULOCAL;
+        static uint64   sctlr64_res1    CPULOCAL;
+        static uint64   spsr32_res0     CPULOCAL;
+        static uint64   spsr64_res0     CPULOCAL;
+        static uint64   tcr32_res0      CPULOCAL;
+        static uint64   tcr64_res0      CPULOCAL;
+        static uint64   midr            CPULOCAL;
+        static uint64   mpidr           CPULOCAL;
+        static uint64   cptr            CPULOCAL;
+        static uint64   mdcr            CPULOCAL;
+
+        static uint64   cpu64_feat[2]   CPULOCAL;   // ID_AA64PFRx
+        static uint64   dbg64_feat[2]   CPULOCAL;   // ID_AA64DFRx
+        static uint64   isa64_feat[3]   CPULOCAL;   // ID_AA64ISARx
+        static uint64   mem64_feat[3]   CPULOCAL;   // ID_AA64MMFRx
+        static uint64   sve64_feat[1]   CPULOCAL;   // ID_AA64ZFRx
+
+        static uint32   cpu32_feat[3]   CPULOCAL;   // ID_PFRx
+        static uint32   dbg32_feat[2]   CPULOCAL;   // ID_DFRx
+        static uint32   isa32_feat[7]   CPULOCAL;   // ID_ISARx
+        static uint32   mem32_feat[6]   CPULOCAL;   // ID_MMFRx
+        static uint32   mfp32_feat[3]   CPULOCAL;   // MVFRx
+
         static unsigned boot_cpu;
         static unsigned online;
         static Spinlock boot_lock       asm ("__boot_lock");
@@ -37,6 +189,26 @@ class Cpu
         static inline auto remote_affinity (unsigned cpu)
         {
             return __atomic_load_n (reinterpret_cast<decltype (affinity) *>(reinterpret_cast<uintptr_t>(&affinity) - CPU_LOCAL_DATA + CPU_GLOBL_DATA + cpu * PAGE_SIZE), __ATOMIC_RELAXED);
+        }
+
+        static inline unsigned feature (Cpu_feature f)
+        {
+            return cpu64_feat[static_cast<unsigned>(f) / 16] >> static_cast<unsigned>(f) % 16 * 4 & 0xf;
+        }
+
+        static inline unsigned feature (Dbg_feature f)
+        {
+            return dbg64_feat[static_cast<unsigned>(f) / 16] >> static_cast<unsigned>(f) % 16 * 4 & 0xf;
+        }
+
+        static inline unsigned feature (Isa_feature f)
+        {
+            return isa64_feat[static_cast<unsigned>(f) / 16] >> static_cast<unsigned>(f) % 16 * 4 & 0xf;
+        }
+
+        static inline unsigned feature (Mem_feature f)
+        {
+            return mem64_feat[static_cast<unsigned>(f) / 16] >> static_cast<unsigned>(f) % 16 * 4 & 0xf;
         }
 
         static inline void halt()
--- src/aarch64/cpu.cpp
+++ src/aarch64/cpu.cpp
@@ -16,6 +16,7 @@
  */
 
 #include "arch.hpp"
+#include "cache.hpp"
 #include "cpu.hpp"
 #include "extern.hpp"
 #include "stdio.hpp"
@@ -23,7 +24,56 @@
 unsigned Cpu::id, Cpu::hazard, Cpu::boot_cpu, Cpu::online;
 bool Cpu::bsp;
 uint32 Cpu::affinity;
+uint64 Cpu::hcr_res0;
+uint64 Cpu::sctlr32_res0, Cpu::sctlr32_res1, Cpu::sctlr64_res0, Cpu::sctlr64_res1;
+uint64 Cpu::spsr32_res0, Cpu::spsr64_res0, Cpu::tcr32_res0, Cpu::tcr64_res0;
+uint64 Cpu::midr, Cpu::mpidr, Cpu::cptr, Cpu::mdcr;
+uint64 Cpu::cpu64_feat[2], Cpu::dbg64_feat[2], Cpu::isa64_feat[3], Cpu::mem64_feat[3], Cpu::sve64_feat[1];
+uint32 Cpu::cpu32_feat[3], Cpu::dbg32_feat[2], Cpu::isa32_feat[7], Cpu::mem32_feat[6], Cpu::mfp32_feat[3];
 Spinlock Cpu::boot_lock;
+
+void Cpu::enumerate_features()
+{
+    asm volatile ("mrs %0, midr_el1"            : "=r" (midr));
+    asm volatile ("mrs %0, mpidr_el1"           : "=r" (mpidr));
+
+    asm volatile ("mrs %0, id_aa64dfr0_el1"     : "=r" (dbg64_feat[0]));
+    asm volatile ("mrs %0, id_aa64dfr1_el1"     : "=r" (dbg64_feat[1]));
+    asm volatile ("mrs %0, id_aa64isar0_el1"    : "=r" (isa64_feat[0]));
+    asm volatile ("mrs %0, id_aa64isar1_el1"    : "=r" (isa64_feat[1]));
+//  asm volatile ("mrs %0, id_aa64isar2_el1"    : "=r" (isa64_feat[2]));    // ARMv8.7
+    asm volatile ("mrs %0, id_aa64mmfr0_el1"    : "=r" (mem64_feat[0]));
+    asm volatile ("mrs %0, id_aa64mmfr1_el1"    : "=r" (mem64_feat[1]));
+//  asm volatile ("mrs %0, id_aa64mmfr2_el1"    : "=r" (mem64_feat[2]));    // ARMv8.2
+    asm volatile ("mrs %0, id_aa64pfr0_el1"     : "=r" (cpu64_feat[0]));
+    asm volatile ("mrs %0, id_aa64pfr1_el1"     : "=r" (cpu64_feat[1]));
+//  asm volatile ("mrs %0, id_aa64zfr0_el1"     : "=r" (sve64_feat[0]));    // SVE
+
+    if (feature (Cpu_feature::EL1) == 2) {
+        asm volatile ("mrs %x0, id_dfr0_el1"    : "=r" (dbg32_feat[0]));
+//      asm volatile ("mrs %x0, id_dfr1_el1"    : "=r" (dbg32_feat[1]));    // ARMv8.6
+        asm volatile ("mrs %x0, id_isar0_el1"   : "=r" (isa32_feat[0]));
+        asm volatile ("mrs %x0, id_isar1_el1"   : "=r" (isa32_feat[1]));
+        asm volatile ("mrs %x0, id_isar2_el1"   : "=r" (isa32_feat[2]));
+        asm volatile ("mrs %x0, id_isar3_el1"   : "=r" (isa32_feat[3]));
+        asm volatile ("mrs %x0, id_isar4_el1"   : "=r" (isa32_feat[4]));
+        asm volatile ("mrs %x0, id_isar5_el1"   : "=r" (isa32_feat[5]));
+//      asm volatile ("mrs %x0, id_isar6_el1"   : "=r" (isa32_feat[6]));    // ARMv8.2
+        asm volatile ("mrs %x0, id_mmfr0_el1"   : "=r" (mem32_feat[0]));
+        asm volatile ("mrs %x0, id_mmfr1_el1"   : "=r" (mem32_feat[1]));
+        asm volatile ("mrs %x0, id_mmfr2_el1"   : "=r" (mem32_feat[2]));
+        asm volatile ("mrs %x0, id_mmfr3_el1"   : "=r" (mem32_feat[3]));
+        asm volatile ("mrs %x0, id_mmfr4_el1"   : "=r" (mem32_feat[4]));
+//      asm volatile ("mrs %x0, id_mmfr5_el1"   : "=r" (mem32_feat[5]));
+        asm volatile ("mrs %x0, id_pfr0_el1"    : "=r" (cpu32_feat[0]));
+        asm volatile ("mrs %x0, id_pfr1_el1"    : "=r" (cpu32_feat[1]));
+//      asm volatile ("mrs %x0, id_pfr2_el1"    : "=r" (cpu32_feat[2]));
+    }
+
+    asm volatile ("mrs %x0, mvfr0_el1"          : "=r" (mfp32_feat[0]));
+    asm volatile ("mrs %x0, mvfr1_el1"          : "=r" (mfp32_feat[1]));
+    asm volatile ("mrs %x0, mvfr2_el1"          : "=r" (mfp32_feat[2]));
+}
 
 void Cpu::init (unsigned cpu, unsigned e)
 {
@@ -32,10 +82,7 @@ void Cpu::init (unsigned cpu, unsigned e)
     id  = cpu;
     bsp = cpu == boot_cpu;
 
-    uint64 midr, mpidr;
-
-    asm volatile ("mrs %0, midr_el1"  : "=r" (midr));
-    asm volatile ("mrs %0, mpidr_el1" : "=r" (mpidr));
+    enumerate_features();
 
     char const *impl = "Unknown", *part = impl;
 
@@ -84,9 +131,120 @@ void Cpu::init (unsigned cpu, unsigned e)
 
     affinity = static_cast<uint32>((mpidr >> 8 & 0xff000000) | (mpidr & 0xffffff));
 
-    trace (TRACE_CPU, "CORE: %x:%x:%x %s %20s r%llup%llu (EL%u)",
+    trace (TRACE_CPU, "CORE: %x:%x:%x %s %20s r%llup%llu PA:%u GIC:%u (EL%u)",
            affinity >> 16 & 0xff, affinity >> 8 & 0xff, affinity & 0xff,
-           impl, part, midr >> 20 & 0xf, midr & 0xf, e);
+           impl, part, midr >> 20 & 0xf, midr & 0xf, feature (Mem_feature::PARANGE), feature (Cpu_feature::GIC), e);
+
+    uint64 cptr_res1 =  (feature (Cpu_feature::FP)      != 15 ||
+                         feature (Cpu_feature::ADVSIMD) != 15 ? 0 : CPTR_TFP)   |
+                        (feature (Cpu_feature::SVE)     >=  1 ? 0 : CPTR_TZ)    |
+                        CPTR_RES1;
+
+    uint64 cptr_res0 =  (feature (Cpu_feature::AMU)     >=  1 ? 0 : CPTR_TAM)   |
+                        CPTR_RES0;
+
+    uint64 mdcr_res1 =  MDCR_RES1;
+
+    uint64 mdcr_res0 =  (feature (Dbg_feature::PMSVER)      >= 3 ? 0 : MDCR_HPMFZS)                                     |
+                        (feature (Dbg_feature::PMSVER)      >= 2 ? 0 : MDCR_TPMS | MDCR_E2PB)                           |
+                        (feature (Dbg_feature::PMUVER)      >= 7 ? 0 : MDCR_HPMFZO)                                     |
+                        (feature (Dbg_feature::PMUVER)      >= 6 ? 0 : MDCR_HLP | MDCR_HCCD)                            |
+                        (feature (Dbg_feature::PMUVER)      >= 4 ? 0 : MDCR_HPMD)                                       |
+                        (feature (Dbg_feature::PMUVER)      >= 1 ? 0 : MDCR_HPME | MDCR_TPM | MDCR_TPMCR | MDCR_HPMN)   |
+                        (feature (Dbg_feature::MTPMU)       >= 1 ? 0 : MDCR_MTPME)                                      |
+                        (feature (Mem_feature::FGT)         >= 1 ? 0 : MDCR_TDCC)                                       |
+                        (feature (Dbg_feature::TRACEBUFFER) >= 1 ? 0 : MDCR_E2TB)                                       |
+                        (feature (Dbg_feature::TRACEFILT)   >= 1 ? 0 : MDCR_TTRF)                                       |
+                        MDCR_RES0;
+
+    cptr = cptr_res1 & ~cptr_res0;
+    mdcr = mdcr_res1 & ~mdcr_res0;
+
+    hcr_res0  = (feature (Mem_feature::TWED) >= 1 ? 0 : HCR_TWEDEL | HCR_TWEDEn)            |
+                (feature (Cpu_feature::MTE)  >= 2 ? 0 : HCR_TID5 | HCR_DCT | HCR_ATA)       |
+                (feature (Mem_feature::EVT)  >= 2 ? 0 : HCR_TTLBOS | HCR_TTLBIS)            |
+                (feature (Mem_feature::EVT)  >= 1 ? 0 : HCR_TOCU | HCR_TICAB | HCR_TID4)    |
+                (feature (Cpu_feature::CSV2) >= 2 ? 0 : HCR_ENSCXT)                         |
+                (feature (Cpu_feature::AMU)  >= 2 ? 0 : HCR_AMVOFFEN)                       |
+                (feature (Cpu_feature::RAS)  >= 2 ? 0 : HCR_FIEN)                           |
+                (feature (Cpu_feature::RAS)  >= 1 ? 0 : HCR_TEA | HCR_TERR)                 |
+                (feature (Mem_feature::FWB)  >= 1 ? 0 : HCR_FWB)                            |
+                (feature (Mem_feature::NV)   >= 2 ? 0 : HCR_NV2)                            |
+                (feature (Mem_feature::NV)   >= 1 ? 0 : HCR_AT | HCR_NV1 | HCR_NV)          |
+                (feature (Isa_feature::APA)  >= 1 ||
+                 feature (Isa_feature::API)  >= 1 ||
+                 feature (Isa_feature::GPA)  >= 1 ||
+                 feature (Isa_feature::GPI)  >= 1 ? 0 : HCR_API | HCR_APK)                  |
+                (feature (Mem_feature::LO)   >= 1 ? 0 : HCR_TLOR)                           |
+                (feature (Mem_feature::VH)   >= 1 ? 0 : HCR_E2H)                            |
+                (feature (Isa_feature::TLB)  >= 1 ? 0 : HCR_TTLB)                           |
+                (feature (Isa_feature::DPB)  >= 1 ? 0 : HCR_TPCP)                           |
+                HCR_RES0;
+
+    sctlr32_res1 = (feature (Mem_feature::LSM)     >= 1 ? 0 : SCTLR_A32_LSMAOE | SCTLR_A32_nTLSMD)  |
+                   (feature (Mem_feature::PAN)     >= 1 ? 0 : SCTLR_ALL_SPAN)                       |
+                   SCTLR_A32_RES1;
+
+    sctlr32_res0 = (feature (Cpu_feature::SSBS)    >= 1 ? 0 : SCTLR_A32_DSSBS)  |
+                   (feature (Isa_feature::SPECRES) >= 1 ? 0 : SCTLR_ALL_EnRCTX) |
+                   SCTLR_A32_RES0;
+
+    sctlr64_res1 = (feature (Mem_feature::LSM)     >= 1 ? 0 : SCTLR_A64_LSMAOE | SCTLR_A64_nTLSMD)  |
+                   (feature (Mem_feature::PAN)     >= 1 ? 0 : SCTLR_ALL_SPAN)                       |
+                   (feature (Mem_feature::EXS)     >= 1 ? 0 : SCTLR_A64_EIS | SCTLR_A64_EOS)        |
+                   (feature (Cpu_feature::CSV2)    >= 2 ? 0 : SCTLR_A64_TSCXT)                      |
+                   SCTLR_A64_RES1;
+
+    sctlr64_res0 = (feature (Dbg_feature::CSRE)    >= 1 ? 0 : SCTLR_A64_EnCSR0)                                                                     |
+                   (feature (Mem_feature::PAN)     >= 3 ? 0 : SCTLR_A64_EPAN)                                                                       |
+                   (feature (Isa_feature::LS64)    >= 1 ? 0 : SCTLR_A64_EnALS | SCTLR_A64_EnAS0 | SCTLR_A64_EnASR)                                  |
+                   (feature (Isa_feature::TME)     >= 1 ? 0 : SCTLR_A64_TME | SCTLR_A64_TME0 | SCTLR_A64_TMT | SCTLR_A64_TMT0)                      |
+                   (feature (Mem_feature::TWED)    >= 1 ? 0 : SCTLR_A64_TWEDEL | SCTLR_A64_TWEDEn)                                                  |
+                   (feature (Cpu_feature::SSBS)    >= 1 ? 0 : SCTLR_A64_DSSBS)                                                                      |
+                   (feature (Cpu_feature::MTE)     >= 2 ? 0 : SCTLR_A64_ATA | SCTLR_A64_ATA0 | SCTLR_A64_TCF | SCTLR_A64_TCF0 | SCTLR_A64_ITFSB)    |
+                   (feature (Cpu_feature::BT)      >= 1 ? 0 : SCTLR_A64_BT1 | SCTLR_A64_BT0)                                                        |
+                   (feature (Isa_feature::APA)     >= 1 ||
+                    feature (Isa_feature::API)     >= 1 ||
+                    feature (Isa_feature::GPA)     >= 1 ||
+                    feature (Isa_feature::GPI)     >= 1 ? 0 : SCTLR_A64_EnIA | SCTLR_A64_EnIB | SCTLR_A64_EnDA | SCTLR_A64_EnDB)                    |
+                   (feature (Mem_feature::IESB)    >= 1 ? 0 : SCTLR_A64_IESB)                                                                       |
+                   (feature (Isa_feature::SPECRES) >= 1 ? 0 : SCTLR_ALL_EnRCTX)                                                                     |
+                   (feature (Mem_feature::AT)      >= 1 ? 0 : SCTLR_A64_nAA)                                                                        |
+                   SCTLR_A64_RES0;
+
+    spsr32_res0 = (feature (Cpu_feature::DIT)  >= 1 ? 0 : SPSR_ALL_DIT)   |
+                  (feature (Cpu_feature::SSBS) >= 1 ? 0 : SPSR_A32_SSBS)  |
+                  (feature (Mem_feature::PAN)  >= 1 ? 0 : SPSR_ALL_PAN)   |
+                  SPSR_A32_RES0;
+
+    spsr64_res0 = (feature (Dbg_feature::CSRE) >= 1 ? 0 : SPSR_A64_CSR)   |
+                  (feature (Cpu_feature::MTE)  >= 2 ? 0 : SPSR_A64_TCO)   |
+                  (feature (Cpu_feature::DIT)  >= 1 ? 0 : SPSR_ALL_DIT)   |
+                  (feature (Mem_feature::UAO)  >= 1 ? 0 : SPSR_A64_UAO)   |
+                  (feature (Mem_feature::PAN)  >= 1 ? 0 : SPSR_ALL_PAN)   |
+                  (feature (Cpu_feature::SSBS) >= 1 ? 0 : SPSR_A64_SSBS)  |
+                  (feature (Cpu_feature::BT)   >= 1 ? 0 : SPSR_A64_BTYPE) |
+                  SPSR_A64_RES0;
+
+    tcr32_res0 = (feature (Mem_feature::HPDS)   >= 2 ? 0 : TCR_ALL_HWU162 | TCR_ALL_HWU161 | TCR_ALL_HWU160 | TCR_ALL_HWU159 | TCR_ALL_HWU062 | TCR_ALL_HWU061 | TCR_ALL_HWU060 | TCR_ALL_HWU059) |
+                 (feature (Mem_feature::HPDS)   >= 1 ? 0 : TCR_ALL_HPD1  | TCR_ALL_HPD0)    |
+                 TCR_A32_RES0;
+
+    tcr64_res0 = (feature (Mem_feature::PARANGE) >= 6 ? 0 : TCR_A64_DS) |
+                 (feature (Cpu_feature::MTE)     >= 2 ? 0 : TCR_A64_TCMA1 | TCR_A64_TCMA0)   |
+                 (feature (Mem_feature::E0PD)    >= 1 ? 0 : TCR_A64_E0PD1 | TCR_A64_E0PD0)   |
+                 (feature (Cpu_feature::SVE)     >= 1 ? 0 : TCR_A64_NFD1  | TCR_A64_NFD0)    |
+                 (feature (Isa_feature::APA)     >= 1 ||
+                  feature (Isa_feature::API)     >= 1 ||
+                  feature (Isa_feature::GPA)     >= 1 ||
+                  feature (Isa_feature::GPI)     >= 1 ? 0 : TCR_A64_TBID1 | TCR_A64_TBID0)   |
+                 (feature (Mem_feature::HPDS)    >= 2 ? 0 : TCR_ALL_HWU162 | TCR_ALL_HWU161 | TCR_ALL_HWU160 | TCR_ALL_HWU159 | TCR_ALL_HWU062 | TCR_ALL_HWU061 | TCR_ALL_HWU060 | TCR_ALL_HWU059) |
+                 (feature (Mem_feature::HPDS)    >= 1 ? 0 : TCR_ALL_HPD1  | TCR_ALL_HPD0)    |
+                 (feature (Mem_feature::HAFDBS)  >= 2 ? 0 : TCR_A64_HD)                      |
+                 (feature (Mem_feature::HAFDBS)  >= 1 ? 0 : TCR_A64_HA)                      |
+                 TCR_A64_RES0;
+
+    Cache::init();
 
     boot_lock.unlock();
 }
