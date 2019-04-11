564f5c69f1c59daded29cfc5eb5553859fa00c79
Udo Steinberg <udo@hypervisor.org>
2019-04-11 16:54:32 +0200

    ARM: Added SC class.

--- src/aarch64/interrupt.cpp
+++ src/aarch64/interrupt.cpp
@@ -21,6 +21,7 @@
 #include "gicd.hpp"
 #include "gicr.hpp"
 #include "interrupt.hpp"
+#include "sc.hpp"
 #include "stdio.hpp"
 #include "timer.hpp"
 
@@ -33,7 +34,7 @@ Event::Selector Interrupt::handle_sgi (uint32 val, bool)
     Counter::count_sgi (sgi);
 
     switch (sgi) {
-        case Sgi::RRQ: break;
+        case Sgi::RRQ: Sc::rrq_handler(); break;
         case Sgi::RKE: break;
     }
 
