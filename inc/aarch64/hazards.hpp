a5591025a983d6dd87c2be8dde7b5bfd0869bbc8
Udo Steinberg <udo@hypervisor.org>
2019-04-11 16:34:19 +0200

    ARM: Added hazards.

---
+++ inc/aarch64/hazards.hpp
@@ -0,0 +1,32 @@
+/*
+ * Hazards
+ *
+ * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2019-2021 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "macros.hpp"
+
+#define HZD_SCHED       BIT  (0)
+#define HZD_SLEEP       BIT  (1)
+#define HZD_RCU         BIT  (2)
+#define HZD_BOOT_HST    BIT  (8)
+#define HZD_BOOT_GST    BIT  (9)
+#define HZD_FPU         BIT (16)
+#define HZD_RECALL      BIT (30)
+#define HZD_ILLEGAL     BIT (31)
