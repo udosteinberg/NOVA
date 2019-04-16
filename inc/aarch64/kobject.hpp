db239d40a6ea3d5a6f1190a4814e37f7bca3bca3
Udo Steinberg <udo@hypervisor.org>
2019-04-16 10:57:56 +0200

    ARM: Added Kobject class.

---
+++ inc/aarch64/kobject.hpp
@@ -0,0 +1,56 @@
+/*
+ * Kernel Object
+ *
+ * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "macros.hpp"
+#include "types.hpp"
+
+class Kobject
+{
+    friend class Capability;
+
+    protected:
+        static constexpr auto alignment = BIT (5);
+
+        enum class Type : uint8
+        {
+            PD,
+            EC,
+            SC,
+            PT,
+            SM,
+        };
+
+        enum class Subtype : uint8
+        {
+            NONE,
+            EC_LOCAL,
+            EC_GLOBAL,
+            EC_VCPU,
+        };
+
+        Type    const   type;
+        Subtype const   subtype;
+
+    public:
+        inline explicit Kobject (Type t, Subtype s = Subtype::NONE) : type (t), subtype (s) {}
+};
