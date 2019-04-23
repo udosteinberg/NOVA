6327fe832e5718b5dad3fd3b9093776634c102b7
Udo Steinberg <udo@hypervisor.org>
2019-04-23 17:08:12 +0200

    ELF: Add enums for ELF constants.

--- inc/elf.hpp
+++ inc/elf.hpp
@@ -5,6 +5,7 @@
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
  * Copyright (C) 2012 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -24,14 +25,46 @@
 
 class Eh
 {
+    private:
+        enum Elf_data : uint8
+        {
+            ED_LSB      = 1,
+        };
+
+        enum Elf_type : uint16
+        {
+            ET_EXEC     = 2,
+        };
+
     public:
-        uint32  ei_magic;
-        uint8   ei_class, ei_data, ei_version, ei_osabi, ei_abiversion, ei_pad[7];
-        uint16  type, machine;
-        uint32  version;
-        mword   entry, ph_offset, sh_offset;
-        uint32  flags;
-        uint16  eh_size, ph_size, ph_count, sh_size, sh_count, strtab;
+        enum Elf_class : uint8
+        {
+            EC_32       = 1,
+            EC_64       = 2,
+        };
+
+        enum Elf_machine : uint16
+        {
+            EM_386      = 3,
+            EM_X86_64   = 62,
+            EM_AARCH64  = 183,
+        };
+
+        bool valid (Elf_class c, Elf_machine m) const
+        {
+            return ei_magic == 0x464c457f && ei_class == c && ei_data == ED_LSB && type == ET_EXEC && machine == m;
+        }
+
+        uint32          ei_magic;
+        Elf_class       ei_class;
+        Elf_data        ei_data;
+        uint8           ei_version, ei_osabi, ei_abiversion, ei_pad[7];
+        Elf_type        type;
+        Elf_machine     machine;
+        uint32          version;
+        mword           entry, ph_offset, sh_offset;
+        uint32          flags;
+        uint16          eh_size, ph_size, ph_count, sh_size, sh_count, strtab;
 };
 
 class Ph32
