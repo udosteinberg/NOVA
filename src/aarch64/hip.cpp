283a462cc55c6973eb826602060ae6ddd2bc9fbb
Udo Steinberg <udo@hypervisor.org>
2019-05-08 12:18:18 -0700

    ARM: Added HIP class.

---
+++ src/aarch64/hip.cpp
@@ -0,0 +1,81 @@
+/*
+ * Hypervisor Information Page (HIP): Architecture-Independent Part
+ *
+ * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2014 Udo Steinberg, FireEye, Inc.
+ * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "acpi.hpp"
+#include "console_mbuf.hpp"
+#include "event.hpp"
+#include "extern.hpp"
+#include "hip.hpp"
+#include "interrupt.hpp"
+#include "memory.hpp"
+#include "space_obj.hpp"
+#include "stdio.hpp"
+#include "timer.hpp"
+#include "uefi.hpp"
+
+Hip *Hip::hip = reinterpret_cast<Hip *>(&MHIP_HVAS);
+
+void Hip::build (uint64 root_s, uint64 root_e)
+{
+    signature       = 0x41564f4e;
+    length          = sizeof (*this);
+    nova_p_addr     = Buddy::sym_to_phys (&NOVA_HPAS);
+    nova_e_addr     = Buddy::sym_to_phys (&NOVA_HPAE);
+    mbuf_p_addr     = Console_mbuf::con.addr();
+    mbuf_e_addr     = Console_mbuf::con.addr() + Console_mbuf::con.size();
+    root_p_addr     = root_s;
+    root_e_addr     = root_e;
+    acpi_rsdp_addr  = Acpi::rsdp;
+    uefi_mmap_addr  = Uefi::map ? reinterpret_cast<uintptr_t>(Uefi::map) : ~0ULL;
+    uefi_mmap_size  = Uefi::map ? static_cast<decltype (uefi_mmap_size)>(Uefi::msz) : 0;
+    uefi_desc_size  = Uefi::map ? static_cast<decltype (uefi_desc_size)>(Uefi::dsc) : 0;
+    uefi_desc_vers  = Uefi::map ? static_cast<decltype (uefi_desc_vers)>(Uefi::ver) : 0;
+    tmr_frq         = Timer::ms_to_ticks (1000);
+    sel_num         = Space_obj::num;
+    sel_hst_arch    = Event::hst_arch;
+    sel_hst_nova    = Event::hst_max;
+    sel_gst_arch    = Event::gst_arch;
+    sel_gst_nova    = Event::gst_max;
+    num_cpu         = static_cast<uint16>(Cpu::online);
+    num_int         = static_cast<uint16>(Interrupt::count());
+
+    trace (TRACE_ROOT, "INFO: NOVA: %#018llx-%#018llx", nova_p_addr, nova_e_addr);
+    trace (TRACE_ROOT, "INFO: MBUF: %#018llx-%#018llx", mbuf_p_addr, mbuf_e_addr);
+    trace (TRACE_ROOT, "INFO: ROOT: %#018llx-%#018llx", root_p_addr, root_e_addr);
+    trace (TRACE_ROOT, "INFO: ACPI: %#llx", acpi_rsdp_addr);
+    trace (TRACE_ROOT, "INFO: UEFI: %#llx", uefi_mmap_addr);
+    trace (TRACE_ROOT, "INFO: FREQ: %llu Hz", tmr_frq);
+    trace (TRACE_ROOT, "INFO: SEL#: %#llx", sel_num);
+    trace (TRACE_ROOT, "INFO: HST#: %3u + %u", sel_hst_arch, sel_hst_nova);
+    trace (TRACE_ROOT, "INFO: GST#: %3u + %u", sel_gst_arch, sel_gst_nova);
+    trace (TRACE_ROOT, "INFO: CPU#: %3u", num_cpu);
+    trace (TRACE_ROOT, "INFO: INT#: %3u", num_int);
+
+    arch.build();
+
+    uint16 c = 0;
+    for (uint16 const *ptr = reinterpret_cast<uint16 const *>(this);
+                       ptr < reinterpret_cast<uint16 const *>(this + 1);
+                       c = static_cast<uint16>(c - *ptr++)) ;
+
+    checksum = c;
+}
--- src/aarch64/hip_arch.cpp
+++ src/aarch64/hip_arch.cpp
@@ -1,5 +1,5 @@
 /*
- * External Symbols
+ * Hypervisor Information Page (HIP): Architecture-Specific Part (ARM)
  *
  * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
  *
@@ -15,19 +15,15 @@
  * GNU General Public License version 2 for more details.
  */
 
-#pragma once
+#include "hip_arch.hpp"
+#include "smmu.hpp"
+#include "stdio.hpp"
 
-#include "types.hpp"
+void Hip_arch::build()
+{
+    num_smg = static_cast<uint16>(Smmu::num_smg);
+    num_ctx = static_cast<uint16>(Smmu::num_ctx);
 
-extern char __init_psci[], __init_spin[], GIT_VER[];
-
-extern uintptr_t __boot_p0, __boot_p1, __boot_p2, __boot_ts;
-
-extern uintptr_t MHIP_HVAS;
-extern uintptr_t NOVA_HPAS, NOVA_HPAE;
-extern uintptr_t PTAB_HVAS;
-
-extern void (*CTORS_L)();
-extern void (*CTORS_C)();
-extern void (*CTORS_G)();
-extern void (*CTORS_E)();
+    trace (TRACE_ROOT, "INFO: SMG#: %3u", num_smg);
+    trace (TRACE_ROOT, "INFO: CTX#: %3u", num_ctx);
+}
