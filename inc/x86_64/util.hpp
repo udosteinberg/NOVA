d4892b051c104fd7e7f8dd2ca81e45d044bf79e4
Udo Steinberg <udo@hypervisor.org>
2019-05-14 05:02:04 +0200

    GEN: Added utility function for greatest common divisor.

--- inc/x86_64/util.hpp
+++ inc/x86_64/util.hpp
@@ -4,6 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
+ *
  * This file is part of the NOVA microhypervisor.
  *
  * NOVA is free software: you can redistribute it and/or modify it
@@ -22,14 +24,27 @@
 
 template <typename T>
 ALWAYS_INLINE
-static inline T min (T v1, T v2)
+static inline constexpr T min (T v1, T v2)
 {
     return v1 < v2 ? v1 : v2;
 }
 
 template <typename T>
 ALWAYS_INLINE
-static inline T max (T v1, T v2)
+static inline constexpr T max (T v1, T v2)
 {
     return v1 > v2 ? v1 : v2;
+}
+
+template <typename T>
+ALWAYS_INLINE
+static inline constexpr T gcd (T v1, T v2)
+{
+    while (v2) {
+        T r = v1 % v2;
+        v1  = v2;
+        v2  = r;
+    }
+
+    return v1;
 }
