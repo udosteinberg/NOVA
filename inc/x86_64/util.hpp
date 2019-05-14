327fc1a49d6582729d3d9c99b2589365ea31b9cf
Udo Steinberg <udo@hypervisor.org>
2019-05-14 05:02:04 +0200

    GEN: Added utility functions.

--- inc/x86_64/util.hpp
+++ inc/x86_64/util.hpp
@@ -4,6 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
+ * Copyright (C) 2019-2024 Udo Steinberg, BedRock Systems, Inc.
+ *
  * This file is part of the NOVA microhypervisor.
  *
  * NOVA is free software: you can redistribute it and/or modify it
@@ -19,17 +21,49 @@
 #pragma once
 
 #include "compiler.hpp"
+#include "types.hpp"
 
 template <typename T>
-ALWAYS_INLINE
-static inline T min (T v1, T v2)
+static constexpr T min (T v1, T v2)
 {
     return v1 < v2 ? v1 : v2;
 }
 
 template <typename T>
-ALWAYS_INLINE
-static inline T max (T v1, T v2)
+static constexpr T max (T v1, T v2)
 {
     return v1 > v2 ? v1 : v2;
+}
+
+template <typename T>
+static constexpr T gcd (T v1, T v2)
+{
+    while (v2) {
+        T r = v1 % v2;
+        v1  = v2;
+        v2  = r;
+    }
+
+    return v1;
+}
+
+template <typename T>
+static constexpr bool match_list (T const list[], T id)
+{
+    for (auto ptr { list }; *ptr; ptr++)
+        if (*ptr == id)
+            return true;
+
+    return false;
+}
+
+/*
+ * Turns p into an "exposed" pointer. As a result, the standard allows
+ * integers to be converted to pointers of the same provenance as p.
+ */
+template <typename T>
+constexpr T *expose (T *p) noexcept
+{
+    [[maybe_unused]] auto x = reinterpret_cast<uintptr_t>(p);
+    return p;
 }
