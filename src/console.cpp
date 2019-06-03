7b5426690ad0b0cc3e91389e60351b5981de7202
Udo Steinberg <udo@hypervisor.org>
2019-06-03 17:14:39 +0200

    BUILD: Fixed range expression warnings.

--- src/console.cpp
+++ src/console.cpp
@@ -99,7 +99,7 @@ void Console::vprintf (char const *format, va_list args)
 
             switch (*++format) {
 
-                case '0'...'9':
+                case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
                     switch (mode) {
                         case MODE_FLAGS:
                             if (*format == '0') {
