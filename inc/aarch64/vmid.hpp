fa51a920d470a6b6734634357d276eaf649e82c2
Udo Steinberg <udo@hypervisor.org>
2019-06-11 14:28:40 +0200

    ARM: Added VMID class.

---
+++ inc/aarch64/vmid.hpp
@@ -0,0 +1,41 @@
+/*
+ * Virtual-Machine Identifier (VMID)
+ *
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "atomic.hpp"
+#include "macros.hpp"
+#include "types.hpp"
+
+class Vmid final
+{
+    private:
+        uint16_t const val;
+
+        static inline Atomic<uint16_t> allocator { 0 };
+
+        // FIXME: Handle overflow
+        static inline auto alloc()
+        {
+            return allocator++;
+        }
+
+    public:
+        inline Vmid() : val (alloc()) {}
+
+        inline operator auto() const { return val & BIT_RANGE (7, 0); }
+};
