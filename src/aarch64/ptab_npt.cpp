cadf94cd881e149ef11361fb158088add53b7e77
Udo Steinberg <udo@hypervisor.org>
2019-07-17 15:56:52 +0200

    Arm: Added NPT class.

--- src/aarch64/ptab_npt.cpp
+++ src/aarch64/ptab_npt.cpp
@@ -1,5 +1,5 @@
 /*
- * Page Table Templates
+ * Nested Page Table (NPT)
  *
  * Copyright (C) 2019-2026 Udo Steinberg, BlueRock Security, Inc.
  *
@@ -15,10 +15,21 @@
  * GNU General Public License version 2 for more details.
  */
 
-#pragma once
-
-#include "ptab_hpt.hpp"
 #include "ptab_npt.hpp"
 
-template class Ptab<Hpt, uint64_t, uint64_t>;
-template class Ptab<Npt, uint64_t, uint64_t>;
+uint64_t Nptp::current;
+
+void Nptp::init()
+{
+    // Reset at resume time to match vttbr
+    current = 0;
+
+    auto const oas { 2 };
+
+    bool const feat_vmid16 { false };
+
+    // Reduce the number of VMIDs according to what this CPU supports
+    Vmid::allocator.reduce (BIT (feat_vmid16 ? 16 : 8));
+
+    asm volatile ("msr vtcr_el2, %x0; isb" : : "r" (VTCR_RES1 | feat_vmid16 * BIT (19) | oas << 16 | TCR_TG0_4K | TCR_SH0_INNER | TCR_ORGN0_WB_RW | TCR_IRGN0_WB_RW | (Npt::lev() - 2) << 6 | (64 - Npt::ibits)) : "memory");
+}
