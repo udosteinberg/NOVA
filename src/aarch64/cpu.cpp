1525f84de7502fd4ffb53d41bfedfdd1d087d083
Udo Steinberg <udo@hypervisor.org>
2019-07-22 15:20:30 -0400

    Arm: Added VMCB class.

--- src/aarch64/cpu.cpp
+++ src/aarch64/cpu.cpp
@@ -26,6 +26,7 @@
 #include "ptab_npt.hpp"
 #include "stdio.hpp"
 #include "timer.hpp"
+#include "vmcb.hpp"
 
 bool Cpu::bsp;
 cpu_t Cpu::id;
@@ -281,6 +282,7 @@ void Cpu::init (cpu_t cpu)
     Timer::init();
 
     Nptp::init();
+    Vmcb::init();
 
     boot_lock.unlock();
 }
