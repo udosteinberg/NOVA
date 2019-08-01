1ec1fc334a14cd5822795851a2ecf040ca48b952
Udo Steinberg <udo@hypervisor.org>
2019-08-01 17:29:11 +0200

    Arm: Added board definitions.

---
+++ inc/aarch64/board_acpi.hpp
@@ -0,0 +1,35 @@
+/*
+ * Board-Specific Configuration: Any ACPI-Based Board
+ *
+ * Copyright (C) 2019-2025 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#ifndef __ASSEMBLER__
+
+#include "debug.hpp"
+
+struct Board
+{
+    static constexpr uint64_t spin_addr { 0 };
+    static constexpr struct Cpu { uint64_t id; } cpu[1] { { 0 } };
+    static constexpr struct Tmr { unsigned ppi, flg; } tmr[2] { { 0, 0 }, { 0, 0 } };
+    static constexpr struct Gic { uint64_t mmio, size; } gic[4] { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } };
+    static constexpr struct Uart { Debug::Subtype type; uint64_t mmio; unsigned clock; } uart { Debug::Subtype::SERIAL_NONE, 0, 0 };
+    static constexpr struct Smmu_v2 { uint64_t mmio; struct { unsigned spi, flg; } glb[1], ctx[1]; } smmu_v2[1] {};
+    static constexpr struct Smmu_v3 { uint64_t mmio; unsigned evt; unsigned glb; unsigned cmd; unsigned pri; } smmu_v3[1] {};
+};
+
+#endif
---
+++ inc/aarch64/board_nxp_imx8m.hpp
@@ -0,0 +1,37 @@
+/*
+ * Board-Specific Configuration: NXP i.MX 8M
+ *
+ * Copyright (C) 2019-2025 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#define RAM_BASE    0x40000000  // 0x40000000 - 0x13fffffff
+
+#ifndef __ASSEMBLER__
+
+#include "debug.hpp"
+
+struct Board
+{
+    static constexpr uint64_t spin_addr { 0 };
+    static constexpr struct Cpu { uint64_t id; } cpu[4] { { 0x0 }, { 0x1 }, { 0x2 }, { 0x3 } };
+    static constexpr struct Tmr { unsigned ppi, flg; } tmr[2] { { 0xa, 0x8 }, { 0xb, 0x8 } };
+    static constexpr struct Gic { uint64_t mmio, size; } gic[4] { { 0x38800000, 0x10000 }, { 0x38880000, 0x80000 }, { 0, 0 }, { 0, 0 } };
+    static constexpr struct Uart { Debug::Subtype type; uint64_t mmio; unsigned clock; } uart { Debug::Subtype::SERIAL_IMX, 0x30860000, 25'000'000 };
+    static constexpr struct Smmu_v2 { uint64_t mmio; struct { unsigned spi, flg; } glb[1], ctx[1]; } smmu_v2[1] {};
+    static constexpr struct Smmu_v3 { uint64_t mmio; unsigned evt; unsigned glb; unsigned cmd; unsigned pri; } smmu_v3[1] {};
+};
+
+#endif
