042081911238a9312dacb5224a80233cdcc1a39e
Udo Steinberg <udo@hypervisor.org>
2019-08-01 17:29:11 +0200

    ARM: Added board definitions.

---
+++ inc/aarch64/board_qemu.hpp
@@ -0,0 +1,36 @@
+/*
+ * Board-Specific Configuration: QEMU Virtual Board
+ *
+ * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#define LOAD_ADDR       0x48000000
+
+#ifndef __ASSEMBLER__
+
+#include "types.hpp"
+
+struct Board
+{
+    static constexpr uint64 spin_addr = 0;
+    static constexpr struct Cpu { uint64 id; } cpu[4] = { { 0 }, { 1 }, { 2 }, { 3 } };
+    static constexpr struct Tmr { unsigned ppi, flg; } tmr[2] = { { 10, 0x4 }, { 11, 0x4 } };
+    static constexpr struct Gic { uint64 mmio, size; } gic[4] = { { 0x8000000, 0x10000 }, { 0x80a0000, 0xf60000 }, { 0x8010000, 0x10000 }, { 0x8030000, 0x10000 } };
+    static constexpr struct Uart { Uart_type type; uint64 mmio; unsigned clock; } uart = { Uart_type::PL011, 0x9000000, 0 };
+    static constexpr struct Smmu { uint64 mmio; struct { unsigned spi, flg; } glb[1], ctx[1]; } smmu[1] = { { 0, { { 0, 0 } }, { { 0, 0 } } } };
+};
+
+#endif
---
+++ inc/aarch64/board_uefi.hpp
@@ -0,0 +1,36 @@
+/*
+ * Board-Specific Configuration: Any UEFI-Based Board
+ *
+ * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#define LOAD_ADDR       0x48000000
+
+#ifndef __ASSEMBLER__
+
+#include "types.hpp"
+
+struct Board
+{
+    static constexpr uint64 spin_addr = 0;
+    static constexpr struct Cpu { uint64 id; } cpu[1] = { { 0 } };
+    static constexpr struct Tmr { unsigned ppi, flg; } tmr[2] = { { 0, 0 }, { 0, 0 } };
+    static constexpr struct Gic { uint64 mmio, size; } gic[4] = { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } };
+    static constexpr struct Uart { Uart_type type; uint64 mmio; unsigned clock; } uart = { Uart_type::NONE, 0, 0 };
+    static constexpr struct Smmu { uint64 mmio; struct { unsigned spi, flg; } glb[1], ctx[1]; } smmu[1] = { { 0, { { 0, 0 } }, { { 0, 0 } } } };
+};
+
+#endif
