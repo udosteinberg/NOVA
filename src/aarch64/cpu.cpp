6b094cbb632c754b26e4767ba2231c2a0ed50b1e
Udo Steinberg <udo@hypervisor.org>
2019-08-03 10:49:50 +0200

    ARM: Added GICD (Distributor).

--- src/aarch64/cpu.cpp
+++ src/aarch64/cpu.cpp
@@ -18,6 +18,7 @@
 #include "cache.hpp"
 #include "cpu.hpp"
 #include "extern.hpp"
+#include "gicd.hpp"
 #include "hazards.hpp"
 #include "npt.hpp"
 #include "stdio.hpp"
@@ -251,6 +252,8 @@ void Cpu::init (unsigned cpu, unsigned e)
                  TCR_A64_RES0;
 
     Cache::init();
+
+    Gicd::init();
 
     boot_lock.unlock();
 }
