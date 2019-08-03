08af85dd001cc8900cab4f06e0b68bfe56dd2184
Udo Steinberg <udo@hypervisor.org>
2019-08-03 10:49:50 +0200

    ARM: Added GICD (Distributor).

---
+++ inc/aarch64/gicd.hpp
@@ -0,0 +1,111 @@
+/*
+ * Generic Interrupt Controller: Distributor (GICD)
+ *
+ * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "coresight.hpp"
+#include "intid.hpp"
+#include "memory.hpp"
+#include "spinlock.hpp"
+#include "types.hpp"
+
+class Gicd : private Coresight, private Intid
+{
+    friend class Acpi_table_madt;
+
+    private:
+        enum Group
+        {
+            GROUP0 = 0x00000000,
+            GROUP1 = 0xffffffff,
+        };
+
+        enum class Register32
+        {
+            CTLR        = 0x0000,   // v2 v3 rw Control Register
+            TYPER       = 0x0004,   // v2 v3 ro Type Register
+            IIDR        = 0x0008,   // v2 v3 ro Implementer Identification Register
+            STATUSR     = 0x0010,   // -- v3 rw Error Reporting Status Register
+            SETSPI_NSR  = 0x0040,   // -- v3 wo Set SPI Register, Non-Secure
+            CLRSPI_NSR  = 0x0048,   // -- v3 wo Clr SPI Register, Non-Secure
+            SETSPI_SR   = 0x0050,   // -- v3 wo Set SPI Register, Secure
+            CLRSPI_SR   = 0x0058,   // -- v3 wo Clr SPI Register, Secure
+            SGIR        = 0x0f00,   // v2 -- wo Software Generated Interrupt Register
+        };
+
+        enum class Array32
+        {
+            IGROUPR     = 0x0080,   // v2 v3 rw Interrupt Group Registers
+            ISENABLER   = 0x0100,   // v2 v3 rw Interrupt Set-Enable Registers
+            ICENABLER   = 0x0180,   // v2 v3 rw Interrupt Clr-Enable Registers
+            ISPENDR     = 0x0200,   // v2 v3 rw Interrupt Set-Pending Registers
+            ICPENDR     = 0x0280,   // v2 v3 rw Interrupt Clr-Pending Registers
+            ISACTIVER   = 0x0300,   // v2 v3 rw Interrupt Set-Active Registers
+            ICACTIVER   = 0x0380,   // v2 v3 rw Interrupt Clr-Active Registers
+            IPRIORITYR  = 0x0400,   // v2 v3 rw Interrupt Priority Registers
+            ITARGETSR   = 0x0800,   // v2 -- rw Interrupt Processor Targets Registers
+            ICFGR       = 0x0c00,   // v2 v3 rw Interrupt Configuration Registers
+            IGRPMODR    = 0x0d00,   // -- v3 -- Interrupt Group Modifier Registers
+            NSACR       = 0x0e00,   // v2 v3 rw Non-Secure Access Control Registers
+            CPENDSGIR   = 0x0f10,   // v2 -- rw SGI Clr-Pending Registers
+            SPENDSGIR   = 0x0f20,   // v2 -- rw SGI Set-Pending Registers
+            IGROUPRE    = 0x1000,   // -- v3 rw Extended Interrupt Group Registers
+            ISENABLERE  = 0x1200,   // -- v3 rw Extended Interrupt Set-Enable Registers
+            ICENABLERE  = 0x1400,   // -- v3 rw Extended Interrupt Clr-Enable Registers
+            ISPENDRE    = 0x1600,   // -- v3 rw Extended Interrupt Set-Pending Registers
+            ICPENDRE    = 0x1800,   // -- v3 rw Extended Interrupt Clr-Pending Registers
+            ISACTIVERE  = 0x1a00,   // -- v3 rw Extended Interrupt Set-Active Registers
+            ICACTIVERE  = 0x1c00,   // -- v3 rw Extended Interrupt Clr-Active Registers
+            IPRIORITYRE = 0x2000,   // -- v3 rw Extended Interrupt Priority Registers
+            ICFGRE      = 0x3000,   // -- v3 rw Extended Interrupt Configuration Registers
+            IGRPMODRE   = 0x3400,   // -- v3 rw Extended Interrupt Group Modifier Registers
+            NSACRE      = 0x3600,   // -- v3 rw Extended Non-Secure Access Control Registers
+        };
+
+        enum class Array64
+        {
+            IROUTER     = 0x6000,   // -- v3 rw Interrupt Routing Registers
+            IROUTERE    = 0x8000,   // -- v3 rw Extended Interrupt Routing Registers
+        };
+
+        static inline uint64    phys    { Board::gic[0].mmio };
+        static inline Spinlock  lock;
+
+        static inline auto read  (Register32 r)             { return *reinterpret_cast<uint32 volatile *>(DEV_GLOBL_GICD + static_cast<unsigned>(r)); }
+        static inline auto read  (Array32 r, unsigned n)    { return *reinterpret_cast<uint32 volatile *>(DEV_GLOBL_GICD + static_cast<unsigned>(r) + n * sizeof (uint32)); }
+        static inline auto read  (Array64 r, unsigned n)    { return *reinterpret_cast<uint64 volatile *>(DEV_GLOBL_GICD + static_cast<unsigned>(r) + n * sizeof (uint64)); }
+
+        static inline void write (Register32 r,          uint32 v) { *reinterpret_cast<uint32 volatile *>(DEV_GLOBL_GICD + static_cast<unsigned>(r)) = v; }
+        static inline void write (Array32 r, unsigned n, uint32 v) { *reinterpret_cast<uint32 volatile *>(DEV_GLOBL_GICD + static_cast<unsigned>(r) + n * sizeof (uint32)) = v; }
+        static inline void write (Array64 r, unsigned n, uint64 v) { *reinterpret_cast<uint64 volatile *>(DEV_GLOBL_GICD + static_cast<unsigned>(r) + n * sizeof (uint64)) = v; }
+
+        static bool mmap_mmio();
+        static void init_mmio();
+        static void wait_rwp();
+
+    public:
+        static inline unsigned arch     { 0 };
+        static inline unsigned ints     { 0 };
+        static inline Group group       { GROUP0 };
+
+        static void init();
+
+        static void conf (unsigned, bool, unsigned = 0);
+        static void mask (unsigned, bool);
+
+        static void send_sgi (unsigned, unsigned);
+};
---
+++ src/aarch64/gicd.cpp
@@ -0,0 +1,166 @@
+/*
+ * Generic Interrupt Controller: Distributor (GICD)
+ *
+ * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "assert.hpp"
+#include "barrier.hpp"
+#include "bits.hpp"
+#include "gicd.hpp"
+#include "lock_guard.hpp"
+#include "lowlevel.hpp"
+#include "ptab_hpt.hpp"
+#include "stdio.hpp"
+
+void Gicd::init()
+{
+    if (EXPECT_FALSE (!phys))
+        return;
+
+    if (Cpu::bsp && !mmap_mmio())
+        return;
+
+    init_mmio();
+}
+
+bool Gicd::mmap_mmio()
+{
+    for (size_t size = PAGE_SIZE; size <= PAGE_SIZE << 4; size <<= 4) {
+
+        Hptp::master.update (DEV_GLOBL_GICD, phys, bit_scan_reverse (size) - PAGE_BITS,
+                             Paging::Permissions (Paging::G | Paging::W | Paging::R),
+                             Memattr::Cacheability::DEV, Memattr::Shareability::NONE);
+
+        auto pidr = Coresight::read (Coresight::Component::PIDR2, DEV_GLOBL_GICD + size);
+
+        if (pidr) {
+
+            auto iidr  = read (Register32::IIDR);
+            auto typer = read (Register32::TYPER);
+            arch       = pidr >> 4 & BIT_RANGE (3, 0);
+            ints       = 32 * ((typer & BIT_RANGE (4, 0)) + 1);
+            group      = arch >= 3 || typer & BIT (10) ? GROUP1 : GROUP0;
+
+            trace (TRACE_INTR, "GICD: %#010llx v%u r%up%u Impl:%#x Prod:%#x ESPI:%u LPIS:%u INT:%u S:%u G:%u",
+                   phys, arch, iidr >> 16 & BIT_RANGE (3, 0), iidr >> 12 & BIT_RANGE (3, 0), iidr & BIT_RANGE (11, 0), iidr >> 24,
+                   arch >= 3 ? !!(typer & BIT (8)) : 0, arch >= 3 ? !!(typer & BIT (17)) : 0, ints, !!(typer & BIT (10)), group & BIT (0));
+
+            return true;
+        }
+    }
+
+    trace (TRACE_INTR, "GICD: MMIO unavailable. This is BAD!");
+
+    return false;
+}
+
+void Gicd::init_mmio()
+{
+    auto itl = Cpu::bsp ? ints / 32 : 1;
+
+    // Disable interrupt forwarding
+    write (Register32::CTLR, 0);
+
+    // BSP initializes all, APs SGI/PPI bank only. v3+ skips SGI/PPI bank
+    for (unsigned i = arch < 3 ? 0 : 1; i < itl * 1; i++) {
+
+        // Disable all interrupts
+        write (Array32::ICENABLER, i, BIT_RANGE (31, 0));
+
+        // Assign interrupt groups
+        write (Array32::IGROUPR, i, group);
+    }
+
+    // BSP initializes all, APs SGI/PPI bank only. v3+ skips SGI/PPI bank
+    for (unsigned i = arch < 3 ? 0 : 8; i < itl * 8; i++) {
+
+        // Assign interrupt priorities
+        write (Array32::IPRIORITYR, i, 0);
+    }
+
+    // Wait for completion on CTLR and ICENABLER
+    wait_rwp();
+
+    // Enable all SGIs
+    if (arch < 3) {
+        write (Array32::ISENABLER, 0, BIT_RANGE (15, 0));
+        trace (TRACE_INTR, "GICD: Available SGIs: %#x", read (Array32::ISENABLER, 0));
+    }
+
+    // Enable interrupt forwarding
+    write (Register32::CTLR, arch < 3 ? BIT (0) : BIT (4) | BIT (1));
+}
+
+void Gicd::conf (unsigned i, bool lvl, unsigned cpu)
+{
+    assert (i >= BASE_SPI || arch < 3);
+
+    if (i >= ints)
+        return;
+
+    Lock_guard <Spinlock> guard (lock);
+
+    // Configure trigger mode
+    auto v = read (Array32::ICFGR, i / 16);
+    v &= ~(BIT_RANGE (1, 0) << 2 * (i % 16));
+    v |= static_cast<uint32>(!lvl) << (2 * (i % 16) + 1);
+    write (Array32::ICFGR, i / 16, v);
+
+    // SGI/PPI CPU targets are read-only
+    if (i < BASE_SPI)
+        return;
+
+    // Configure SPI CPU target
+    if (arch < 3) {
+        auto t = read (Array32::ITARGETSR, i / 4);
+        t &= ~(BIT_RANGE (7, 0) << 8 * (i % 4));
+        t |= BIT (cpu) << 8 * (i % 4);
+        write (Array32::ITARGETSR, i / 4, t);
+    } else {
+        uint64 affinity = Cpu::remote_affinity (cpu);
+        write (Array64::IROUTER, i, (affinity & BIT_RANGE (31, 24)) << 8 | (affinity & BIT_RANGE (23, 0)));
+    }
+}
+
+void Gicd::mask (unsigned i, bool masked)
+{
+    assert (i >= BASE_SPI || arch < 3);
+
+    if (i >= ints)
+        return;
+
+    if (masked) {
+        write (Array32::ICENABLER, i / 32, BIT (i % 32));
+        wait_rwp();
+    } else
+        write (Array32::ISENABLER, i / 32, BIT (i % 32));
+}
+
+void Gicd::send_sgi (unsigned cpu, unsigned sgi)
+{
+    assert (sgi < NUM_SGI && cpu < 8 && arch < 3);
+
+    // Ensure completion (global observability) of all previous stores
+    Barrier::wmb_sync();
+
+    write (Register32::SGIR, BIT (cpu + 16) | sgi);
+}
+
+void Gicd::wait_rwp()
+{
+    if (arch >= 3)
+        while (read (Register32::CTLR) & BIT (31))
+            pause();
+}
