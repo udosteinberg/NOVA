eee34583fa3f00aa5eae55a1e4a5dda52e6a6605
Udo Steinberg <udo@hypervisor.org>
2019-08-03 10:49:50 +0200

    ARM: Added GICD (Distributor).

--- src/aarch64/cpu.cpp
+++ src/aarch64/cpu.cpp
@@ -19,6 +19,7 @@
 #include "cache.hpp"
 #include "cpu.hpp"
 #include "fpu.hpp"
+#include "gicd.hpp"
 #include "ptab_hpt.hpp"
 #include "ptab_npt.hpp"
 #include "stdio.hpp"
@@ -259,6 +260,8 @@ void Cpu::init (cpu_t cpu, unsigned e)
            mpidr >> 32 & BIT_RANGE (7, 0), mpidr >> 16 & BIT_RANGE (7, 0), mpidr >> 8 & BIT_RANGE (7, 0), mpidr & BIT_RANGE (7, 0),
            impl, part, midr >> 20 & BIT_RANGE (3, 0), midr & BIT_RANGE (3, 0),
            feature (Mem_feature::PARANGE), feature (Mem_feature::XNX), feature (Cpu_feature::GIC), e);
+
+    Gicd::init();
 
     Nptp::init();
 
