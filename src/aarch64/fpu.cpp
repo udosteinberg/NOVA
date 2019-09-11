8c07e6645f1794cedc3f7fdb004bf8cbe8f64119
Udo Steinberg <udo@hypervisor.org>
2019-09-11 16:56:35 +0200

    ARM: Added FPU class.

---
+++ src/aarch64/fpu.cpp
@@ -0,0 +1,22 @@
+/*
+ * Floating Point Unit (FPU)
+ *
+ * Copyright (C) 2019-2024 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "fpu.hpp"
+
+void Fpu::fini()
+{
+}
