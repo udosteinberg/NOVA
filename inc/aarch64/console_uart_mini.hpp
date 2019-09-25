6b1d01ec0b093f3f7885df5f776ba90c09e625b4
Udo Steinberg <udo@hypervisor.org>
2019-09-25 16:24:31 +0200

    ARM: Added UART console (MINI).

---
+++ inc/aarch64/console_uart_mini.hpp
@@ -0,0 +1,56 @@
+/*
+ * Console: Mini UART
+ *
+ * Copyright (C) 2019-2022 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "console_uart.hpp"
+
+class Console_uart_mini final : private Console_uart
+{
+    private:
+        static Console_uart_mini singleton;
+
+        enum class Register32 : unsigned
+        {
+            AUXENB      = 0x04,     // Auxiliary Enables
+            MU_IO       = 0x40,     // Mini UART I/O Data
+            MU_IER      = 0x44,     // Mini UART Interrupt Enable
+            MU_LCR      = 0x4c,     // Mini UART Line Control
+            MU_CTRL     = 0x60,     // Mini UART Extra Control
+            MU_STAT     = 0x64,     // Mini UART Extra Status
+            MU_BAUD     = 0x68,     // Mini UART Baudrate
+        };
+
+        auto read  (Register32 r) const    { return *reinterpret_cast<uint32 volatile *>(mmap + std::to_underlying (r)); }
+        void write (Register32 r, uint32 v) const { *reinterpret_cast<uint32 volatile *>(mmap + std::to_underlying (r)) = v; }
+
+        bool tx_busy() const override final { return !(read (Register32::MU_STAT) & BIT (9)); }
+        bool tx_full() const override final { return  (read (Register32::MU_STAT) & BIT (5)); }
+        void tx (uint8 c) const override final { write (Register32::MU_IO, c); }
+
+        void init() const override final
+        {
+            write (Register32::AUXENB, BIT (0));
+            write (Register32::MU_LCR, BIT_RANGE (1, 0));
+            write (Register32::MU_IER, BIT_RANGE (2, 1));
+            write (Register32::MU_BAUD, clock / baudrate / 8 - 1);
+            write (Register32::MU_CTRL, BIT (1));
+        }
+
+    protected:
+        explicit Console_uart_mini (Hpt::OAddr p, unsigned c) : Console_uart (c) { setup_regs (Acpi_gas::Asid::MMIO, p); }
+};
