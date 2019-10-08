0bfc8f95a26e71ecffa95d5d9bc094c29a56dc02
Udo Steinberg <udo@hypervisor.org>
2019-10-08 19:11:06 +0200

    ARM: Added SMMU.

--- src/aarch64/bootstrap.cpp
+++ src/aarch64/bootstrap.cpp
@@ -18,11 +18,16 @@
 #include "compiler.hpp"
 #include "cpu.hpp"
 #include "lowlevel.hpp"
+#include "smmu.hpp"
 
 extern "C" [[noreturn]]
 void bootstrap (cpu_t c, unsigned e)
 {
     Cpu::init (c, e);
+
+    // Before cores leave the barrier into userland, the SMMU must be active
+    if (Cpu::bsp)
+        Smmu::initialize();
 
     // Barrier: wait for all CPUs to arrive here
     for (Cpu::online++; Cpu::online != Cpu::count; pause()) ;
--- src/aarch64/hip_arch.cpp
+++ src/aarch64/hip_arch.cpp
@@ -16,7 +16,14 @@
  */
 
 #include "hip_arch.hpp"
+#include "smmu.hpp"
+#include "stdio.hpp"
 
 void Hip_arch::build()
 {
+    num_smg = Smmu::avail_smg();
+    num_ctx = Smmu::avail_ctx();
+
+    trace (TRACE_ROOT, "INFO: SMG#: %5u", num_smg);
+    trace (TRACE_ROOT, "INFO: CTX#: %5u", num_ctx);
 }
