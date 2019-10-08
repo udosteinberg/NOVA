4bd12b4cfffc60a346555dd0217daf0167b9519d
Udo Steinberg <udo@hypervisor.org>
2019-10-08 19:11:06 +0200

    ARM: Added SMMU.

--- src/aarch64/cpu.cpp
+++ src/aarch64/cpu.cpp
@@ -23,6 +23,7 @@
 #include "gich.hpp"
 #include "gicr.hpp"
 #include "hazards.hpp"
+#include "smmu.hpp"
 #include "stdio.hpp"
 #include "timer.hpp"
 #include "vmcb.hpp"
@@ -258,6 +259,9 @@ void Cpu::init (unsigned cpu, unsigned e)
     Gicr::init();
     Gicc::init();
     Gich::init();
+
+    if (bsp)
+        Smmu::initialize();
 
     Timer::init();
 
