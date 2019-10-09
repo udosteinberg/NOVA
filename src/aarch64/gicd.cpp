3d02a1798c49b8f3297b38d69088a28f54da1a0d
Udo Steinberg <udo@hypervisor.org>
2019-10-09 14:02:17 +0200

    ARM: Reserve kernel MMIO regions.

--- src/aarch64/gicd.cpp
+++ src/aarch64/gicd.cpp
@@ -21,6 +21,7 @@
 #include "gicd.hpp"
 #include "lock_guard.hpp"
 #include "lowlevel.hpp"
+#include "pd_kern.hpp"
 #include "ptab_hpt.hpp"
 #include "stdio.hpp"
 
@@ -56,6 +57,9 @@ bool Gicd::mmap_mmio()
             trace (TRACE_INTR, "GICD: %#010llx v%u r%up%u Impl:%#x Prod:%#x ESPI:%u LPIS:%u INT:%u S:%u G:%u",
                    phys, arch, iidr >> 16 & BIT_RANGE (3, 0), iidr >> 12 & BIT_RANGE (3, 0), iidr & BIT_RANGE (11, 0), iidr >> 24,
                    arch >= 3 ? !!(typer & BIT (8)) : 0, arch >= 3 ? !!(typer & BIT (17)) : 0, ints, !!(typer & BIT (10)), group & BIT (0));
+
+            // Reserve MMIO region
+            Pd_kern::remove_user_mem (phys, size);
 
             return true;
         }
