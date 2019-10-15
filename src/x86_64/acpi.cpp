f21179fab713d8f20fb02208f1499d0ceb20a737
Udo Steinberg <udo@hypervisor.org>
2019-10-15 11:40:01 +0200

    x86: Modernized console infrastructure.

--- src/x86_64/acpi.cpp
+++ src/x86_64/acpi.cpp
@@ -160,7 +160,7 @@ unsigned Acpi::hw_read (Acpi_gas *gas)
         }
     }
 
-    Console::panic ("Unimplemented ASID %d", gas->asid);
+    panic ("Unimplemented ASID %d", gas->asid);
 }
 
 void Acpi::hw_write (Acpi_gas *gas, unsigned val)
@@ -182,7 +182,7 @@ void Acpi::hw_write (Acpi_gas *gas, unsigned val)
         }
     }
 
-    Console::panic ("Unimplemented ASID %d", gas->asid);
+    panic ("Unimplemented ASID %d", gas->asid);
 }
 
 void Acpi::interrupt()
