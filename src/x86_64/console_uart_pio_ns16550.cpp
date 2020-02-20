2e7690c4ec0eca9adb5030296cc55bf1807fb418
Udo Steinberg <udo@hypervisor.org>
2020-02-20 15:59:48 +0100

    x86: Added UART console (NS16550).

---
+++ src/x86_64/console_uart_pio_ns16550.cpp
@@ -0,0 +1,34 @@
+/*
+ * Console: NS16550 UART
+ *
+ * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "cmdline.hpp"
+#include "console_uart_pio_ns16550.hpp"
+#include "hpt.hpp"
+
+INIT_PRIORITY (PRIO_CONSOLE) Console_uart_pio_ns16550 Console_uart_pio_ns16550::singleton (115200);
+
+unsigned Console_uart_pio_ns16550::probe()
+{
+    if (!Cmdline::serial)
+        return 0;
+
+    return *reinterpret_cast<uint16 *>(Hpt::remap (0x400));
+}
