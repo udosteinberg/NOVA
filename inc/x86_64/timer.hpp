b036b9f6f36f7e67e5a4a9de1b28a9a26d0afba6
Udo Steinberg <udo@hypervisor.org>
2020-02-20 12:18:26 +0100

    x86: Added Timer class.

---
+++ inc/x86_64/timer.hpp
@@ -0,0 +1,42 @@
+/*
+ * Timer: Architecture-Specific (x86)
+ *
+ * Copyright (C) 2019-2024 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "lapic.hpp"
+#include "stc.hpp"
+
+class Timer final : private Stc
+{
+    public:
+        static auto time()
+        {
+            return Lapic::time();
+        }
+
+        static void set_dln (uint64_t ticks)
+        {
+            Lapic::set_timer (ticks);
+        }
+
+        static void stop() {}
+
+        static void set_time (uint64_t t)
+        {
+            Msr::write (Msr::Register::IA32_TSC, t);
+        }
+};
