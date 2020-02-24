e32344b8c84c238ce113b353a83f9d5f1603babc
Udo Steinberg <udo@hypervisor.org>
2020-02-24 14:33:04 +0100

    x86: Use 64bit values for MSR access.

--- src/x86_64/bootstrap.cpp
+++ src/x86_64/bootstrap.cpp
@@ -37,7 +37,7 @@ void bootstrap()
     // Barrier: wait for all ECs to arrive here
     for (Atomic::add (barrier, 1UL); barrier != Cpu::online; pause()) ;
 
-    Msr::write<uint64>(Msr::IA32_TSC, 0);
+    Msr::write (Msr::IA32_TSC, 0);
 
     // Create root task
     if (Cpu::bsp) {
