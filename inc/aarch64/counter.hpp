733e248f4fac730e358c6c46b1895831631fed87
Udo Steinberg <udo@hypervisor.org>
2020-03-03 10:42:26 +0100

    ARM: Added Counter class.

---
+++ inc/aarch64/counter.hpp
@@ -0,0 +1,48 @@
+/*
+ * Event Counters
+ *
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "atomic.hpp"
+#include "compiler.hpp"
+#include "intid.hpp"
+#include "kmem.hpp"
+#include "types.hpp"
+
+class Counter final
+{
+    private:
+        Atomic<unsigned> val { 0 };
+
+    public:
+        static Counter req[Intid::NUM_SGI]  CPULOCAL;
+        static Counter loc[Intid::NUM_PPI]  CPULOCAL;
+        static Counter schedule             CPULOCAL;
+        static Counter helping              CPULOCAL;
+
+        ALWAYS_INLINE
+        inline void inc()
+        {
+            val = val + 1;
+        }
+
+        ALWAYS_INLINE
+        inline unsigned get (cpu_t cpu) const
+        {
+            return *Kmem::loc_to_glob (cpu, &val);
+        }
+};
