3a74ba115859aee4422124e494125e3802079fee
Udo Steinberg <udo@hypervisor.org>
2020-04-27 18:57:20 +0200

    ARM: Added ACPI class.

---
+++ inc/aarch64/acpi.hpp
@@ -0,0 +1,26 @@
+/*
+ * Advanced Configuration and Power Interface (ACPI)
+ *
+ * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "types.hpp"
+
+class Acpi
+{
+    public:
+        static uint64 const rsdp = ~0ULL;
+};
