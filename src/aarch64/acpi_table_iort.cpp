99aa06c8fb2bab94390d542d65e4804d03b7cb4b
Udo Steinberg <udo@hypervisor.org>
2020-04-27 18:57:20 +0200

    ARM: Added ACPI classes.

---
+++ src/aarch64/acpi_table_iort.cpp
@@ -0,0 +1,30 @@
+/*
+ * Advanced Configuration and Power Interface (ACPI)
+ *
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "acpi_table_iort.hpp"
+
+void Acpi_table_iort::parse() const
+{
+    auto const addr { reinterpret_cast<uintptr_t>(this) };
+
+    for (auto a { addr + node_ofs }; a < addr + table.header.length; ) {
+
+        auto const n { reinterpret_cast<Node const *>(a) };
+
+        a += n->length;
+    }
+}
