bcea6b18d5739d02fbe61dad9fbb8e9e761a9a56
Udo Steinberg <udo@hypervisor.org>
2020-04-29 14:18:39 +0200

    GEN: Added type definitions.

--- inc/types.hpp
+++ inc/types.hpp
@@ -1,8 +1,10 @@
 /*
- * Constant-Width Types
+ * Type Definitions
  *
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -18,12 +20,34 @@
 
 #pragma once
 
-#include <stddef.h>
+using uint8     = unsigned char;
+using uint16    = unsigned short;
+using uint32    = unsigned int;
+using uint64    = unsigned long long;
 
-typedef unsigned char       uint8;
-typedef unsigned short      uint16;
-typedef unsigned int        uint32;
-typedef unsigned long long  uint64;
+using mword     = unsigned long;
+using Paddr     = unsigned long;
 
-typedef unsigned long       mword;
-typedef unsigned long       Paddr;
+// Signed Integer Types
+using int32_t   = __INT32_TYPE__;
+using int64_t   = __INT64_TYPE__;
+
+// Unsigned Integer Types
+using uint8_t   = __UINT8_TYPE__;
+using uint16_t  = __UINT16_TYPE__;
+using uint32_t  = __UINT32_TYPE__;
+using uint64_t  = __UINT64_TYPE__;
+using uint128_t = __uint128_t;
+
+// Pointer Types
+using uintptr_t = __UINTPTR_TYPE__;
+
+// Size Types
+using size_t    = __SIZE_TYPE__;
+
+// NOVA Types
+using apic_t    = uint32_t;
+using pci_t     = uint32_t;
+using cos_t     = uint16_t;
+using cpu_t     = uint16_t;
+using port_t    = uint16_t;
