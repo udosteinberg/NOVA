732f05bddd66d8eced61b9b4787efe7ad55d9d5e
Udo Steinberg <udo@hypervisor.org>
2020-05-07 10:26:40 +0200

    GEN: Added status codes.

---
+++ inc/status.hpp
@@ -0,0 +1,38 @@
+/*
+ * Hypercall Status Codes
+ *
+ * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+enum class Status : unsigned
+{
+    SUCCESS,
+    TIMEOUT,
+    ABORTED,
+    OVRFLOW,
+    BAD_HYP,
+    BAD_CAP,
+    BAD_PAR,
+    BAD_FTR,
+    BAD_CPU,
+    BAD_DEV,
+    MEM_OBJ,
+    MEM_CAP,
+};
