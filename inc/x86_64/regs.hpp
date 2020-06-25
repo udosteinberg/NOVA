42490fdbfce67547dd224483a7160b541da9010c
Udo Steinberg <udo@hypervisor.org>
2020-06-25 10:35:25 +0200

    x86: Removed x86_32 support.

--- inc/x86_64/regs.hpp
+++ inc/x86_64/regs.hpp
@@ -24,269 +24,147 @@
 #include "arch.hpp"
 #include "atomic.hpp"
 #include "hazards.hpp"
+#include "macros.hpp"
+#include "selectors.hpp"
 #include "svm.hpp"
 #include "types.hpp"
 #include "vmx.hpp"
 
-class Sys_regs
+struct Sys_regs
 {
-    public:
+    uintptr_t   rax {};
+    uintptr_t   rcx {};
+    uintptr_t   rdx {};
+    uintptr_t   rbx {};
+    uintptr_t   rbp {};
+    uintptr_t   rsi {};
+    uintptr_t   rdi {};
+    uintptr_t   r8  {};
+    uintptr_t   r9  {};
+    uintptr_t   r10 {};
+    uintptr_t   r11 {};
+    uintptr_t   r12 {};
+    uintptr_t   r13 {};
+    uintptr_t   r14 {};
+    uintptr_t   r15 {};
+
+    unsigned flags() const { return ARG_1 >> 4 & BIT_RANGE (3, 0); }
+
+    void set_pt (uintptr_t pt) { ARG_1 = pt; }
+    void set_ip (uintptr_t ip) { ARG_IP = ip; }
+    void set_sp (uintptr_t sp) { ARG_SP = sp; }
+};
+
+static_assert (__is_standard_layout (Sys_regs) && sizeof (Sys_regs) == __SIZEOF_POINTER__ * 15);
+
+struct Exc_regs
+{
+    Sys_regs            sys;
+
+    union {
         struct {
-#ifdef __x86_64__
-            mword   r15     { 0 };
-            mword   r14     { 0 };
-            mword   r13     { 0 };
-            mword   r12     { 0 };
-            mword   r11     { 0 };
-            mword   r10     { 0 };
-            mword   r9      { 0 };
-            mword   r8      { 0 };
-#endif
-            mword   REG(di) { 0 };
-            mword   REG(si) { 0 };
-            mword   REG(bp) { 0 };
-            mword   cr2     { 0 };
-            mword   REG(bx) { 0 };
-            mword   REG(dx) { 0 };
-            mword   REG(cx) { 0 };
-            mword   REG(ax) { 0 };
+            uintptr_t   err {};
+            uintptr_t   vec {};
+            uintptr_t   rip {};
+            uintptr_t   cs  { SEL_USER_CODE };
+            uintptr_t   rfl { Cpu::EFL_AC | Cpu::EFL_IF | Cpu::EFL_1 };
+            uintptr_t   rsp {};
+            uintptr_t   ss  { SEL_USER_DATA };
         };
-
-        enum Status
-        {
-            SUCCESS,
-            COM_TIM,
-            COM_ABT,
-            BAD_HYP,
-            BAD_CAP,
-            BAD_PAR,
-            BAD_FTR,
-            BAD_CPU,
-            BAD_DEV,
+        struct {
+            uint64_t    offset_tsc;
+            uintptr_t   reserved;
+            uintptr_t   shadow_cr0;
+            uintptr_t   shadow_cr4;
+            uintptr_t   intcpt_cr0;
+            uintptr_t   intcpt_cr4;
+            uint32_t    intcpt_exc;
+            bool        fpu_on;
         };
+    };
 
-        ALWAYS_INLINE
-        inline unsigned flags() const { return ARG_1 >> 4 & 0xf; }
+    auto &ip() { return sys.rcx; }
+    auto &sp() { return sys.r11; }
 
-        ALWAYS_INLINE
-        inline void set_status (Status status) { ARG_1 = status; }
+    // CPL0/1/2 (supervisor) CPL3 (user)
+    bool user() const { return (cs & 3) == 3; }
 
-        ALWAYS_INLINE
-        inline void set_pt (mword pt) { ARG_1 = pt; }
+    auto ep() const { return vec; }
 
-        ALWAYS_INLINE
-        inline void set_ip (mword ip) { ARG_IP = ip; }
-
-        ALWAYS_INLINE
-        inline void set_sp (mword sp) { ARG_SP = sp; }
+    void set_ep (uintptr_t val) { vec = val; }
 };
 
-class Exc_regs : public Sys_regs
+static_assert (__is_standard_layout (Exc_regs) && sizeof (Exc_regs) == __SIZEOF_POINTER__ * 22);
+
+class alignas (16) Cpu_regs final
 {
+    private:
+        template<typename T> uintptr_t set_cr0() const { return T::fix_cr0_set | !exc.fpu_on * Cpu::CR0_TS; }
+        template<typename T> uintptr_t set_cr4() const { return T::fix_cr4_set; }
+        template<typename T> uintptr_t msk_cr0() const { return T::fix_cr0_clr | set_cr0<T>(); }
+        template<typename T> uintptr_t msk_cr4() const { return T::fix_cr4_clr | set_cr4<T>(); }
+
+        auto set_exc() const { return BIT (EXC_AC) | !exc.fpu_on * BIT (EXC_NM); }
+
     public:
+        Exc_regs                exc;
+        uintptr_t               cr2 {};
         union {
-            struct {
-                mword   gs;
-                mword   fs;
-                mword   es;
-                mword   ds;
-                mword   err;
-                mword   vec;
-                mword   REG(ip);
-                mword   cs;
-                mword   REG(fl);
-                mword   REG(sp);
-                mword   ss;
-            };
-            struct {
-                union {
-                    Vmcs *  vmcs;
-                    Vmcb *  vmcb;
-                };
-                mword   reserved1;
-                mword   reserved2;
-                mword   cr0_shadow;
-                mword   cr4_shadow;
-                mword   dst_portal;
-                uint8   fpu_on;
-            };
+            Vmcb *              vmcb;
+            Vmcs *              vmcs;
         };
+        uintptr_t               hzd;
+        uintptr_t               mtd;
+        uint64_t                tsc_offset;
 
-    private:
-        template <typename T> ALWAYS_INLINE inline mword get_g_cr0() const;
-        template <typename T> ALWAYS_INLINE inline mword get_g_cr2() const;
-        template <typename T> ALWAYS_INLINE inline mword get_g_cr3() const;
-        template <typename T> ALWAYS_INLINE inline mword get_g_cr4() const;
-        template <typename T> ALWAYS_INLINE inline void  set_g_cr0 (mword) const;
-        template <typename T> ALWAYS_INLINE inline void  set_g_cr2 (mword);
-        template <typename T> ALWAYS_INLINE inline void  set_g_cr3 (mword) const;
-        template <typename T> ALWAYS_INLINE inline void  set_g_cr4 (mword) const;
-        template <typename T> ALWAYS_INLINE inline void  set_s_cr0 (mword);
-        template <typename T> ALWAYS_INLINE inline void  set_s_cr4 (mword);
-        template <typename T> ALWAYS_INLINE inline void  set_e_bmp (uint32) const;
+        uintptr_t hazard() const { return hzd; }
 
-    protected:
-        template <typename T> ALWAYS_INLINE inline uint64 get_g_efer() const;
+        void set_hazard (uintptr_t h) { Atomic::set_mask (hzd, h); }
+        void clr_hazard (uintptr_t h) { Atomic::clr_mask (hzd, h); }
 
-        template <typename T>
-        mword cr0_set() const
-        {
-            mword set = 0;
-
-            if (!fpu_on)
-                set |= Cpu::CR0_TS;
-
-            return T::fix_cr0_set | set;
-        }
-
-        template <typename T>
-        mword cr0_msk() const
-        {
-            return T::fix_cr0_clr | cr0_set<T>();
-        }
-
-        template <typename T>
-        mword cr4_set() const
-        {
-            mword set = 0;
-
-            return T::fix_cr4_set | set;
-        }
-
-        template <typename T>
-        mword cr4_msk() const
-        {
-            return T::fix_cr4_clr | cr4_set<T>();
-        }
-
-    public:
-        ALWAYS_INLINE
-        inline bool user() const { return cs & 3; }
-
-        void fpu_ctrl (bool);
-
-        template <typename T> void set_cpu_ctrl0 (uint32);
-        template <typename T> void set_cpu_ctrl1 (uint32);
-
-        template <typename T> void nst_ctrl();
-
-        template <typename T>
-        mword get_cr0() const
-        {
-            mword msk = cr0_msk<T>();
-            return (get_g_cr0<T>() & ~msk) | (cr0_shadow & msk);
-        }
-
-        template <typename T>
-        mword get_cr2() const
-        {
-            return get_g_cr2<T>();
-        }
-
-        template <typename T>
-        mword get_cr3() const
-        {
-            return get_g_cr3<T>();
-        }
-
-        template <typename T>
-        mword get_cr4() const
-        {
-            mword msk = cr4_msk<T>();
-            return (get_g_cr4<T>() & ~msk) | (cr4_shadow & msk);
-        }
-
-        template <typename T>
-        void set_cr0 (mword v)
-        {
-            set_g_cr0<T> ((v & (~cr0_msk<T>() | Cpu::CR0_PE)) | (cr0_set<T>() & ~Cpu::CR0_PE));
-            set_s_cr0<T> (v);
-        }
-
-        template <typename T>
-        void set_cr2 (mword v)
-        {
-            set_g_cr2<T> (v);
-        }
-
-        template <typename T>
-        void set_cr3 (mword v)
-        {
-            set_g_cr3<T> (v);
-        }
-
-        template <typename T>
-        void set_cr4 (mword v)
-        {
-            set_g_cr4<T> ((v & ~cr4_msk<T>()) | cr4_set<T>());
-            set_s_cr4<T> (v);
-        }
-
-        template <typename T>
-        void set_exc()
-        {
-            uint32 val = 1U << Cpu::EXC_AC;
-
-            if (!fpu_on)
-                val |= 1U << Cpu::EXC_NM;
-
-            set_e_bmp<T> (val);
-        }
-
-        template <typename T> void write_efer (uint64);
-};
-
-class Cpu_regs : public Exc_regs
-{
-    private:
-        mword   hzd;
-
-    public:
-        uint64  tsc_offset;
-        mword   mtd;
-
-        ALWAYS_INLINE
-        inline mword hazard() const { return hzd; }
-
-        ALWAYS_INLINE
-        inline void set_hazard (mword h) { Atomic::set_mask (hzd, h); }
-
-        ALWAYS_INLINE
-        inline void clr_hazard (mword h) { Atomic::clr_mask (hzd, h); }
-
-        ALWAYS_INLINE
-        inline void add_tsc_offset (uint64 tsc)
+        void add_tsc_offset (uint64_t tsc)
         {
             tsc_offset += tsc;
             set_hazard (HZD_TSC);
         }
+
+        void fpu_ctrl (bool);
+        void svm_set_cpu_pri (uint32_t) const;
+        void svm_set_cpu_sec (uint32_t) const;
+        void vmx_set_cpu_pri (uint32_t) const;
+        void vmx_set_cpu_sec (uint32_t) const;
+        void vmx_set_cpu_ter (uint64_t) const;
+
+        void svm_set_bmp_exc() const { vmcb->intercept_exc = set_exc() | exc.intcpt_exc; }
+        void vmx_set_bmp_exc() const { Vmcs::write (Vmcs::Encoding::EXC_BITMAP, set_exc() | exc.intcpt_exc); }
+
+        void vmx_set_msk_cr0() const { Vmcs::write (Vmcs::Encoding::CR0_MASK, msk_cr0<Vmcs>() | exc.intcpt_cr0); }
+        void vmx_set_msk_cr4() const { Vmcs::write (Vmcs::Encoding::CR4_MASK, msk_cr4<Vmcs>() | exc.intcpt_cr4); }
+
+        void vmx_set_rsh_cr0 (uintptr_t v) { Vmcs::write (Vmcs::Encoding::CR0_READ_SHADOW, exc.shadow_cr0 = v); }
+        void vmx_set_rsh_cr4 (uintptr_t v) { Vmcs::write (Vmcs::Encoding::CR4_READ_SHADOW, exc.shadow_cr4 = v); }
+
+        void vmx_set_gst_cr0 (uintptr_t v)
+        {
+            vmx_set_rsh_cr0 (v);
+            Vmcs::write (Vmcs::Encoding::GUEST_CR0, (v & ~msk_cr0<Vmcs>()) | set_cr0<Vmcs>());
+        }
+
+        void vmx_set_gst_cr4 (uintptr_t v)
+        {
+            vmx_set_rsh_cr4 (v);
+            Vmcs::write (Vmcs::Encoding::GUEST_CR4, (v & ~msk_cr4<Vmcs>()) | set_cr4<Vmcs>());
+        }
+
+        auto vmx_get_gst_cr0() const
+        {
+            auto const msk { msk_cr0<Vmcs>() };
+            return (Vmcs::read<uintptr_t> (Vmcs::Encoding::GUEST_CR0) & ~msk) | (exc.shadow_cr0 & msk);
+        }
+
+        auto vmx_get_gst_cr4() const
+        {
+            auto const msk { msk_cr4<Vmcs>() };
+            return (Vmcs::read<uintptr_t> (Vmcs::Encoding::GUEST_CR4) & ~msk) | (exc.shadow_cr4 & msk);
+        }
 };
-
-template <> inline uint64 Exc_regs::get_g_efer<Vmcb>()         const { return vmcb->efer; }
-template <> inline uint64 Exc_regs::get_g_efer<Vmcs>()         const { return Vmcs::read<uint64> (Vmcs::GUEST_EFER); }
-
-template <> inline mword  Exc_regs::get_g_cr0<Vmcb>()          const { return static_cast<mword>(vmcb->cr0); }
-template <> inline mword  Exc_regs::get_g_cr2<Vmcb>()          const { return static_cast<mword>(vmcb->cr2); }
-template <> inline mword  Exc_regs::get_g_cr3<Vmcb>()          const { return static_cast<mword>(vmcb->cr3); }
-template <> inline mword  Exc_regs::get_g_cr4<Vmcb>()          const { return static_cast<mword>(vmcb->cr4); }
-template <> inline mword  Exc_regs::get_g_cr0<Vmcs>()          const { return Vmcs::read<mword>  (Vmcs::GUEST_CR0); }
-template <> inline mword  Exc_regs::get_g_cr2<Vmcs>()          const { return cr2; }
-template <> inline mword  Exc_regs::get_g_cr3<Vmcs>()          const { return Vmcs::read<mword>  (Vmcs::GUEST_CR3); }
-template <> inline mword  Exc_regs::get_g_cr4<Vmcs>()          const { return Vmcs::read<mword>  (Vmcs::GUEST_CR4); }
-
-template <> inline void   Exc_regs::set_g_cr0<Vmcb> (mword v)  const { vmcb->cr0 = v; }
-template <> inline void   Exc_regs::set_g_cr2<Vmcb> (mword v)        { vmcb->cr2 = v; }
-template <> inline void   Exc_regs::set_g_cr3<Vmcb> (mword v)  const { vmcb->cr3 = v; }
-template <> inline void   Exc_regs::set_g_cr4<Vmcb> (mword v)  const { vmcb->cr4 = v; }
-template <> inline void   Exc_regs::set_g_cr0<Vmcs> (mword v)  const { Vmcs::write (Vmcs::GUEST_CR0, v); }
-template <> inline void   Exc_regs::set_g_cr2<Vmcs> (mword v)        { cr2 = v; }
-template <> inline void   Exc_regs::set_g_cr3<Vmcs> (mword v)  const { Vmcs::write (Vmcs::GUEST_CR3, v); }
-template <> inline void   Exc_regs::set_g_cr4<Vmcs> (mword v)  const { Vmcs::write (Vmcs::GUEST_CR4, v); }
-
-template <> inline void   Exc_regs::set_s_cr0<Vmcb> (mword v)        { cr0_shadow = v; }
-template <> inline void   Exc_regs::set_s_cr4<Vmcb> (mword v)        { cr4_shadow = v; }
-template <> inline void   Exc_regs::set_s_cr0<Vmcs> (mword v)        { Vmcs::write (Vmcs::CR0_READ_SHADOW, cr0_shadow = v); }
-template <> inline void   Exc_regs::set_s_cr4<Vmcs> (mword v)        { Vmcs::write (Vmcs::CR4_READ_SHADOW, cr4_shadow = v); }
-
-template <> inline void   Exc_regs::set_e_bmp<Vmcb> (uint32 v) const { vmcb->intercept_exc = v; }
-template <> inline void   Exc_regs::set_e_bmp<Vmcs> (uint32 v) const { Vmcs::write (Vmcs::EXC_BITMAP, v); }
