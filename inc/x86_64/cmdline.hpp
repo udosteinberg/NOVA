eb15010bf24899dc571c6c18e62b04aa77a7d6f7
Udo Steinberg <udo@hypervisor.org>
2020-09-09 14:20:43 +0200

    x86: Modernized command line.

--- inc/x86_64/cmdline.hpp
+++ inc/x86_64/cmdline.hpp
@@ -6,6 +6,7 @@
  *
  * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
  * Copyright (C) 2014 Udo Steinberg, FireEye, Inc.
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -21,26 +22,36 @@
 
 #pragma once
 
-#include "compiler.hpp"
 #include "types.hpp"
 
-class Cmdline
+class Cmdline final
 {
-    private:
-        static struct param_map
-        {
-            char   const *arg;
-            bool * const  ptr;
-        } map[];
-
-        static char *get_arg (char **line);
-
     public:
-        static bool iommu;
-        static bool serial;
-        static bool nodl;
-        static bool nopcid;
-        static bool novpid;
+        static inline bool insecure { false };
+        static inline bool nodl     { false };
+        static inline bool nopcid   { false };
+        static inline bool nosmmu   { false };
+        static inline bool nouart   { false };
+        static inline bool novpid   { false };
 
-        static void init (mword);
+        static void init (Paddr);
+
+    private:
+        static constexpr struct
+        {
+            char const *    str;
+            bool &          var;
+        } options[]
+        {
+            { "insecure",   insecure    },
+            { "nodl",       nodl        },
+            { "nopcid",     nopcid      },
+            { "nosmmu",     nosmmu      },
+            { "nouart",     nouart      },
+            { "novpid",     novpid      },
+        };
+
+        static inline size_t arg_len (char const *&);
+
+        static void parse (char const *);
 };
