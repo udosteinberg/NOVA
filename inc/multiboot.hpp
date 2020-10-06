118e3f86f229d516ffc3486a440ad4fad42adfb2
Udo Steinberg <udo@hypervisor.org>
2020-10-06 20:44:39 +0200

    GEN: Added Multiboot definitions.

---
+++ inc/multiboot.hpp
@@ -0,0 +1,24 @@
+/*
+ * Multiboot Definitions (v1, v2)
+ *
+ * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#define MULTIBOOT_HEADER_MAGIC_V1   0x1badb002
+#define MULTIBOOT_LOADER_MAGIC_V1   0x2badb002
+
+#define MULTIBOOT_HEADER_MAGIC_V2   0xe85250d6
+#define MULTIBOOT_LOADER_MAGIC_V2   0x36d76289
