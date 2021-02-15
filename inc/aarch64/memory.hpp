0263ea878bc7d00ba0aa3f6d078f062dc4d276d9
Udo Steinberg <udo@hypervisor.org>
2021-02-15 14:15:09 +0100

    ARM: Initial functionality.

---
+++ inc/aarch64/memory.hpp
@@ -0,0 +1,29 @@
+/*
+ * Virtual-Memory Layout
+ *
+ * Copyright (C) 2019-2021 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "board.hpp"
+#include "macros.hpp"
+
+#define PAGE_BITS       12
+#define PAGE_SIZE       BITN (PAGE_BITS)
+#define OFFS_MASK       (PAGE_SIZE - 1)
+
+#define LINK_ADDR       LOAD_ADDR
+#define MMAP_CPU_DATA   0
+#define OFFSET          (LINK_ADDR - LOAD_ADDR)
