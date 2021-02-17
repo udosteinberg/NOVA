fd145c663e259beceddc282504c8cd1327fc1cf6
Udo Steinberg <udo@hypervisor.org>
2021-02-17 15:06:13 +0100

    x86: Updated Counter class.

--- src/x86_64/ec.cpp
+++ src/x86_64/ec.cpp
@@ -222,11 +222,7 @@ void Ec::idle()
         if (EXPECT_FALSE (hzd))
             handle_hazard (hzd, idle);
 
-        uint64 t1 = rdtsc();
         asm volatile ("sti; hlt; cli" : : : "memory");
-        uint64 t2 = rdtsc();
-
-        Counter::cycles_idle += t2 - t1;
     }
 }
 
