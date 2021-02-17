686320df7a57fb67845be6fb04a64034d68cd741
Udo Steinberg <udo@hypervisor.org>
2021-02-17 15:06:13 +0100

    x86: Updated Counter class.

--- inc/x86_64/ec.hpp
+++ inc/x86_64/ec.hpp
@@ -185,7 +185,7 @@ class Ec : public Kobject, public Refcount, public Queue<Sc>
         {
             if (EXPECT_TRUE (cont != dead)) {
 
-                Counter::helping++;
+                Counter::helping.inc();
 
                 current->cont = c;
 
