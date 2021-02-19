eb12aa73a550422959c3fc04615a74ca54db5a93
Udo Steinberg <udo@hypervisor.org>
2021-02-19 13:12:56 +0100

    x86: Removed AVL and MDB structures.

--- inc/x86_64/capability.hpp
+++ inc/x86_64/capability.hpp
@@ -1,10 +1,11 @@
 /*
- * Capability
+ * Object Capability
  *
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
- * Copyright (C) 2012 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -20,23 +21,121 @@
 
 #pragma once
 
+#include "compiler.hpp"
 #include "kobject.hpp"
+#include "std.hpp"
 
-class Capability
+class Capability final
 {
     private:
-        mword val;
+        uintptr_t val { 0 };
 
-        static mword const perm = 0x1f;
+        ALWAYS_INLINE
+        inline bool validate (Kobject::Type t, unsigned p) const
+        {
+            return obj() && obj()->type == t && (prm() & p) == p;
+        }
+
+        ALWAYS_INLINE
+        inline bool validate (Kobject::Type t, Kobject::Subtype s, unsigned p) const
+        {
+            return obj() && obj()->type == t && obj()->subtype == s && (prm() & p) == p;
+        }
 
     public:
-        Capability() : val (0) {}
+        // Mask of low bits that can store permissions
+        static constexpr uintptr_t pmask { Kobject::alignment - 1 };
 
-        Capability (Kobject *o, mword a) : val (a ? reinterpret_cast<mword>(o) | (a & perm) : 0) {}
+        // Space Capability Permissions
+        enum class Perm_sp : unsigned
+        {
+            GRANT       = BIT (0),
+            TAKE        = BIT (1),
+            ASSIGN      = BIT (2),
+            DEFINED_OBJ = TAKE | GRANT,
+            DEFINED_HST = TAKE | GRANT,
+            DEFINED_GST = ASSIGN | GRANT,
+            DEFINED_DMA = ASSIGN | GRANT,
+            DEFINED_PIO = ASSIGN | TAKE | GRANT,
+            DEFINED_MSR = ASSIGN | TAKE | GRANT,
+        };
+
+        // PD Object Capability Permissions
+        enum class Perm_pd : unsigned
+        {
+            CTRL        = BIT (0),
+            PD          = BIT (1),
+            EC_PT_SM    = BIT (2),
+            SC          = BIT (3),
+            ASSIGN      = BIT (4),
+            DEFINED     = ASSIGN | SC | EC_PT_SM | PD | CTRL,
+        };
+
+        // EC Object Capability Permissions
+        enum class Perm_ec : unsigned
+        {
+            CTRL        = BIT (0),
+            BIND_PT     = BIT (2),
+            BIND_SC     = BIT (3),
+            DEFINED     = BIND_SC | BIND_PT | CTRL,
+        };
+
+        // SC Object Capability Permissions
+        enum class Perm_sc : unsigned
+        {
+            CTRL        = BIT (0),
+            DEFINED     = CTRL,
+        };
+
+        // PT Object Capability Permissions
+        enum class Perm_pt : unsigned
+        {
+            CTRL        = BIT (0),
+            CALL        = BIT (1),
+            EVENT       = BIT (2),
+            DEFINED     = EVENT | CALL | CTRL,
+        };
+
+        // SM Object Capability Permissions
+        enum class Perm_sm : unsigned
+        {
+            CTRL_UP     = BIT (0),
+            CTRL_DN     = BIT (1),
+            ASSIGN      = BIT (4),
+            DEFINED     = CTRL_DN | CTRL_UP,
+            DEFINED_INT = CTRL_DN | ASSIGN,
+        };
+
+        // Raw Capability Constructor
+        inline explicit Capability (uintptr_t v) : val (v) {}
+
+        // Null Capability Constructor
+        inline Capability() : val (0) {}
+
+        // Object Capability Constructor
+        inline Capability (Kobject *o, unsigned p) : val (p ? reinterpret_cast<uintptr_t>(o) | (p & pmask) : 0) {}
 
         ALWAYS_INLINE
-        inline Kobject *obj() const { return reinterpret_cast<Kobject *>(val & ~perm); }
+        inline Kobject *obj() const { return reinterpret_cast<Kobject *>(val & ~pmask); }
 
         ALWAYS_INLINE
-        inline unsigned prm() const { return val & perm; }
+        inline unsigned prm() const { return val & pmask; }
+
+        ALWAYS_INLINE
+        inline auto validate (Perm_sp p, Kobject::Subtype s) const { return validate (Kobject::Type::PD, s, std::to_underlying (p)); }
+
+        ALWAYS_INLINE
+        inline auto validate (Perm_pd p) const { return validate (Kobject::Type::PD, std::to_underlying (p)); }
+
+        ALWAYS_INLINE
+        inline auto validate (Perm_ec p) const { return validate (Kobject::Type::EC, std::to_underlying (p)); }
+
+        ALWAYS_INLINE
+        inline auto validate (Perm_sc p) const { return validate (Kobject::Type::SC, std::to_underlying (p)); }
+
+        ALWAYS_INLINE
+        inline auto validate (Perm_pt p) const { return validate (Kobject::Type::PT, std::to_underlying (p)); }
+
+        ALWAYS_INLINE
+        inline auto validate (Perm_sm p) const { return validate (Kobject::Type::SM, std::to_underlying (p)); }
 };
