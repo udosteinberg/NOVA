bf0b077b9d1a34a5e276e2b9ab0feceda804d37b
Udo Steinberg <udo@hypervisor.org>
2021-02-19 15:09:37 +0100

    x86: Modernized buddy allocator.

--- inc/x86_64/extern.hpp
+++ inc/x86_64/extern.hpp
@@ -27,7 +27,7 @@ extern char PAGE_0;
 extern char PAGE_1;
 extern char PAGE_H;
 
-extern char GIT_VER, NOVA_HPAS, PTAB_HPAS, DSTK_TOP;
+extern char GIT_VER, NOVA_HPAS, PTAB_HPAS, KMEM_HVAS, DSTK_TOP;
 extern void (*CTORS_S[])(), (*CTORS_E[])(), (*CTORS_C[])(), (*CTORS_L[])();
 
 extern char entry_sys;
