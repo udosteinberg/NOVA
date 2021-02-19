7976a09a09508cda5f603c07a77d308ba76855bc
Udo Steinberg <udo@hypervisor.org>
2021-02-19 14:57:48 +0100

    x86: Added Cache class.

---
+++ inc/x86_64/cache.hpp
@@ -0,0 +1,54 @@
+/*
+ * Cache Maintenance
+ *
+ * Copyright (C) 2019-2021 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "compiler.hpp"
+#include "types.hpp"
+
+class Cache
+{
+    friend class Cpu;
+
+    private:
+        static unsigned dcache_line_size CPULOCAL;
+        static unsigned icache_line_size CPULOCAL;
+
+    public:
+        ALWAYS_INLINE
+        static inline void data_clean()
+        {
+            asm volatile ("wbinvd" : : : "memory");
+        }
+
+        ALWAYS_INLINE
+        static inline void data_clean (void const *ptr)
+        {
+            asm volatile ("clflush (%0)" : : "r" (ptr) : "memory");
+        }
+
+        ALWAYS_INLINE
+        static inline void data_clean (void const *ptr, size_t size)
+        {
+            // Assumes if size is less than dcache_line_size, the region is NOT split across cache lines
+            asm volatile ("1:   clflush (%0)    ;"
+                          "     add     %2, %0  ;"
+                          "     cmp     %1, %0  ;"
+                          "     jb      1b      ;"
+                          : "+&r" (ptr) : "r" (static_cast<char const *>(ptr) + size), "r" (static_cast<size_t>(dcache_line_size)) : "memory");
+        }
+};
