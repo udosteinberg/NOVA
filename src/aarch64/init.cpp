1f89480690fefd67cdd64a29b4a51aa77d5927e4
Udo Steinberg <udo@hypervisor.org>
2021-02-19 15:09:18 +0100

    ARM: Unified allocators.

--- src/aarch64/init.cpp
+++ src/aarch64/init.cpp
@@ -15,12 +15,14 @@
  * GNU General Public License version 2 for more details.
  */
 
-#include "arch.hpp"
+#include "buddy.hpp"
 #include "console.hpp"
 #include "extern.hpp"
 
 extern "C" void init()
 {
+    Buddy::init();
+
     for (auto func { CTORS_S }; func != CTORS_E; (*func++)()) ;
 
     for (auto func { CTORS_C }; func != CTORS_S; (*func++)()) ;
