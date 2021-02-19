8b468c3e9e1d055b5b1787366999f5a78ce2eaa7
Udo Steinberg <udo@hypervisor.org>
2021-02-19 14:50:15 +0100

    Arm: Unified page tables.

---
+++ inc/aarch64/ptab_pte.hpp
@@ -0,0 +1,35 @@
+/*
+ * Page Table Entry (Arm)
+ *
+ * Copyright (C) 2019-2026 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "ptab.hpp"
+
+template<typename T, typename I, typename O> class Pte : public Ptab<T, I, O>::Entry
+{
+    using E = typename Ptab<T, I, O>::Entry;
+
+    public:
+        auto type (unsigned l) const { return E::val ? l == T::lev() || (l && (E::val & T::ATTR_nL)) ? E::Type::PTAB : E::Type::LEAF : E::Type::HOLE; }
+
+        // Physical address size
+        static constexpr auto pas (unsigned e)
+        {
+            constexpr unsigned encoding[8] { 32, 36, 40, 42, 44, 48, 52, 56 };
+            return encoding[BIT_RANGE (2, 0) & e];
+        }
+};
