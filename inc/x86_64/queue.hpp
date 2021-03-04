8b6d0d3487d446083e1dd40b38c92d269d42da84
Udo Steinberg <udo@hypervisor.org>
2021-03-04 18:28:46 +0100

    x86: Modernized queue infrastructure.

--- inc/x86_64/queue.hpp
+++ inc/x86_64/queue.hpp
@@ -6,6 +6,7 @@
  *
  * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
  * Copyright (C) 2014 Udo Steinberg, FireEye, Inc.
+ * Copyright (C) 2019-2021 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -21,51 +22,116 @@
 
 #pragma once
 
+#include "assert.hpp"
 #include "compiler.hpp"
 
 template <typename T>
 class Queue
 {
-    private:
-        T *headptr;
-
     public:
-        ALWAYS_INLINE
-        inline Queue() : headptr (nullptr) {}
-
-        ALWAYS_INLINE
-        inline T *head() const { return headptr; }
-
-        ALWAYS_INLINE
-        inline void enqueue (T *t)
+        class Element
         {
-            if (!headptr)
-                headptr = t->prev = t->next = t;
-            else {
-                t->next = headptr;
-                t->prev = headptr->prev;
-                t->next->prev = t->prev->next = t;
+            friend class Queue<T>;
+
+            private:
+                Queue   *qptr { nullptr };
+                Element *next { nullptr };
+                Element *prev { nullptr };
+
+            public:
+                /*
+                 * Determine if this element is queued
+                 *
+                 * @return      True if the element is queued, false otherwise
+                 */
+                ALWAYS_INLINE
+                inline bool queued() const { return qptr; }
+        };
+
+        /*
+         * Determine if this queue is empty
+         *
+         * @return      True if the queue is empty, false otherwise
+         */
+        ALWAYS_INLINE
+        inline bool empty() const { return !head; }
+
+        /*
+         * Enqueue element into this queue
+         *
+         * @param e     Element to enqueue
+         * @param h     Enqueue as head (true) or tail (false)
+         * @return      True if the queue was empty, false otherwise
+         */
+        ALWAYS_INLINE NONNULL
+        inline bool enqueue (T *e, bool h)
+        {
+            assert (!e->queued());
+
+            e->qptr = this;
+
+            if (!head) {
+                head = e->prev = e->next = e;
+                return true;
             }
+
+            e->next = head;
+            e->prev = head->prev;
+            e->next->prev = e->prev->next = e;
+
+            if (h)
+                head = e;
+
+            return false;
         }
 
-        ALWAYS_INLINE
-        inline bool dequeue (T *t)
-        {
-            if (!t || !t->next || !t->prev)
-                return false;
+        ALWAYS_INLINE NONNULL
+        inline auto enqueue_head (T *e) { return enqueue (e, true); }
 
-            if (t == t->next)
-                headptr = nullptr;
+        ALWAYS_INLINE NONNULL
+        inline auto enqueue_tail (T *e) { return enqueue (e, false); }
+
+        /*
+         * Dequeue element from this queue
+         *
+         * @param e     Element to dequeue
+         */
+        ALWAYS_INLINE NONNULL
+        inline void dequeue (T *e)
+        {
+            assert (e->queued());
+
+            e->qptr = nullptr;
+
+            if (e == e->next)
+                head = nullptr;
 
             else {
-                t->next->prev = t->prev;
-                t->prev->next = t->next;
-                if (t == headptr)
-                    headptr = t->next;
+                e->next->prev = e->prev;
+                e->prev->next = e->next;
+                if (e == head)
+                    head = e->next;
             }
 
-            t->next = t->prev = nullptr;
-
-            return true;
+            e->next = e->prev = nullptr;
         }
+
+        /*
+         * Dequeue head element from this queue
+         *
+         * @return      Element that was dequeued or nullptr
+         */
+        ALWAYS_INLINE
+        inline auto dequeue_head()
+        {
+            auto h = static_cast<T *>(head);
+
+            if (h)
+                dequeue (h);
+
+            return h;
+        }
+
+    private:
+        Element *head { nullptr };
 };
