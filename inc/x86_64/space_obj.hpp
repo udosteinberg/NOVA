b4be282d15046203755ffe2dbb83719012e0998a
Udo Steinberg <udo@hypervisor.org>
2021-03-05 11:18:38 +0100

    x86: Modernized OBJ space.

--- inc/x86_64/space_obj.hpp
+++ inc/x86_64/space_obj.hpp
@@ -4,7 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
- * Copyright (C) 2012 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2022 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -20,38 +21,52 @@
 
 #pragma once
 
+#include "atomic.hpp"
+#include "bits.hpp"
 #include "capability.hpp"
+#include "macros.hpp"
+#include "memory.hpp"
+#include "space.hpp"
+#include "status.hpp"
 
-class Space_mem;
-
-class Space_obj
+class Space_obj : public Space
 {
+    friend class Pd;
+
     private:
-        ALWAYS_INLINE
-        static inline mword idx_to_virt (unsigned long idx)
-        {
-            return MMAP_SPC_OBJ + (idx % caps) * sizeof (Capability);
-        }
+        struct Captable;
 
-        ALWAYS_INLINE
-        inline Space_mem *space_mem();
+        Atomic<Captable *> root { nullptr };
 
-        void update (mword, Capability);
+        static constexpr auto lev { 2 };
+        static constexpr auto bpl { bit_scan_reverse (PAGE_SIZE / sizeof (Captable *)) };
+
+        ~Space_obj();
+
+        Atomic<Capability> *walk (unsigned long, bool);
 
     public:
-        static unsigned const caps = (END_SPACE_LIM - MMAP_SPC_OBJ) / sizeof (Capability);
+        static Space_obj nova;
 
-        ALWAYS_INLINE
-        static inline Capability lookup (unsigned long idx)
+        static constexpr auto num { BIT64 (lev * bpl) };
+
+        enum Selector
         {
-            return *reinterpret_cast<Capability *>(idx_to_virt (idx));
-        }
+            NOVA_CON = num - 1,
+            NOVA_OBJ = num - 2,
+            NOVA_HST = num - 3,
+            NOVA_PIO = num - 4,
+            NOVA_MSR = num - 5,
+            ROOT_OBJ = num - 6,
+            ROOT_HST = num - 7,
+            ROOT_PIO = num - 8,
+            NOVA_INT = BIT (16),
+            NOVA_CPU = 0,
+        };
 
-        size_t lookup (mword, Capability &);
+        Capability lookup (unsigned long) const;
+        Status     update (unsigned long, Capability, Capability &);
+        Status     insert (unsigned long, Capability);
 
-        Paddr walk (mword = 0);
-
-        static void page_fault (mword, mword);
-
-        static bool insert_root (Kobject *);
+        Status delegate (Space_obj const *, unsigned long, unsigned long, unsigned, unsigned);
 };
