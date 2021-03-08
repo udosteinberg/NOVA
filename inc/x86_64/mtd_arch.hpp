6d9ce38006701f548126733f04c5ec8135ba9c49
Udo Steinberg <udo@hypervisor.org>
2021-03-08 14:01:30 +0100

    x86: Modernized MTD.

---
+++ inc/x86_64/mtd_arch.hpp
@@ -0,0 +1,70 @@
+/*
+ * Message Transfer Descriptor (MTD): Architecture-Specific Part (x86)
+ *
+ * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "mtd.hpp"
+
+class Mtd_arch final : public Mtd
+{
+    public:
+        enum Item
+        {
+            POISON          = BIT  (0),
+
+            GPR_0_7         = BIT  (1),
+            GPR_8_15        = BIT  (2),
+            RFLAGS          = BIT  (3),
+            RIP             = BIT  (4),
+
+            STA             = BIT  (5),
+            QUAL            = BIT  (6),
+            CTRL            = BIT  (7),
+            TPR             = BIT  (8),
+            INJ             = BIT  (9),
+
+            CS_SS           = BIT (10),
+            DS_ES           = BIT (11),
+            FS_GS           = BIT (12),
+            TR              = BIT (13),
+            LDTR            = BIT (14),
+            GDTR            = BIT (15),
+            IDTR            = BIT (16),
+
+            PDPTE           = BIT (17),
+            CR              = BIT (18),
+            DR              = BIT (19),
+            XSAVE           = BIT (20),
+            APIC            = BIT (21),
+            SYSCALL         = BIT (22),
+            SYSENTER        = BIT (23),
+            PAT             = BIT (24),
+            EFER            = BIT (25),
+            KERNEL_GS_BASE  = BIT (26),
+            TSC             = BIT (27),
+
+            TLB             = BIT (29),
+            FPU             = BIT (30),
+
+            SPACES          = BIT (31),
+        };
+
+        explicit Mtd_arch (uint32_t v = 0) : Mtd { v } {}
+};
