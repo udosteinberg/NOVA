802f4b55c9210dc0607cb1280872a5e411b57686
Udo Steinberg <udo@hypervisor.org>
2021-03-08 14:01:30 +0100

    x86: Modernized MTD.

--- src/x86_64/syscall.cpp
+++ src/x86_64/syscall.cpp
@@ -73,7 +73,7 @@ void Ec::send_msg()
     if (!ec->cont) [[likely]] {
         current->cont = C;
         current->set_partner (ec);
-        current->regs.mtd = pt->mtd.val;
+        current->regs.mtd = pt->mtd;
         ec->cont = recv_kern;
         ec->sys_regs().set_pt (pt->id);
         ec->sys_regs().set_ip (pt->ip);
