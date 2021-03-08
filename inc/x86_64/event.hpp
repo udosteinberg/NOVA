c4e0132edd7fc1193389d11a63b15f58bc277e5b
Udo Steinberg <udo@hypervisor.org>
2021-03-08 13:38:57 +0100

    x86: Added Event class.

---
+++ inc/x86_64/event.hpp
@@ -0,0 +1,35 @@
+/*
+ * Event Selectors
+ *
+ * Copyright (C) 2019-2022 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+class Event final
+{
+    public:
+        enum Selector
+        {
+            NONE        = -1,
+            STARTUP     =  0,
+            RECALL      =  1,
+        };
+
+        static constexpr auto hst_arch  { 32 };
+        static constexpr auto gst_arch  { 256 };
+
+        static constexpr auto hst_max   { 1 + RECALL };
+        static constexpr auto gst_max   { 1 + RECALL };
+};
