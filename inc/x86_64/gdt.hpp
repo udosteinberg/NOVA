98c5b08e3e505ca492158fd97c78b048b0b38d53
Udo Steinberg <udo@hypervisor.org>
2021-03-09 16:36:50 +0100

    x86: Modernized GDT/IDT descriptors.

--- inc/x86_64/gdt.hpp
+++ inc/x86_64/gdt.hpp
@@ -4,7 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
- * Copyright (C) 2012 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -23,23 +24,23 @@
 #include "descriptor.hpp"
 #include "selectors.hpp"
 
-class Gdt : public Descriptor
+class Gdt final : private Descriptor
 {
     private:
-        uint32 val[2];
+        uint32_t val[2];
 
         ALWAYS_INLINE
-        inline void set32 (Type type, Granularity gran, Size size, bool l, unsigned dpl, mword base, mword limit)
+        inline void set32 (Type type, Granularity gran, Size size, bool l, unsigned dpl, uintptr_t base, uintptr_t limit)
         {
-            val[0] = static_cast<uint32>(base << 16 | (limit & 0xffff));
-            val[1] = static_cast<uint32>((base & 0xff000000) | gran | size | (limit & 0xf0000) | l << 21 | 1u << 15 | dpl << 13 | type | (base >> 16 & 0xff));
+            val[0] = static_cast<uint32_t>(base << 16 | (limit & 0xffff));
+            val[1] = static_cast<uint32_t>((base & 0xff000000) | gran | size | (limit & 0xf0000) | l << 21 | 1u << 15 | dpl << 13 | type | (base >> 16 & 0xff));
         }
 
         ALWAYS_INLINE
-        inline void set64 (Type type, Granularity gran, Size size, bool l, unsigned dpl, mword base, mword limit)
+        inline void set64 (Type type, Granularity gran, Size size, bool l, unsigned dpl, uintptr_t base, uintptr_t limit)
         {
             set32 (type, gran, size, l, dpl, base, limit);
-            (this + 1)->val[0] = static_cast<uint32>(base >> 32);
+            (this + 1)->val[0] = static_cast<uint32_t>(base >> 32);
             (this + 1)->val[1] = 0;
         }
 
@@ -51,7 +52,8 @@ class Gdt : public Descriptor
         ALWAYS_INLINE
         static inline void load()
         {
-            asm volatile ("lgdt %0" : : "m" (Pseudo_descriptor (sizeof (gdt) - 1, reinterpret_cast<mword>(gdt))));
+            Pseudo_descriptor d { gdt, sizeof (gdt) };
+            asm volatile ("lgdt %0" : : "m" (d));
         }
 
         ALWAYS_INLINE
