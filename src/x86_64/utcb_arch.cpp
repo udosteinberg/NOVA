b7eb10cae8a15098694eff0bf18799bbe5b6bd3f
Udo Steinberg <udo@hypervisor.org>
2021-03-09 14:45:28 +0100

    x86: Modernized UTCB.

---
+++ src/x86_64/utcb_arch.cpp
@@ -0,0 +1,558 @@
+/*
+ * User Thread Control Block (UTCB)
+ *
+ * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2026 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "config.hpp"
+#include "lowlevel.hpp"
+#include "mtd_arch.hpp"
+#include "regs.hpp"
+#include "svm.hpp"
+#include "vmx.hpp"
+
+void Utcb_arch::load_exc (Mtd_arch const m, Cpu_regs const &c)
+{
+    auto const &e { c.exc };
+    auto const &s { c.exc.sys };
+
+    if (m & Mtd_arch::Item::GPR_0_7) {
+        rax = s.rax; rcx = s.rcx; rdx = s.rdx; rbx = s.rbx;
+        rsp = e.rsp; rbp = s.rbp; rsi = s.rsi; rdi = s.rdi;
+    }
+
+    if (m & Mtd_arch::Item::GPR_8_15) {
+        r8  = s.r8;  r9  = s.r9;  r10 = s.r10; r11 = s.r11;
+        r12 = s.r12; r13 = s.r13; r14 = s.r14; r15 = s.r15;
+    }
+
+    if (m & Mtd_arch::Item::RFLAGS)
+        rfl = e.rfl;
+
+    if (m & Mtd_arch::Item::RIP)
+        rip = e.rip;
+
+    if (m & Mtd_arch::Item::QUAL) {
+        qual[0] = e.err;
+        qual[1] = c.cr2;
+        qual[2] = 0;
+    }
+}
+
+bool Utcb_arch::save_exc (Mtd_arch const m, Cpu_regs &c) const
+{
+    auto &e { c.exc };
+    auto &s { c.exc.sys };
+
+    if (m & Mtd_arch::Item::POISON)
+        return false;
+
+    if (m & Mtd_arch::Item::GPR_0_7) {
+        s.rax = rax; s.rcx = rcx; s.rdx = rdx; s.rbx = rbx;
+        e.rsp = rsp; s.rbp = rbp; s.rsi = rsi; s.rdi = rdi;
+    }
+
+    if (m & Mtd_arch::Item::GPR_8_15) {
+        s.r8  = r8;  s.r9  = r9;  s.r10 = r10; s.r11 = r11;
+        s.r12 = r12; s.r13 = r13; s.r14 = r14; s.r15 = r15;
+    }
+
+    if (m & Mtd_arch::Item::RFLAGS)
+        e.rfl = (rfl & ~(RFL_VIP | RFL_VIF | RFL_VM | RFL_RF | RFL_NT | RFL_IOPL)) | RFL_AC | RFL_IF | RFL_1;
+
+    if (m & Mtd_arch::Item::RIP)
+        e.rip = rip;
+
+    // QUAL state is read-only
+
+    return true;
+}
+
+void Utcb_arch::load_vmx (Mtd_arch const m, Cpu_regs const &c)
+{
+    auto const &s { c.exc.sys };
+
+    c.vmcs->make_current();
+
+    if (m & Mtd_arch::Item::GPR_0_7) {
+        rax = s.rax; rcx = s.rcx; rdx = s.rdx; rbx = s.rbx;
+        rbp = s.rbp; rsi = s.rsi; rdi = s.rdi;
+        rsp = Vmcs::read<Vmcs::Encoding::GUEST_RSP>();
+    }
+
+    if (m & Mtd_arch::Item::GPR_8_15) {
+        r8  = s.r8;  r9  = s.r9;  r10 = s.r10; r11 = s.r11;
+        r12 = s.r12; r13 = s.r13; r14 = s.r14; r15 = s.r15;
+    }
+
+    if (m & Mtd_arch::Item::RFLAGS)
+        rfl = Vmcs::read<Vmcs::Encoding::GUEST_RFLAGS>();
+
+    if (m & Mtd_arch::Item::RIP) {
+        rip       = Vmcs::read<Vmcs::Encoding::GUEST_RIP>();
+        inst_len  = Vmcs::read<Vmcs::Encoding::EXI_INST_LEN>();
+        inst_info = Vmcs::read<Vmcs::Encoding::EXI_INST_INFO>();
+    }
+
+    if (m & Mtd_arch::Item::STA) {
+        intr_state = Vmcs::read<Vmcs::Encoding::GUEST_INTR_STATE>();
+        actv_state = Vmcs::read<Vmcs::Encoding::GUEST_ACTV_STATE>();
+    }
+
+    if (m & Mtd_arch::Item::QUAL) {
+        qual[0] = Vmcs::read<Vmcs::Encoding::EXI_QUALIFICATION>();
+        qual[1] = Vmcs::read<Vmcs::Encoding::GUEST_LINEAR_ADDRESS>();
+        qual[2] = Vmcs::read<Vmcs::Encoding::GUEST_PHYSICAL_ADDRESS>();
+    }
+
+    // CTRL and TPR state is write-only
+
+    if (m & Mtd_arch::Item::INJ) {
+        if (c.exc.ep() == 33 || c.exc.ep() == NUM_VMI - 1) {
+            intr_info = Vmcs::read<Vmcs::Encoding::INJ_EVENT_IDENT>();
+            intr_errc = Vmcs::read<Vmcs::Encoding::INJ_EVENT_ERROR>();
+        } else {
+            intr_info = Vmcs::read<Vmcs::Encoding::EXI_EVENT_IDENT>();
+            intr_errc = Vmcs::read<Vmcs::Encoding::EXI_EVENT_ERROR>();
+            vect_info = Vmcs::read<Vmcs::Encoding::ORG_EVENT_IDENT>();
+            vect_errc = Vmcs::read<Vmcs::Encoding::ORG_EVENT_ERROR>();
+        }
+    }
+
+    if (m & Mtd_arch::Item::CS_SS) {
+        cs.set_vmx (Vmcs::read<Vmcs::Encoding::GUEST_SEL_CS>(), Vmcs::read<Vmcs::Encoding::GUEST_BASE_CS>(), Vmcs::read<Vmcs::Encoding::GUEST_LIMIT_CS>(), Vmcs::read<Vmcs::Encoding::GUEST_AR_CS>());
+        ss.set_vmx (Vmcs::read<Vmcs::Encoding::GUEST_SEL_SS>(), Vmcs::read<Vmcs::Encoding::GUEST_BASE_SS>(), Vmcs::read<Vmcs::Encoding::GUEST_LIMIT_SS>(), Vmcs::read<Vmcs::Encoding::GUEST_AR_SS>());
+    }
+
+    if (m & Mtd_arch::Item::DS_ES) {
+        ds.set_vmx (Vmcs::read<Vmcs::Encoding::GUEST_SEL_DS>(), Vmcs::read<Vmcs::Encoding::GUEST_BASE_DS>(), Vmcs::read<Vmcs::Encoding::GUEST_LIMIT_DS>(), Vmcs::read<Vmcs::Encoding::GUEST_AR_DS>());
+        es.set_vmx (Vmcs::read<Vmcs::Encoding::GUEST_SEL_ES>(), Vmcs::read<Vmcs::Encoding::GUEST_BASE_ES>(), Vmcs::read<Vmcs::Encoding::GUEST_LIMIT_ES>(), Vmcs::read<Vmcs::Encoding::GUEST_AR_ES>());
+    }
+
+    if (m & Mtd_arch::Item::FS_GS) {
+        fs.set_vmx (Vmcs::read<Vmcs::Encoding::GUEST_SEL_FS>(), Vmcs::read<Vmcs::Encoding::GUEST_BASE_FS>(), Vmcs::read<Vmcs::Encoding::GUEST_LIMIT_FS>(), Vmcs::read<Vmcs::Encoding::GUEST_AR_FS>());
+        gs.set_vmx (Vmcs::read<Vmcs::Encoding::GUEST_SEL_GS>(), Vmcs::read<Vmcs::Encoding::GUEST_BASE_GS>(), Vmcs::read<Vmcs::Encoding::GUEST_LIMIT_GS>(), Vmcs::read<Vmcs::Encoding::GUEST_AR_GS>());
+    }
+
+    if (m & Mtd_arch::Item::TR)
+        tr.set_vmx (Vmcs::read<Vmcs::Encoding::GUEST_SEL_TR>(), Vmcs::read<Vmcs::Encoding::GUEST_BASE_TR>(), Vmcs::read<Vmcs::Encoding::GUEST_LIMIT_TR>(), Vmcs::read<Vmcs::Encoding::GUEST_AR_TR>());
+
+    if (m & Mtd_arch::Item::LDTR)
+        ld.set_vmx (Vmcs::read<Vmcs::Encoding::GUEST_SEL_LDTR>(), Vmcs::read<Vmcs::Encoding::GUEST_BASE_LDTR>(), Vmcs::read<Vmcs::Encoding::GUEST_LIMIT_LDTR>(), Vmcs::read<Vmcs::Encoding::GUEST_AR_LDTR>());
+
+    if (m & Mtd_arch::Item::GDTR)
+        gd.set_vmx (0, Vmcs::read<Vmcs::Encoding::GUEST_BASE_GDTR>(), Vmcs::read<Vmcs::Encoding::GUEST_LIMIT_GDTR>(), 0);
+
+    if (m & Mtd_arch::Item::IDTR)
+        id.set_vmx (0, Vmcs::read<Vmcs::Encoding::GUEST_BASE_IDTR>(), Vmcs::read<Vmcs::Encoding::GUEST_LIMIT_IDTR>(), 0);
+
+    if (m & Mtd_arch::Item::PDPTE) {
+        pdpte[0] = Vmcs::read<Vmcs::Encoding::GUEST_PDPTE0>();
+        pdpte[1] = Vmcs::read<Vmcs::Encoding::GUEST_PDPTE1>();
+        pdpte[2] = Vmcs::read<Vmcs::Encoding::GUEST_PDPTE2>();
+        pdpte[3] = Vmcs::read<Vmcs::Encoding::GUEST_PDPTE3>();
+    }
+
+    if (m & Mtd_arch::Item::CR) {
+        cr0 = c.vmx_get_gst_cr0();
+        cr4 = c.vmx_get_gst_cr4();
+        cr2 = c.cr2;
+        cr3 = Vmcs::read<Vmcs::Encoding::GUEST_CR3>();
+    }
+
+    if (m & Mtd_arch::Item::DR)
+        dr7 = Vmcs::read<Vmcs::Encoding::GUEST_DR7>();
+
+    if (m & Mtd_arch::Item::SYSENTER) {
+        sysenter_cs  = Vmcs::read<Vmcs::Encoding::GUEST_SYSENTER_CS>();
+        sysenter_esp = Vmcs::read<Vmcs::Encoding::GUEST_SYSENTER_ESP>();
+        sysenter_eip = Vmcs::read<Vmcs::Encoding::GUEST_SYSENTER_EIP>();
+    }
+
+    if (m & Mtd_arch::Item::PAT)
+        pat = Vmcs::read<Vmcs::Encoding::GUEST_PAT>();
+
+    if (m & Mtd_arch::Item::EFER)
+        efer = Vmcs::read<Vmcs::Encoding::GUEST_EFER>();
+}
+
+bool Utcb_arch::save_vmx (Mtd_arch const m, Cpu_regs &c) const
+{
+    auto &s { c.exc.sys };
+
+    c.vmcs->make_current();
+
+    if (m & Mtd_arch::Item::POISON)
+        return false;
+
+    if (m & Mtd_arch::Item::GPR_0_7) {
+        s.rax = rax; s.rcx = rcx; s.rdx = rdx; s.rbx = rbx;
+        s.rbp = rbp; s.rsi = rsi; s.rdi = rdi;
+        Vmcs::write<Vmcs::Encoding::GUEST_RSP>(rsp);
+    }
+
+    if (m & Mtd_arch::Item::GPR_8_15) {
+        s.r8  = r8;  s.r9  = r9;  s.r10 = r10; s.r11 = r11;
+        s.r12 = r12; s.r13 = r13; s.r14 = r14; s.r15 = r15;
+    }
+
+    if (m & Mtd_arch::Item::RFLAGS)
+        Vmcs::write<Vmcs::Encoding::GUEST_RFLAGS>(rfl);
+
+    if (m & Mtd_arch::Item::RIP) {
+        Vmcs::write<Vmcs::Encoding::GUEST_RIP>(rip);
+        Vmcs::write<Vmcs::Encoding::ENT_INST_LEN>(inst_len);
+    }
+
+    if (m & Mtd_arch::Item::STA) {
+        Vmcs::write<Vmcs::Encoding::GUEST_INTR_STATE>(intr_state);
+        Vmcs::write<Vmcs::Encoding::GUEST_ACTV_STATE>(actv_state);
+    }
+
+    // QUAL state is read-only
+
+    if (m & Mtd_arch::Item::CTRL) {
+        c.vmx_set_cpu_pri (ctrl_pri);
+        c.vmx_set_cpu_sec (ctrl_sec);
+        Vmcs::write<Vmcs::Encoding::PF_ERROR_MASK>(pfe_mask);
+        Vmcs::write<Vmcs::Encoding::PF_ERROR_MATCH>(pfe_match);
+    }
+
+    if (m & Mtd_arch::Item::TPR)
+        Vmcs::write<Vmcs::Encoding::TPR_THRESHOLD>(tpr_threshold);
+
+    if (m & Mtd_arch::Item::INJ) {
+
+        auto val { Vmcs::read<Vmcs::Encoding::CPU_CONTROLS_PRI>() };
+
+        if (intr_info & 0x1000)
+            val |=  Vmcs::CPU_INTR_WINDOW;
+        else
+            val &= ~Vmcs::CPU_INTR_WINDOW;
+
+        if (intr_info & 0x2000)
+            val |=  Vmcs::CPU_NMI_WINDOW;
+        else
+            val &= ~Vmcs::CPU_NMI_WINDOW;
+
+        c.vmx_set_cpu_pri (val);
+
+        Vmcs::write<Vmcs::Encoding::INJ_EVENT_IDENT>(intr_info & ~0x3000);
+        Vmcs::write<Vmcs::Encoding::INJ_EVENT_ERROR>(intr_errc);
+    }
+
+    if (m & Mtd_arch::Item::CS_SS) {
+        Vmcs::write<Vmcs::Encoding::GUEST_SEL_CS>  (cs.sel);
+        Vmcs::write<Vmcs::Encoding::GUEST_BASE_CS> (cs.base);
+        Vmcs::write<Vmcs::Encoding::GUEST_LIMIT_CS>(cs.limit);
+        Vmcs::write<Vmcs::Encoding::GUEST_AR_CS>  ((cs.ar << 4 & 0x1f000) | (cs.ar & 0xff));
+        Vmcs::write<Vmcs::Encoding::GUEST_SEL_SS>  (ss.sel);
+        Vmcs::write<Vmcs::Encoding::GUEST_BASE_SS> (ss.base);
+        Vmcs::write<Vmcs::Encoding::GUEST_LIMIT_SS>(ss.limit);
+        Vmcs::write<Vmcs::Encoding::GUEST_AR_SS>  ((ss.ar << 4 & 0x1f000) | (ss.ar & 0xff));
+    }
+
+    if (m & Mtd_arch::Item::DS_ES) {
+        Vmcs::write<Vmcs::Encoding::GUEST_SEL_DS>  (ds.sel);
+        Vmcs::write<Vmcs::Encoding::GUEST_BASE_DS> (ds.base);
+        Vmcs::write<Vmcs::Encoding::GUEST_LIMIT_DS>(ds.limit);
+        Vmcs::write<Vmcs::Encoding::GUEST_AR_DS>  ((ds.ar << 4 & 0x1f000) | (ds.ar & 0xff));
+        Vmcs::write<Vmcs::Encoding::GUEST_SEL_ES>  (es.sel);
+        Vmcs::write<Vmcs::Encoding::GUEST_BASE_ES> (es.base);
+        Vmcs::write<Vmcs::Encoding::GUEST_LIMIT_ES>(es.limit);
+        Vmcs::write<Vmcs::Encoding::GUEST_AR_ES>  ((es.ar << 4 & 0x1f000) | (es.ar & 0xff));
+    }
+
+    if (m & Mtd_arch::Item::FS_GS) {
+        Vmcs::write<Vmcs::Encoding::GUEST_SEL_FS>  (fs.sel);
+        Vmcs::write<Vmcs::Encoding::GUEST_BASE_FS> (fs.base);
+        Vmcs::write<Vmcs::Encoding::GUEST_LIMIT_FS>(fs.limit);
+        Vmcs::write<Vmcs::Encoding::GUEST_AR_FS>  ((fs.ar << 4 & 0x1f000) | (fs.ar & 0xff));
+        Vmcs::write<Vmcs::Encoding::GUEST_SEL_GS>  (gs.sel);
+        Vmcs::write<Vmcs::Encoding::GUEST_BASE_GS> (gs.base);
+        Vmcs::write<Vmcs::Encoding::GUEST_LIMIT_GS>(gs.limit);
+        Vmcs::write<Vmcs::Encoding::GUEST_AR_GS>  ((gs.ar << 4 & 0x1f000) | (gs.ar & 0xff));
+    }
+
+    if (m & Mtd_arch::Item::TR) {
+        Vmcs::write<Vmcs::Encoding::GUEST_SEL_TR>  (tr.sel);
+        Vmcs::write<Vmcs::Encoding::GUEST_BASE_TR> (tr.base);
+        Vmcs::write<Vmcs::Encoding::GUEST_LIMIT_TR>(tr.limit);
+        Vmcs::write<Vmcs::Encoding::GUEST_AR_TR>  ((tr.ar << 4 & 0x1f000) | (tr.ar & 0xff));
+    }
+
+    if (m & Mtd_arch::Item::LDTR) {
+        Vmcs::write<Vmcs::Encoding::GUEST_SEL_LDTR>  (ld.sel);
+        Vmcs::write<Vmcs::Encoding::GUEST_BASE_LDTR> (ld.base);
+        Vmcs::write<Vmcs::Encoding::GUEST_LIMIT_LDTR>(ld.limit);
+        Vmcs::write<Vmcs::Encoding::GUEST_AR_LDTR>  ((ld.ar << 4 & 0x1f000) | (ld.ar & 0xff));
+    }
+
+    if (m & Mtd_arch::Item::GDTR) {
+        Vmcs::write<Vmcs::Encoding::GUEST_BASE_GDTR> (gd.base);
+        Vmcs::write<Vmcs::Encoding::GUEST_LIMIT_GDTR>(gd.limit);
+    }
+
+    if (m & Mtd_arch::Item::IDTR) {
+        Vmcs::write<Vmcs::Encoding::GUEST_BASE_IDTR> (id.base);
+        Vmcs::write<Vmcs::Encoding::GUEST_LIMIT_IDTR>(id.limit);
+    }
+
+    if (m & Mtd_arch::Item::PDPTE) {
+        Vmcs::write<Vmcs::Encoding::GUEST_PDPTE0>(pdpte[0]);
+        Vmcs::write<Vmcs::Encoding::GUEST_PDPTE1>(pdpte[1]);
+        Vmcs::write<Vmcs::Encoding::GUEST_PDPTE2>(pdpte[2]);
+        Vmcs::write<Vmcs::Encoding::GUEST_PDPTE3>(pdpte[3]);
+    }
+
+    if (m & Mtd_arch::Item::CR) {
+        c.vmx_set_gst_cr0 (cr0);
+        c.vmx_set_gst_cr4 (cr4);
+        c.cr2 = cr2;
+        Vmcs::write<Vmcs::Encoding::GUEST_CR3>(cr3);
+    }
+
+    if (m & Mtd_arch::Item::DR)
+        Vmcs::write<Vmcs::Encoding::GUEST_DR7>(dr7);
+
+    if (m & Mtd_arch::Item::SYSENTER) {
+        Vmcs::write<Vmcs::Encoding::GUEST_SYSENTER_CS>(static_cast<uint32_t>(sysenter_cs));
+        Vmcs::write<Vmcs::Encoding::GUEST_SYSENTER_ESP>(sysenter_esp);
+        Vmcs::write<Vmcs::Encoding::GUEST_SYSENTER_EIP>(sysenter_eip);
+    }
+
+    if (m & Mtd_arch::Item::PAT)
+        Vmcs::write<Vmcs::Encoding::GUEST_PAT>(pat);
+
+    if (m & Mtd_arch::Item::EFER) {
+
+        Vmcs::write<Vmcs::Encoding::GUEST_EFER>(efer);
+
+        auto ent { Vmcs::read<Vmcs::Encoding::ENT_CONTROLS>() };
+
+        if (efer & EFER_LMA)
+            ent |=  Vmcs::ENT_GUEST_64;
+        else
+            ent &= ~Vmcs::ENT_GUEST_64;
+
+        Vmcs::write<Vmcs::Encoding::ENT_CONTROLS>(ent);
+    }
+
+    return true;
+}
+
+void Utcb_arch::load_svm (Mtd_arch const m, Cpu_regs const &c)
+{
+    auto const &s { c.exc.sys };
+    auto const  v { c.vmcb };
+
+    if (m & Mtd_arch::Item::GPR_0_7) {
+        rax = v->rax; rcx = s.rcx; rdx = s.rdx; rbx = s.rbx;
+        rsp = v->rsp; rbp = s.rbp; rsi = s.rsi; rdi = s.rdi;
+    }
+
+    if (m & Mtd_arch::Item::GPR_8_15) {
+        r8  = s.r8;  r9  = s.r9;  r10 = s.r10; r11 = s.r11;
+        r12 = s.r12; r13 = s.r13; r14 = s.r14; r15 = s.r15;
+    }
+
+    if (m & Mtd_arch::Item::RFLAGS)
+        rfl = v->rflags;
+
+    if (m & Mtd_arch::Item::RIP)
+        rip = v->rip;
+
+    if (m & Mtd_arch::Item::STA) {
+        intr_state = static_cast<uint32_t>(v->int_shadow);
+        actv_state = 0;
+    }
+
+    if (m & Mtd_arch::Item::QUAL) {
+        qual[0] = v->exitinfo1;
+        qual[1] = v->exitinfo2;
+        qual[2] = 0;
+    }
+
+    // CTRL and TPR state is write-only
+
+    if (m & Mtd_arch::Item::INJ) {
+        if (c.exc.ep() == NUM_VMI - 3 || c.exc.ep() == NUM_VMI - 1) {
+            intr_info = static_cast<uint32_t>(v->inj_control);
+            intr_errc = static_cast<uint32_t>(v->inj_control >> 32);
+        } else {
+            intr_info = static_cast<uint32_t>(v->exitcode);
+            intr_errc = static_cast<uint32_t>(v->exitinfo1);
+            vect_info = static_cast<uint32_t>(v->exitintinfo);
+            vect_errc = static_cast<uint32_t>(v->exitintinfo >> 32);
+        }
+    }
+
+    if (m & Mtd_arch::Item::CS_SS) {
+        cs = v->cs;
+        ss = v->ss;
+    }
+
+    if (m & Mtd_arch::Item::DS_ES) {
+        ds = v->ds;
+        es = v->es;
+    }
+
+    if (m & Mtd_arch::Item::FS_GS) {
+        fs = v->fs;
+        gs = v->gs;
+    }
+
+    if (m & Mtd_arch::Item::TR)
+        tr = v->tr;
+
+    if (m & Mtd_arch::Item::LDTR)
+        ld = v->ldtr;
+
+    if (m & Mtd_arch::Item::GDTR)
+        gd = v->gdtr;
+
+    if (m & Mtd_arch::Item::IDTR)
+        id = v->idtr;
+
+    // PDPTE registers are not used
+
+    if (m & Mtd_arch::Item::CR) {
+        cr0 = v->cr0;
+        cr2 = v->cr2;
+        cr3 = v->cr3;
+        cr4 = v->cr4;
+    }
+
+    if (m & Mtd_arch::Item::DR)
+        dr7 = v->dr7;
+
+    if (m & Mtd_arch::Item::SYSENTER) {
+        sysenter_cs  = v->sysenter_cs;
+        sysenter_esp = v->sysenter_esp;
+        sysenter_eip = v->sysenter_eip;
+    }
+
+    if (m & Mtd_arch::Item::PAT)
+        pat = v->g_pat;
+
+    if (m & Mtd_arch::Item::EFER)
+        efer = v->efer;
+}
+
+bool Utcb_arch::save_svm (Mtd_arch const m, Cpu_regs &c) const
+{
+    auto &s { c.exc.sys };
+    auto  v { c.vmcb };
+
+    if (m & Mtd_arch::Item::POISON)
+        return false;
+
+    if (m & Mtd_arch::Item::GPR_0_7) {
+        v->rax = rax; s.rcx = rcx; s.rdx = rdx; s.rbx = rbx;
+        v->rsp = rsp; s.rbp = rbp; s.rsi = rsi; s.rdi = rdi;
+    }
+
+    if (m & Mtd_arch::Item::GPR_8_15) {
+        s.r8  = r8;  s.r9  = r9;  s.r10 = r10; s.r11 = r11;
+        s.r12 = r12; s.r13 = r13; s.r14 = r14; s.r15 = r15;
+    }
+
+    if (m & Mtd_arch::Item::RFLAGS)
+        v->rflags = rfl;
+
+    if (m & Mtd_arch::Item::RIP)
+        v->rip = rip;
+
+    if (m & Mtd_arch::Item::STA)
+        v->int_shadow = intr_state;
+
+    // QUAL state is read-only
+
+    if (m & Mtd_arch::Item::CTRL) {
+        c.svm_set_cpu_pri (ctrl_pri);
+        c.svm_set_cpu_sec (ctrl_sec);
+    }
+
+    // TPR threshold is not used
+
+    if (m & Mtd_arch::Item::INJ) {
+
+        if (intr_info & 0x1000) {
+            v->int_control      |=  (1ul << 8 | 1ul << 20);
+            v->intercept_cpu[0] |=  Vmcb::CPU_VINTR;
+        } else {
+            v->int_control      &= ~(1ul << 8 | 1ul << 20);
+            v->intercept_cpu[0] &= ~Vmcb::CPU_VINTR;
+        }
+
+        v->inj_control = static_cast<uint64_t>(intr_errc) << 32 | (intr_info & ~0x3000);
+    }
+
+    if (m & Mtd_arch::Item::CS_SS) {
+        v->cs = cs;
+        v->ss = ss;
+    }
+
+    if (m & Mtd_arch::Item::DS_ES) {
+        v->ds = ds;
+        v->es = es;
+    }
+
+    if (m & Mtd_arch::Item::FS_GS) {
+        v->fs = fs;
+        v->gs = gs;
+    }
+
+    if (m & Mtd_arch::Item::TR)
+        v->tr = tr;
+
+    if (m & Mtd_arch::Item::LDTR)
+        v->ldtr = ld;
+
+    if (m & Mtd_arch::Item::GDTR)
+        v->gdtr = gd;
+
+    if (m & Mtd_arch::Item::IDTR)
+        v->idtr = id;
+
+    // PDPTE registers are not used
+
+    if (m & Mtd_arch::Item::CR) {
+        v->cr0 = cr0;
+        v->cr2 = cr2;
+        v->cr3 = cr3;
+        v->cr4 = cr4;
+    }
+
+    if (m & Mtd_arch::Item::DR)
+        v->dr7 = dr7;
+
+    if (m & Mtd_arch::Item::SYSENTER) {
+        v->sysenter_cs  = sysenter_cs;
+        v->sysenter_esp = sysenter_esp;
+        v->sysenter_eip = sysenter_eip;
+    }
+
+    if (m & Mtd_arch::Item::PAT)
+        v->g_pat = pat;
+
+    if (m & Mtd_arch::Item::EFER)
+        v->efer = efer;
+
+    return true;
+}
