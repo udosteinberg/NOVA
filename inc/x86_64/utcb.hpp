321bf75ce6fbb89c5b4ee818a393722f77c4712e
Udo Steinberg <udo@hypervisor.org>
2021-03-09 14:45:28 +0100

    x86: Modernized UTCB.

--- inc/x86_64/utcb.hpp
+++ inc/x86_64/utcb.hpp
@@ -1,5 +1,5 @@
 /*
- * User Thread Control Block (UTCB)
+ * User Thread Control Block (UTCB): Architecture-Independent
  *
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
@@ -22,104 +22,43 @@
 #pragma once
 
 #include "buddy.hpp"
-#include "util.hpp"
+#include "utcb_arch.hpp"
 
-class Cpu_regs;
-
-class Utcb_segment
-{
-    public:
-        uint16  sel, ar;
-        uint32  limit;
-        uint64  base;
-
-        ALWAYS_INLINE
-        inline void set_vmx (uint16 s, mword b, uint32 l, uint32 a)
-        {
-            sel   = static_cast<uint16>(s);
-            ar    = static_cast<uint16>((a >> 4 & 0x1f00) | (a & 0xff));
-            limit = static_cast<uint32>(l);
-            base  = b;
-        }
-};
-
-class Utcb_head
-{
-    protected:
-        mword items;
-
-    public:
-        mword   tls;
-};
-
-class Utcb_data
-{
-    protected:
-        union {
-            struct {
-                mword           mtd, inst_len, rip, rflags;
-                uint32          intr_state, actv_state;
-                union {
-                    struct {
-                        uint32  intr_info, intr_error;
-                    };
-                    uint64      inj;
-                };
-
-                mword           rax, rcx, rdx, rbx, rsp, rbp, rsi, rdi;
-                mword           r8,  r9,  r10, r11, r12, r13, r14, r15;
-                uint64          qual[2];
-                uint32          ctrl[2];
-                uint64          reserved;
-                mword           cr0, cr2, cr3, cr4;
-                uint64          pdpte[4];
-                mword           cr8, efer;
-                mword           dr7, sysenter_cs, sysenter_rsp, sysenter_rip;
-                Utcb_segment    es, cs, ss, ds, fs, gs, ld, tr, gd, id;
-                uint64          tsc_val, tsc_off;
-            };
-
-            mword mr[1];
-        };
-};
-
-class Utcb : public Utcb_head, private Utcb_data
+class Utcb final
 {
     private:
-        static mword const words = (PAGE_SIZE (0) - sizeof (Utcb_head)) / sizeof (mword);
+        union {
+            uintptr_t mr[Mtd_user::items];
+            Utcb_arch state;
+        };
 
     public:
-        [[nodiscard]] bool load_exc (Cpu_regs *);
-        [[nodiscard]] bool load_vmx (Cpu_regs *);
-        [[nodiscard]] bool load_svm (Cpu_regs *);
-        [[nodiscard]] bool save_exc (Cpu_regs *);
-        [[nodiscard]] bool save_vmx (Cpu_regs *);
-        [[nodiscard]] bool save_svm (Cpu_regs *);
+        inline auto arch() { return &state; }
 
-        inline mword ucnt() const { return static_cast<uint16>(items); }
-        inline mword tcnt() const { return static_cast<uint16>(items >> 16); }
-
-        inline mword ui() const { return min (words / 1, ucnt()); }
-        inline mword ti() const { return min (words / 2, tcnt()); }
-
-        ALWAYS_INLINE NONNULL
-        inline void save (Utcb *dst)
+        inline void copy (Mtd_user const mtd, Utcb *dst) const
         {
-            mword n = ui();
-
-            dst->items = items;
-#if 0
-            mword *d = dst->mr, *s = mr;
-            asm volatile ("rep; movsl" : "+D" (d), "+S" (s), "+c" (n) : : "memory");
-#else
-            for (unsigned long i = 0; i < n; i++)
+            for (unsigned i { 0 }; i < mtd.count(); i++)
                 dst->mr[i] = mr[i];
-#endif
         }
 
-        ALWAYS_INLINE
-        static inline void *operator new (size_t) { return Buddy::alloc (0, Buddy::Fill::BITS0); }
+        /*
+         * Allocate UTCB
+         *
+         * @return      Pointer to the UTCB (allocation success) or nullptr (allocation failure)
+         */
+        [[nodiscard]] static void *operator new (size_t) noexcept
+        {
+            static_assert (sizeof (Utcb) <= PAGE_SIZE (0));
+            return Buddy::alloc (0, Buddy::Fill::BITS0);
+        }
 
-        ALWAYS_INLINE
-        static inline void operator delete (void *ptr) { Buddy::free (ptr); }
+        /*
+         * Deallocate UTCB
+         *
+         * @param ptr   Pointer to the UTCB (or nullptr)
+         */
+        static void operator delete (void *ptr)
+        {
+            Buddy::free (ptr);
+        }
 };
---
+++ inc/x86_64/utcb_arch.hpp
@@ -0,0 +1,78 @@
+/*
+ * User Thread Control Block (UTCB): Architecture-Specific (x86)
+ *
+ * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2024 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "mtd_arch.hpp"
+
+class Cpu_regs;
+
+class Utcb_segment final
+{
+    public:
+        uint16_t    sel, ar;
+        uint32_t    limit;
+        uint64_t    base;
+
+        void set_vmx (uint16_t s, uint64_t b, uint32_t l, uint32_t a)
+        {
+            sel   = s;
+            ar    = static_cast<uint16_t>((a >> 4 & 0x1f00) | (a & 0xff));
+            limit = l;
+            base  = b;
+        }
+};
+
+class Utcb_arch final
+{
+    private:
+        uint64_t        rax, rcx, rdx, rbx, rsp, rbp, rsi, rdi;
+        uint64_t        r8,  r9,  r10, r11, r12, r13, r14, r15;
+        uint64_t        rfl, rip;
+        uint32_t        inst_len, inst_info, intr_state, actv_state;
+        uint64_t        qual[3], reserved;
+        uint32_t        ctrl_pri, ctrl_sec;
+        uint64_t        ctrl_ter;
+        uint64_t        intcpt_cr0, intcpt_cr4;
+        uint32_t        intcpt_exc, pfe_mask, pfe_match, tpr_threshold;
+        uint32_t        intr_info, intr_errc, vect_info, vect_errc;
+        Utcb_segment    cs, ss, ds, es, fs, gs, tr, ld, gd, id;
+        uint64_t        pdpte[4];
+        uint64_t        cr0, cr2, cr3, cr4, cr8, dr7, xcr0, xss;
+        uint64_t        apic_base, sysenter_cs, sysenter_esp, sysenter_eip;
+        uint64_t        pat, efer, star, lstar, fmask, kernel_gs_base, tsc_aux;
+
+        struct {
+            uint64_t    gst;
+            uint64_t    pio;
+            uint64_t    msr;
+        } sel;
+
+    public:
+        void load_exc (Mtd_arch, Cpu_regs const *);
+        void load_vmx (Mtd_arch, Cpu_regs const *);
+        void load_svm (Mtd_arch, Cpu_regs const *);
+        bool save_exc (Mtd_arch, Cpu_regs *) const;
+        bool save_vmx (Mtd_arch, Cpu_regs *) const;
+        bool save_svm (Mtd_arch, Cpu_regs *) const;
+};
+
+static_assert (__is_standard_layout (Utcb_arch) && sizeof (Utcb_arch) == 0x270);
