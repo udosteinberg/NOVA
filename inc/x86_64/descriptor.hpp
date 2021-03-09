45565de1616ffc3e406a15645fa60c07e6a19730
Udo Steinberg <udo@hypervisor.org>
2021-03-09 16:36:50 +0100

    x86: Modernized GDT/IDT descriptors.

--- inc/x86_64/descriptor.hpp
+++ inc/x86_64/descriptor.hpp
@@ -4,6 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
+ * Copyright (C) 2019-2024 Udo Steinberg, BlueRock Security, Inc.
+ *
  * This file is part of the NOVA microhypervisor.
  *
  * NOVA is free software: you can redistribute it and/or modify it
@@ -18,62 +20,121 @@
 
 #pragma once
 
+#include "byteorder.hpp"
 #include "compiler.hpp"
-#include "types.hpp"
+#include "macros.hpp"
+#include "std.hpp"
 
-class Descriptor
+/*
+ * Descriptor Base Class
+ */
+template<unsigned N> struct Descriptor
 {
-    protected:
-        enum Granularity
-        {
-            BYTES                   = 0u << 23,
-            PAGES                   = 1u << 23,
-        };
+    uint32_t val[N];
 
-        enum Size
-        {
-            BIT_16                  = 0u << 22,
-            BIT_32                  = 1u << 22
-        };
+    enum class Type : unsigned
+    {
+        // System Segments
+        SYS_LDT                 = 0x2,
+        SYS_TSS                 = 0x9,
+        SYS_CALL_GATE           = 0xc,
+        SYS_INTR_GATE           = 0xe,
+        SYS_TRAP_GATE           = 0xf,
 
-        enum Type
-        {
-            SYS_LDT                 =  2u << 8,
-            SYS_TASK_GATE           =  5u << 8,
-            SYS_TSS                 =  9u << 8,
-            SYS_CALL_GATE           = 12u << 8,
-            SYS_INTR_GATE           = 14u << 8,
-            SYS_TRAP_GATE           = 15u << 8,
+        // Data Segments
+        DATA_R                  = 0x10,
+        DATA_RA                 = 0x11,
+        DATA_RW                 = 0x12,
+        DATA_RWA                = 0x13,
+        DATA_DOWN_R             = 0x14,
+        DATA_DOWN_RA            = 0x15,
+        DATA_DOWN_RW            = 0x16,
+        DATA_DOWN_RWA           = 0x17,
 
-            DATA_R                  = 16u << 8,
-            DATA_RA                 = 17u << 8,
-            DATA_RW                 = 18u << 8,
-            DATA_RWA                = 19u << 8,
-            DATA_DOWN_R             = 20u << 8,
-            DATA_DOWN_RA            = 21u << 8,
-            DATA_DOWN_RW            = 22u << 8,
-            DATA_DOWN_RWA           = 23u << 8,
-
-            CODE_X                  = 24u << 8,
-            CODE_XA                 = 25u << 8,
-            CODE_XR                 = 26u << 8,
-            CODE_XRA                = 27u << 8,
-            CODE_CONF_X             = 28u << 8,
-            CODE_CONF_XA            = 29u << 8,
-            CODE_CONF_XR            = 30u << 8,
-            CODE_CONF_XRA           = 31u << 8
-        };
+        // Code Segments
+        CODE_X                  = 0x18,
+        CODE_XA                 = 0x19,
+        CODE_XR                 = 0x1a,
+        CODE_XRA                = 0x1b,
+        CODE_CONF_X             = 0x1c,
+        CODE_CONF_XA            = 0x1d,
+        CODE_CONF_XR            = 0x1e,
+        CODE_CONF_XRA           = 0x1f,
+    };
 };
 
-#pragma pack(1)
-class Pseudo_descriptor
+/*
+ * Descriptor: Page-Granular 64-Bit Code/Data Segment
+ */
+struct Descriptor_gdt_seg final : public Descriptor<2>
+{
+    explicit constexpr Descriptor_gdt_seg() : Descriptor { 0, 0 } {}
+
+    /*
+     * Constructor
+     *
+     * @param t Descriptor Type
+     * @param d Descriptor Privilege Level (0...3)
+     */
+    explicit constexpr Descriptor_gdt_seg (Type t, unsigned d) : Descriptor { 0, BIT (23) | BIT (21) | BIT (15) | d << 13 | std::to_underlying (t) << 8 } {}
+};
+
+static_assert (__is_standard_layout (Descriptor_gdt_seg) && sizeof (Descriptor_gdt_seg) == 8);
+
+/*
+ * Descriptor: Byte-Granular 64-Bit System Segment
+ */
+struct Descriptor_gdt_sys final : public Descriptor<4>
+{
+    explicit constexpr Descriptor_gdt_sys() : Descriptor { 0, 0, 0, 0 } {}
+
+    /*
+     * Constructor
+     *
+     * @param t Descriptor Type
+     * @param b Segment Base Address
+     * @param l Segment Limit
+     */
+    explicit constexpr Descriptor_gdt_sys (Type t, uint64_t b, uint32_t l) : Descriptor { static_cast<uint32_t>(b << 16 | (l & BIT_RANGE (15, 0))),
+                                                                                          static_cast<uint32_t>((b & BIT_RANGE (31, 24)) | (l & BIT_RANGE (19, 16)) | BIT (15) | std::to_underlying (t) << 8 | (b >> 16 & BIT_RANGE (7, 0))),
+                                                                                          static_cast<uint32_t>(b >> 32), 0 } {}
+};
+
+static_assert (__is_standard_layout (Descriptor_gdt_sys) && sizeof (Descriptor_gdt_sys) == 16);
+
+/*
+ * Descriptor: 64-Bit IDT Gate
+ */
+struct Descriptor_idt final : public Descriptor<4>
+{
+    explicit constexpr Descriptor_idt() : Descriptor { 0, 0, 0, 0 } {}
+
+    /*
+     * Constructor
+     *
+     * @param d Descriptor Privilege Level (0...3)
+     * @param i Interrupt Stack Table Slot (0...7)
+     * @param s Segment Selector (Destination Code Segment)
+     * @param e Entry Instruction Pointer (Interrupt Handler)
+     */
+    explicit constexpr Descriptor_idt (unsigned d, unsigned i, uint16_t s, uint64_t e) : Descriptor { static_cast<uint32_t>(s << 16 | (e & BIT_RANGE (15, 0))),
+                                                                                                      static_cast<uint32_t>((e & BIT_RANGE (31, 16)) | BIT (15) | d << 13 | std::to_underlying (Type::SYS_INTR_GATE) << 8 | i),
+                                                                                                      static_cast<uint32_t>(e >> 32), 0 } {}
+};
+
+static_assert (__is_standard_layout (Descriptor_idt) && sizeof (Descriptor_idt) == 16);
+
+/*
+ * Pseudo Descriptor for LGDT/LIDT
+ */
+class Pseudo_descriptor final
 {
     private:
-        uint16  limit;
-        mword   base;
+        Unaligned_le<uint16_t>      limit;
+        Unaligned_le<uintptr_t>     base;
 
     public:
-        ALWAYS_INLINE
-        inline Pseudo_descriptor (mword l, mword b) : limit (static_cast<uint16>(l)), base (b) {}
+        explicit Pseudo_descriptor (void *b, size_t l) : limit { static_cast<uint16_t>(l - 1) }, base { reinterpret_cast<uintptr_t>(b) } {}
 };
-#pragma pack()
+
+static_assert (__is_standard_layout (Pseudo_descriptor) && alignof (Pseudo_descriptor) == 1 && sizeof (Pseudo_descriptor) == 10);
--- inc/x86_64/idt.hpp
+++ inc/x86_64/idt.hpp
@@ -4,7 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
- * Copyright (C) 2012 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2024 Udo Steinberg, BlueRock Security, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -23,27 +24,16 @@
 #include "descriptor.hpp"
 #include "vectors.hpp"
 
-class Idt : public Descriptor
+class Idt final
 {
-    private:
-        uint32 val[sizeof (mword) / 2];
-
-        ALWAYS_INLINE
-        inline void set (Type type, unsigned dpl, unsigned selector, mword offset)
-        {
-            val[0] = static_cast<uint32>(selector << 16 | (offset & 0xffff));
-            val[1] = static_cast<uint32>((offset & 0xffff0000) | 1u << 15 | dpl << 13 | type);
-            val[2] = static_cast<uint32>(offset >> 32);
-        }
-
     public:
-        static Idt idt[VEC_MAX];
+        static inline constinit ALIGNED(8) Descriptor_idt idt[VEC_MAX];
 
         static void build();
 
-        ALWAYS_INLINE
-        static inline void load()
+        static void load()
         {
-            asm volatile ("lidt %0" : : "m" (Pseudo_descriptor (sizeof (idt) - 1, reinterpret_cast<mword>(idt))));
+            Pseudo_descriptor const d { idt, sizeof (idt) };
+            asm volatile ("lidt %0" : : "m" (d));
         }
 };
