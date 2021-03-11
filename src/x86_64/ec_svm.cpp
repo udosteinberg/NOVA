a511c4fd43725ca7fca50d4c8cb9150f3c98f1ed
Udo Steinberg <udo@hypervisor.org>
2021-03-11 16:38:46 +0100

    x86: Modernized register file.

--- src/x86_64/ec_svm.cpp
+++ src/x86_64/ec_svm.cpp
@@ -36,7 +36,7 @@ void Ec::svm_exception (uint64_t reason)
     switch (reason) {
 
         default:
-            current->regs.dst_portal = reason;
+            current->exc_regs().set_ep (reason);
             break;
 
         case 0x47:          // #NM
@@ -72,7 +72,7 @@ void Ec::handle_svm()
             ret_user_vmrun();
     }
 
-    current->regs.dst_portal = reason;
+    current->exc_regs().set_ep (reason);
 
     send_msg<ret_user_vmrun>();
 }
