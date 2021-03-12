e732475a5f71faef927c4258c8520a12eb815640
Udo Steinberg <udo@hypervisor.org>
2021-03-12 14:07:19 +0100

    x86: Modernized VPID.

---
+++ src/x86_64/vpid.cpp
@@ -0,0 +1,23 @@
+/*
+ * Virtual Processor Identifier (VPID)
+ *
+ * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "vpid.hpp"
+
+Atomic<uint16_t> Vpid::allocator { 0 };
