915fc936800410398dc4088d58ff00d0e61cc774
Udo Steinberg <udo@hypervisor.org>
2021-03-15 14:25:00 +0100

    x86: Modernized timeout infrastructure.

--- inc/x86_64/ec.hpp
+++ inc/x86_64/ec.hpp
@@ -161,8 +161,7 @@ class Ec : public Kobject, public Queue<Sc>
         ALWAYS_INLINE
         inline void clr_timeout()
         {
-            if (timeout.active()) [[unlikely]]
-                timeout.dequeue();
+            timeout.dequeue();
         }
 
         ALWAYS_INLINE
