a6a07a3c2c2580a53f527234dae287d2313109c1
Udo Steinberg <udo@hypervisor.org>
2021-03-15 14:25:00 +0100

    x86: Modernized timeout infrastructure.

--- src/x86_64/timeout_budget.cpp
+++ src/x86_64/timeout_budget.cpp
@@ -2,6 +2,7 @@
  * Budget Timeout
  *
  * Copyright (C) 2014 Udo Steinberg, FireEye, Inc.
+ * Copyright (C) 2019-2021 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -21,7 +22,7 @@
 #include "timeout_budget.hpp"
 
 INIT_PRIORITY (PRIO_LOCAL)
-Timeout_budget Timeout_budget::budget;
+Timeout_budget Timeout_budget::timeout;
 
 void Timeout_budget::trigger()
 {
