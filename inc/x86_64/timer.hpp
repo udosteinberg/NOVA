ffeb9ca01499ba6922358db41040c14c78836062
Udo Steinberg <udo@hypervisor.org>
2021-03-15 14:25:00 +0100

    x86: Modernized timeout infrastructure.

--- inc/x86_64/timer.hpp
+++ inc/x86_64/timer.hpp
@@ -42,5 +42,7 @@ class Timer : private Stc
         static inline void set_time (uint64 t)
         {
             Msr::write (Msr::Register::IA32_TSC, t);
+
+            Timeout::sync();
         }
 };
