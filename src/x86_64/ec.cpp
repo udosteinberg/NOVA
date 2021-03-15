987660ad8e85df8e59fc2a633b821545218c0b51
Udo Steinberg <udo@hypervisor.org>
2021-03-15 13:42:37 +0100

    x86: Modernized SC.

--- src/x86_64/ec.cpp
+++ src/x86_64/ec.cpp
@@ -102,13 +102,15 @@ void Ec::destroy()
 
 void Ec::create_idle()
 {
+    Status s;
+
     auto const ec { Ec::create (Cpu::id, idle) };
-    auto const sc { new Sc (nullptr, Cpu::id, ec) };
+    auto const sc { Pd::nova.create_sc (s, &Space_obj::nova, Space_obj::Selector::NOVA_CPU + Cpu::id, ec, Cpu::id, 1000, 0, 0) };
 
     assert (ec && sc);
 
     current = ec;
-    Sc::current = sc;
+    Scheduler::set_current (sc);
 }
 
 bool Ec::load_root (Space_hst *hst, uint64_t mod_s, uint64_t mod_e, uint64_t &img_s, uint64_t &img_e, uint64_t &msize, uint64_t &entry)
@@ -234,7 +236,7 @@ void Ec::create_root()
     }
 
     auto const ec { Pd::root->create_ec (s, obj, Space_obj::selectors - 4, Cpu::id, 0, 0, Space_hst::utcb_addr(), BIT (2) | BIT (1)) };
-    auto const sc { new Sc (Pd::root, NUM_EXC + 2, ec, Cpu::id, Sc::default_prio, Sc::default_quantum) };
+    auto const sc { Pd::root->create_sc (s, obj, Space_obj::selectors - 5, ec, Cpu::id, 1000, Scheduler::priorities - 1, 0) };
 
     if (!ec || !sc) [[unlikely]]
         return;
@@ -253,7 +255,7 @@ void Ec::create_root()
 
     trace (TRACE_ROOT, "ROOT: Invoking entry point %#lx %s measuring %#lx bytes", entry, m ? "after" : "without", msize);
 
-    sc->remote_enqueue();
+    Scheduler::unblock (sc);
 
     Console::flush();
 }
@@ -281,13 +283,13 @@ void Ec::help (Ec *ec, cont_t c)
     // Preempt long helping chains, including livelocks
     Cpu::preemption_point();
     if (Cpu::hazard & Hazard::SCHED) [[unlikely]]
-        Sc::schedule (false);
+        Scheduler::schedule (false);
 
     Counter::helping.inc();
 
     ec->activate();
 
-    Sc::schedule (true);
+    Scheduler::schedule (true);
 }
 
 void Ec::idle (Ec *const self)
