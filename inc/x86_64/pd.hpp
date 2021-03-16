58dba9c3d24fc71b2eeba4cdd7c8fd74841726df
Udo Steinberg <udo@hypervisor.org>
2021-03-16 15:01:36 +0100

    x86: Modernized PT.

--- inc/x86_64/pd.hpp
+++ inc/x86_64/pd.hpp
@@ -34,6 +34,7 @@ class Space_obj;
 class Space_pio;
 
 class Ec;
+class Pt;
 class Sc;
 
 class Pd final : public Kobject
@@ -89,4 +90,5 @@ class Pd final : public Kobject
         static Pd *create_pd (Status &, Space_obj *, unsigned long, unsigned);
         static Ec *create_ec (Status &, Space_obj *, unsigned long, Pd *, cpu_t, uintptr_t, uintptr_t, uintptr_t, uint8_t);
         static Sc *create_sc (Status &, Space_obj *, unsigned long, Ec *, cpu_t, uint16_t, uint8_t, uint16_t);
+        static Pt *create_pt (Status &, Space_obj *, unsigned long, Ec *, uintptr_t);
 };
