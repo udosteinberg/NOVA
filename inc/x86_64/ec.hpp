66e9bd7e556fe6fd8a99b8431fb04796eece0c5d
Udo Steinberg <udo@hypervisor.org>
2021-03-17 16:02:43 +0100

    x86: Modernized SVM.

--- inc/x86_64/ec.hpp
+++ inc/x86_64/ec.hpp
@@ -74,8 +74,7 @@ class Ec : private Kobject, public Timeout_hypercall, public Queue<Ec>::Element,
         static bool handle_exc_gp (Exc_regs *);
         static bool handle_exc_pf (Exc_regs *);
 
-        [[noreturn]]
-        static inline void svm_exception (mword);
+        [[noreturn]] static void svm_exception (uint64_t);
 
         [[noreturn]]
         static inline void vmx_exception();
