9db549d2fb2db573cb3f0ad98e6019dcc5b21e45
Udo Steinberg <udo@hypervisor.org>
2021-03-18 15:19:19 +0100

    x86: Modernized PCI.

--- inc/x86_64/pci.hpp
+++ inc/x86_64/pci.hpp
@@ -5,6 +5,7 @@
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
  * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2022 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -24,69 +25,69 @@
 #include "list.hpp"
 #include "memory.hpp"
 #include "slab.hpp"
+#include "std.hpp"
 
 class Smmu;
 
-class Pci : public List<Pci>
+class Pci final : public List<Pci>
 {
     friend class Acpi_table_mcfg;
 
-    private:
-        mword  const        reg_base;
-        uint16 const        rid;
-        uint16 const        lev;
-        Smmu *              smmu;
-
-        static unsigned     bus_base;
-        static Paddr        cfg_base;
-        static size_t       cfg_size;
-
-        static Pci *        list;
-        static Slab_cache   cache;
-
-        static struct quirk_map
+    public:
+        enum class Register8 : unsigned
         {
-            uint16 vid, did;
-            void (Pci::*func)();
-        } map[];
-
-        enum Register
-        {
-            REG_VID         = 0x0,
-            REG_DID         = 0x2,
-            REG_HDR         = 0xe,
-            REG_SBUSN       = 0x19,
-            REG_MAX         = 0xfff,
+            HTYPE   = 0xe,      // Header Type
+            BUS_PRI = 0x18,     // Bus Number: Primary
+            BUS_SEC = 0x19,     // Bus Number: Secondary
+            BUS_SUB = 0x1a,     // Bus Number: Subordinate
         };
 
-        template <typename T>
-        ALWAYS_INLINE
-        inline unsigned read (Register r) { return *reinterpret_cast<T volatile *>(reg_base + r); }
-
-        template <typename T>
-        ALWAYS_INLINE
-        inline void write (Register r, T v) { *reinterpret_cast<T volatile *>(reg_base + r) = v; }
-
-        ALWAYS_INLINE
-        static inline Pci *find_dev (unsigned long r)
+        enum class Register16 : unsigned
         {
-            for (Pci *pci = list; pci; pci = pci->next)
-                if (pci->rid == r)
-                    return pci;
+            VID     = 0x0,      // Vendor ID
+            DID     = 0x2,      // Device ID
+            CMD     = 0x4,      // Command Register
+            STS     = 0x6,      // Status Register
+        };
 
-            return nullptr;
-        }
+        enum class Register32 : unsigned
+        {
+            DID_VID = 0x0,      // Device ID + Vendor ID
+            STS_CMD = 0x4,      // Status Register + Command Register
+            CCP_RID = 0x8,      // Class Codes + Programming Interface + Revision ID
+            BAR0    = 0x10,     // Base Address Register 0
+            BAR1    = 0x14,     // Base Address Register 1
+        };
 
-    public:
+        // Compose BDF triplet
+        static constexpr auto bdf (unsigned b, unsigned d, unsigned f) { return static_cast<uint16>(b << 8 | d << 3 | f); }
+
+        // Configuration Space Address
+        static void cfg_addr (uint16 bdf, unsigned r) { Io::out<uint32>(0xcf8, BIT (31) | bdf << 8 | r); }
+
+        // Configuration Space Read
+        static auto cfg_read (uint16 bdf, Register8  r) { cfg_addr (bdf, std::to_underlying (r)); auto p { 0xcfc | (std::to_underlying (r) & 3) }; return Io::in<uint8> (p); }
+        static auto cfg_read (uint16 bdf, Register16 r) { cfg_addr (bdf, std::to_underlying (r)); auto p { 0xcfc | (std::to_underlying (r) & 2) }; return Io::in<uint16>(p); }
+        static auto cfg_read (uint16 bdf, Register32 r) { cfg_addr (bdf, std::to_underlying (r)); auto p { 0xcfc };                                return Io::in<uint32>(p); }
+
+        // Configuration Space Write
+        static void cfg_write (uint16 bdf, Register8  r, uint8  v) { cfg_addr (bdf, std::to_underlying (r)); auto p { 0xcfc | (std::to_underlying (r) & 3) }; Io::out<uint8> (p, v); }
+        static void cfg_write (uint16 bdf, Register16 r, uint16 v) { cfg_addr (bdf, std::to_underlying (r)); auto p { 0xcfc | (std::to_underlying (r) & 2) }; Io::out<uint16>(p, v); }
+        static void cfg_write (uint16 bdf, Register32 r, uint32 v) { cfg_addr (bdf, std::to_underlying (r)); auto p { 0xcfc };                                Io::out<uint32>(p, v); }
+
+        // Configuration Space Read/Write
+        static void cfg_rd_wr (uint16 bdf, Register8  r, uint8  s) { cfg_addr (bdf, std::to_underlying (r)); auto p { 0xcfc | (std::to_underlying (r) & 3) }; Io::out<uint8> (p, Io::in<uint8> (p) | s); }
+        static void cfg_rd_wr (uint16 bdf, Register16 r, uint16 s) { cfg_addr (bdf, std::to_underlying (r)); auto p { 0xcfc | (std::to_underlying (r) & 2) }; Io::out<uint16>(p, Io::in<uint16>(p) | s); }
+        static void cfg_rd_wr (uint16 bdf, Register32 r, uint32 s) { cfg_addr (bdf, std::to_underlying (r)); auto p { 0xcfc };                                Io::out<uint32>(p, Io::in<uint32>(p) | s); }
+
         Pci (unsigned, unsigned);
 
-        ALWAYS_INLINE
-        static inline void *operator new (size_t) { return cache.alloc(); }
+        static void init (unsigned = 0, unsigned = 0);
 
         ALWAYS_INLINE
         static inline void claim_all (Smmu *s)
         {
-            for (Pci *pci = list; pci; pci = pci->next)
+            for (auto pci { list }; pci; pci = pci->next)
                 if (!pci->smmu)
                     pci->smmu = s;
         }
@@ -94,30 +95,75 @@ class Pci : public List<Pci>
         ALWAYS_INLINE
         static inline bool claim_dev (Smmu *s, unsigned r)
         {
-            Pci *pci = find_dev (r);
+            auto pci { find_dev (r) };
 
             if (!pci)
                 return false;
 
-            unsigned l = pci->lev;
+            unsigned const l { pci->lev };
+
             do pci->smmu = s; while ((pci = pci->next) && pci->lev > l);
 
             return true;
         }
 
-        static void init (unsigned = 0, unsigned = 0);
-
-        ALWAYS_INLINE
-        static inline unsigned phys_to_rid (Paddr p)
-        {
-            return p - cfg_base < cfg_size ? static_cast<unsigned>((bus_base << 8) + (p - cfg_base) / PAGE_SIZE) : ~0U;
-        }
-
         ALWAYS_INLINE
         static inline Smmu *find_smmu (unsigned long r)
         {
-            Pci *pci = find_dev (r);
+            auto const pci { find_dev (r) };
 
             return pci ? pci->smmu : nullptr;
+        }
+
+        [[nodiscard]] ALWAYS_INLINE
+        static inline void *operator new (size_t) noexcept
+        {
+            return cache.alloc();
+        }
+
+        ALWAYS_INLINE
+        static inline void operator delete (void *ptr)
+        {
+            if (EXPECT_TRUE (ptr))
+                cache.free (ptr);
+        }
+
+    private:
+        uintptr_t   const   reg_base;
+        uint16      const   rid;
+        uint16      const   lev;
+        Smmu *              smmu;
+
+        static          Slab_cache  cache;                      // PCI Slab Cache
+        static inline   Pci *       list    { nullptr };        // PCI List
+        static inline   uintptr_t   mmap    { MMAP_GLB_PCIE };  // PCI Memory Map Pointer
+        static inline   unsigned    bus_base;
+        static inline   Paddr       cfg_base;
+        static inline   size_t      cfg_size;
+
+        static struct quirk_map
+        {
+            uint32 didvid;
+            void (Pci::*func)();
+        } map[];
+
+        // ECAM Read
+        auto cfg_read (Register8  r) const { return *reinterpret_cast<uint8  volatile *>(reg_base + std::to_underlying (r)); }
+        auto cfg_read (Register16 r) const { return *reinterpret_cast<uint16 volatile *>(reg_base + std::to_underlying (r)); }
+        auto cfg_read (Register32 r) const { return *reinterpret_cast<uint32 volatile *>(reg_base + std::to_underlying (r)); }
+
+        // ECAM Write
+        void cfg_write (Register8  r, uint8  v) const { *reinterpret_cast<uint8  volatile *>(reg_base + std::to_underlying (r)) = v; }
+        void cfg_write (Register16 r, uint16 v) const { *reinterpret_cast<uint16 volatile *>(reg_base + std::to_underlying (r)) = v; }
+        void cfg_write (Register32 r, uint32 v) const { *reinterpret_cast<uint32 volatile *>(reg_base + std::to_underlying (r)) = v; }
+
+        ALWAYS_INLINE
+        static inline Pci *find_dev (unsigned long r)
+        {
+            for (auto pci { list }; pci; pci = pci->next)
+                if (pci->rid == r)
+                    return pci;
+
+            return nullptr;
         }
 };
