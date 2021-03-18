1bcc4dbbde5c87597a8135f5657bf224fa949a9f
Udo Steinberg <udo@hypervisor.org>
2021-03-18 15:14:11 +0100

    x86: Modernized FPU.

--- src/x86_64/fpu.cpp
+++ src/x86_64/fpu.cpp
@@ -4,6 +4,9 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2022 Udo Steinberg, BedRock Systems, Inc.
+ *
  * This file is part of the NOVA microhypervisor.
  *
  * NOVA is free software: you can redistribute it and/or modify it
@@ -18,5 +21,10 @@
 
 #include "fpu.hpp"
 
-INIT_PRIORITY (PRIO_SLAB)
-Slab_cache Fpu::cache (sizeof (Fpu), 16);
+void Fpu::init()
+{
+}
+
+void Fpu::fini()
+{
+}
