16f049bfc8485eb9726a256f91fa2afe1e0f730a
Udo Steinberg <udo@hypervisor.org>
2021-03-24 10:18:40 +0100

    x86: Modernized IO.

--- inc/x86_64/io.hpp
+++ inc/x86_64/io.hpp
@@ -4,6 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
+ *
  * This file is part of the NOVA microhypervisor.
  *
  * NOVA is free software: you can redistribute it and/or modify it
@@ -20,12 +22,12 @@
 
 #include "compiler.hpp"
 
-class Io
+class Io final
 {
     public:
         template <typename T>
         ALWAYS_INLINE
-        static inline unsigned in (unsigned port)
+        static T in (unsigned port)
         {
             T val;
             asm volatile ("in %w1, %0" : "=a" (val) : "Nd" (port));
@@ -34,7 +36,7 @@ class Io
 
         template <typename T>
         ALWAYS_INLINE
-        static inline void out (unsigned port, T val)
+        static void out (unsigned port, T val)
         {
             asm volatile ("out %0, %w1" : : "a" (val), "Nd" (port));
         }
