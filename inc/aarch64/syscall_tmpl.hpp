e1a525dd0d3ef61fbbe17ec11da535f6cecdbb17
Udo Steinberg <udo@hypervisor.org>
2021-03-24 18:03:01 +0100

    ARM: Unified EC,SC,PT,SM,HIP,MBUF class.

---
+++ inc/aarch64/syscall_tmpl.hpp
@@ -0,0 +1,20 @@
+/*
+ * System-Call Interface
+ *
+ * Copyright (C) 2019-2020 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+template void Ec::send_msg<Ec_arch::ret_user_vmexit> (Ec *);
