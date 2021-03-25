25b742e2999db08f83e05cb7862352ab2360bf89
Udo Steinberg <udo@hypervisor.org>
2021-03-25 14:04:21 +0100

    x86: Added multiboot v1/v2 launch.

--- inc/x86_64/multiboot.hpp
+++ inc/x86_64/multiboot.hpp
@@ -1,8 +1,11 @@
 /*
- * Multiboot Support
+ * Multiboot Definitions (v1, v2)
  *
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2021 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -18,68 +21,8 @@
 
 #pragma once
 
-#include "compiler.hpp"
+#define MULTIBOOT_HEADER_MAGIC_V1   0x1badb002
+#define MULTIBOOT_LOADER_MAGIC_V1   0x2badb002
 
-/*
- * Multiboot Information Structure
- */
-class Multiboot
-{
-    public:
-        enum
-        {
-            MAGIC           = 0x2badb002,
-            MEMORY          = 1ul << 0,
-            BOOT_DEVICE     = 1ul << 1,
-            CMDLINE         = 1ul << 2,
-            MODULES         = 1ul << 3,
-            SYMBOLS         = 1ul << 4 | 1ul << 5,
-            MEMORY_MAP      = 1ul << 6,
-            DRIVES          = 1ul << 7,
-            CONFIG_TABLE    = 1ul << 8,
-            LOADER_NAME     = 1ul << 9,
-            APM_TABLE       = 1ul << 10,
-            VBE_INFO        = 1ul << 11
-        };
-
-        uint32  flags;              // 0
-        uint32  mem_lower;          // 4
-        uint32  mem_upper;          // 8
-        uint32  boot_device;        // 12
-        uint32  cmdline;            // 16
-        uint32  mods_count;         // 20
-        uint32  mods_addr;          // 24
-        uint32  syms[4];            // 28,32,36,40
-        uint32  mmap_len;           // 44
-        uint32  mmap_addr;          // 48
-        uint32  drives_length;      // 52
-        uint32  drives_addr;        // 56
-        uint32  config_table;       // 60
-        uint32  loader_name;        // 64
-};
-
-/*
- * Multiboot Module
- */
-class Multiboot_module
-{
-    public:
-        uint32  s_addr;
-        uint32  e_addr;
-        uint32  cmdline;
-        uint32  reserved;
-};
-
-/*
- * Multiboot Memory Map
- */
-#pragma pack(1)
-class Multiboot_mmap
-{
-    public:
-        uint32  size;
-        uint64  addr;
-        uint64  len;
-        uint32  type;
-};
-#pragma pack()
+#define MULTIBOOT_HEADER_MAGIC_V2   0xe85250d6
+#define MULTIBOOT_LOADER_MAGIC_V2   0x36d76289
