c90dc8a1f5cd97a5797cc001d6d5c0dcedc83277
Udo Steinberg <udo@hypervisor.org>
2021-03-25 16:54:52 +0100

    x86: Modernized IOAPIC.

--- src/x86_64/ioapic.cpp
+++ src/x86_64/ioapic.cpp
@@ -4,7 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
- * Copyright (C) 2012 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2021 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -25,16 +26,21 @@
 INIT_PRIORITY (PRIO_SLAB)
 Slab_cache Ioapic::cache (sizeof (Ioapic), 8);
 
-Ioapic *Ioapic::list;
-
-Ioapic::Ioapic (Paddr p, unsigned i, unsigned g) : List<Ioapic> (list), reg_base ((hwdev_addr -= PAGE_SIZE) | (p & OFFS_MASK)), gsi_base (g), id (i), rid (0)
+Ioapic::Ioapic (Paddr p, unsigned i, unsigned g) : List (list), reg_base (mmap | (p & OFFS_MASK)), gsi_base (g), id (i)
 {
+    mmap += PAGE_SIZE;
+
 #if 0   // FIXME
     Pd::kern.Space_mem::delreg (p & ~OFFS_MASK);
 #endif
 
     Pd::kern.Space_mem::insert (reg_base, 0, Hpt::HPT_NX | Hpt::HPT_G | Hpt::HPT_UC | Hpt::HPT_W | Hpt::HPT_P, p & ~OFFS_MASK);
 
-    trace (TRACE_INTR, "APIC:%#lx ID:%#x VER:%#x IRT:%#x PRQ:%u GSI:%u",
-           p, i, version(), irt_max(), prq(), gsi_base);
+    trace (TRACE_INTR, "APIC: %#010lx ID:%#x VER:%#x GSI:%u-%u", p, i, ver(), gsi_base, gsi_base + mre());
+}
+
+void Ioapic::init()
+{
+    for (unsigned i = 0; i <= mre(); i++)
+        set_cfg (gsi_base + i);
 }
