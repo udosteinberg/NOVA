e843091a1481e8545b54101219381a20d5fbfcaa
Udo Steinberg <udo@hypervisor.org>
2021-03-25 14:04:21 +0100

    x86: Added multiboot launch.

--- inc/x86_64/cmdline.hpp
+++ inc/x86_64/cmdline.hpp
@@ -34,7 +34,7 @@ class Cmdline final
         static inline bool nouart   { false };
         static inline bool novpid   { false };
 
-        static void init (Paddr);
+        static void init();
 
     private:
         static constexpr struct
