9030fa3ff5e290141fff11bc80e47a9a74d7780b
Udo Steinberg <udo@hypervisor.org>
2021-04-09 14:52:49 +0200

    x86: Added PIO bitmap.

---
+++ inc/x86_64/bitmap_pio.hpp
@@ -0,0 +1,74 @@
+/*
+ * PIO Bitmap
+ *
+ * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2021 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "atomic.hpp"
+#include "buddy.hpp"
+#include "compiler.hpp"
+
+class Bitmap_pio
+{
+    private:
+        static constexpr auto sels = BITN (16);
+        static constexpr auto bits = 8 * sizeof (uintptr_t);
+
+        // I/O Bitmap
+        struct {
+            Atomic<uintptr_t> bmp { ~0UL };
+        } io[sels / bits];
+
+        inline auto &sel_to_bmp (unsigned long s)       { return io[s % sels / bits].bmp; }
+        inline auto &sel_to_bmp (unsigned long s) const { return io[s % sels / bits].bmp; }
+
+        static inline auto sel_bmask (unsigned long s) { return BITN (s % bits); }
+
+    public:
+        static inline bool sel_valid (unsigned long s) { return s < sels; }
+
+        inline void clr (unsigned long s)       {        sel_to_bmp (s) &= ~sel_bmask (s); }
+        inline void set (unsigned long s)       {        sel_to_bmp (s) |=  sel_bmask (s); }
+        inline bool tst (unsigned long s) const { return sel_to_bmp (s) &   sel_bmask (s); }
+
+        /*
+         * Allocate PIO bitmap
+         *
+         * @return      Pointer to the PIO bitmap (allocation success) or nullptr (allocation failure)
+         */
+        NODISCARD ALWAYS_INLINE
+        static inline void *operator new (size_t) noexcept
+        {
+            static_assert (sizeof (Bitmap_pio) == PAGE_SIZE << 1);
+            return Buddy::alloc (1);
+        }
+
+        /*
+         * Deallocate PIO bitmap
+         *
+         * @param ptr   Pointer to the PIO bitmap (or nullptr)
+         */
+        ALWAYS_INLINE
+        static inline void operator delete (void *ptr)
+        {
+            if (EXPECT_TRUE (ptr))
+                Buddy::free (ptr);
+        }
+};
