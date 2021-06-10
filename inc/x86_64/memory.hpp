8f9cc96b73fd0b98e72630f35ee74de41810ddf6
Udo Steinberg <udo@hypervisor.org>
2021-06-10 09:57:23 +0200

    x86: Added load address flexibility.

--- inc/x86_64/memory.hpp
+++ inc/x86_64/memory.hpp
@@ -59,7 +59,7 @@
 #define MMAP_GLB_MAP0   VIRT_ADDR (511, 510, 496,   0)          //   4M + gap
 #define MMAP_GLB_UART   VIRT_ADDR (511, 510, 488,   0)          //  16M
 #define MMAP_GLB_MMIO   VIRT_ADDR (511, 510, 448,   0)          //  64M
-#define LINK_ADDR       0xffffffff81000000
+#define LINK_ADDR       VIRT_ADDR (511, 510,   0,   0)          // 896M
 
 #define MMAP_GLB_CPUS   VIRT_ADDR (511, 509,   0,   0)          //   1G (262144 CPUs)
 #define MMAP_GLB_PCIE   MMAP_GLB_CPUS
