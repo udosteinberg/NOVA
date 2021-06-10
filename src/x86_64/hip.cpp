c957c99fd531b8358501ef2f145a861556283ef6
Udo Steinberg <udo@hypervisor.org>
2021-06-10 09:57:23 +0200

    x86: Added load address flexibility.

--- src/x86_64/hip.cpp
+++ src/x86_64/hip.cpp
@@ -6,6 +6,7 @@
  *
  * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
  * Copyright (C) 2014 Udo Steinberg, FireEye, Inc.
+ * Copyright (C) 2019 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -110,7 +111,7 @@ void Hip::add_mod (Hip_mem *&mem, mword addr, size_t count)
 void Hip::add_mhv (Hip_mem *&mem)
 {
     mem->addr = LOAD_ADDR;
-    mem->size = reinterpret_cast<mword>(&LINK_E) - mem->addr;
+    mem->size = reinterpret_cast<mword>(&NOVA_HPAE) - mem->addr;
     mem->type = Hip_mem::HYPERVISOR;
     mem++;
 }
