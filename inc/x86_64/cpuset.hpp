a412f357f5dbaf1d77e6f23277ac15a2d1e4dd6e
Udo Steinberg <udo@hypervisor.org>
2021-06-21 15:16:33 +0200

    x86: Modernized CPU set.

--- inc/x86_64/cpuset.hpp
+++ inc/x86_64/cpuset.hpp
@@ -4,7 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
- * Copyright (C) 2012 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2012-2013 Udo Steinberg, Intel Corporation.
+ * Copyright (C) 2019-2022 Udo Steinberg, BedRock Systems, Inc.
  *
  * This file is part of the NOVA microhypervisor.
  *
@@ -20,34 +21,36 @@
 
 #pragma once
 
+#include "atomic.hpp"
+#include "macros.hpp"
 #include "types.hpp"
 
 class Cpuset
 {
     private:
-        mword val;
+        Atomic<uintptr_t> msk[1] { 0 };
 
-        template <typename T>
-        static inline bool test_set_bit (T &val, unsigned long bit)
-        {
-            bool ret;
-            asm volatile ("lock; bts%z1 %2, %1; setc %0" : "=q" (ret), "+m" (val) : "ir" (bit) : "cc");
-            return ret;
-        }
+        static constexpr auto bits { 8 * sizeof (*msk) };
+
+        inline auto &cpu_to_msk (unsigned c)       { return msk[c / bits]; }
+        inline auto &cpu_to_msk (unsigned c) const { return msk[c / bits]; }
+
+        static inline auto cpu_to_bit (unsigned c) { return BITN (c % bits); }
 
     public:
         ALWAYS_INLINE
-        inline explicit Cpuset() : val (0) {}
+        inline bool tst (unsigned c) const { return cpu_to_msk (c) & cpu_to_bit (c); }
 
         ALWAYS_INLINE
-        inline bool chk (unsigned cpu) const { return val & 1UL << cpu; }
+        inline void clr (unsigned c) { cpu_to_msk (c) &= ~cpu_to_bit (c); }
 
         ALWAYS_INLINE
-        inline bool set (unsigned cpu) { return !test_set_bit (val, cpu); }
+        inline bool tas (unsigned c) { return cpu_to_msk (c).test_and_set (cpu_to_bit (c)); }
 
         ALWAYS_INLINE
-        inline void clr (unsigned cpu) { __atomic_and_fetch (&val, ~(1UL << cpu), __ATOMIC_SEQ_CST); }
-
-        ALWAYS_INLINE
-        inline void merge (Cpuset &s) { __atomic_or_fetch (&val, s.val, __ATOMIC_SEQ_CST); }
+        inline void merge (Cpuset const &x)
+        {
+            for (unsigned i = 0; i < sizeof (msk) / sizeof (*msk); i++)
+                msk[i] |= x.msk[i];
+        }
 };
