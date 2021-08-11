cf979e9a959892e3577115b9b394bf52a683b4e9
Udo Steinberg <udo@hypervisor.org>
2021-08-11 17:07:12 +0200

    x86: Modernized boot lock.

--- src/x86_64/cpu.cpp
+++ src/x86_64/cpu.cpp
@@ -42,8 +42,6 @@ char const * const Cpu::vendor_string[] =
     "AuthenticAMD"
 };
 
-mword       Cpu::boot_lock;
-
 // Order of these matters
 unsigned    Cpu::online;
 uint8       Cpu::acpi_id[NUM_CPU];
@@ -217,5 +215,5 @@ void Cpu::init()
 
     Hip::hip->add_cpu();
 
-    boot_lock++;
+    boot_lock.unlock();
 }
