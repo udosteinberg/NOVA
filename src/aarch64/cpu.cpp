91c92797e127c38e8c2c77170fa5dda1acb20378
Udo Steinberg <udo@hypervisor.org>
2021-12-20 21:34:33 +0100

    Arm: Added GICC (CPU Interface).

--- src/aarch64/cpu.cpp
+++ src/aarch64/cpu.cpp
@@ -19,6 +19,7 @@
 #include "cache.hpp"
 #include "cpu.hpp"
 #include "fpu.hpp"
+#include "gicc.hpp"
 #include "gicd.hpp"
 #include "gicr.hpp"
 #include "ptab_npt.hpp"
@@ -276,6 +277,7 @@ void Cpu::init (cpu_t cpu)
 
     Gicd::init();
     Gicr::init();
+    Gicc::init();
 
     Timer::init();
 
