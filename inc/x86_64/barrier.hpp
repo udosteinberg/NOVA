b0a6fb553345fa84fbf36c90acae51a033e4c2b9
Udo Steinberg <udo@hypervisor.org>
2022-01-21 15:35:52 +0100

    x86: Modernized barriers.

--- inc/x86_64/barrier.hpp
+++ inc/x86_64/barrier.hpp
@@ -4,6 +4,8 @@
  * Copyright (C) 2009-2011 Udo Steinberg <udo@hypervisor.org>
  * Economic rights: Technische Universitaet Dresden (Germany)
  *
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
+ *
  * This file is part of the NOVA microhypervisor.
  *
  * NOVA is free software: you can redistribute it and/or modify it
@@ -19,6 +21,42 @@
 #pragma once
 
 #include "compiler.hpp"
+
+class Barrier final
+{
+    public:
+        /*
+         * Read Memory Barrier
+         *
+         *      LD -->|         |<-- LD
+         *      ST ---|-->+     |<-- ST
+         * CLFLUSH -->|         |<-- CLFLUSH
+         *
+         * Additional serialization
+         * - all prior instructions must complete before LFENCE executes
+         * - all later instructions cannot execute until LFENCE completes (not even speculatively)
+         * + for prior store instructions, stored data may become globally visible after LFENCE
+         */
+        static void rmb() { asm volatile ("lfence" : : : "memory"); }
+
+        /*
+         * Write Memory Barrier
+         *
+         *      LD ---|-->   <--|--- LD
+         *      ST -->|         |<-- ST
+         * CLFLUSH -->|         |<-- CLFLUSH
+         */
+        static void wmb() { asm volatile ("sfence" : : : "memory"); }
+
+        /*
+         * Full Memory Barrier
+         *
+         *      LD -->|         |<-- LD
+         *      ST -->|         |<-- ST
+         * CLFLUSH -->|         |<-- CLFLUSH
+         */
+        static void fmb() { asm volatile ("mfence" : : : "memory"); }
+};
 
 ALWAYS_INLINE
 inline void barrier()
