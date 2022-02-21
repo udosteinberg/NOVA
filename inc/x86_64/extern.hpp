a8da352d8ab6b072d3f6891d79bc29eb7cf615d8
Udo Steinberg <udo@hypervisor.org>
2022-02-21 15:04:05 +0100

    x86: Added CET support.

--- inc/x86_64/extern.hpp
+++ inc/x86_64/extern.hpp
@@ -23,7 +23,7 @@
 
 #include "types.hpp"
 
-extern char GIT_VER, NOVA_HPAS, PTAB_HPAS, KMEM_HVAS, DSTK_TOP;
+extern char GIT_VER, NOVA_HPAS, PTAB_HPAS, KMEM_HVAS, DSTK_TOP, DSHD_TOP;
 extern void (*CTORS_S[])(), (*CTORS_E[])(), (*CTORS_C[])(), (*CTORS_L[])();
 
 extern char entry_sys;
