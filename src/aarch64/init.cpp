bed36f2d1be009726323f4faaeaa493831d44ea6
Udo Steinberg <udo@hypervisor.org>
2022-03-17 16:47:19 +0100

    ARM: Unified command line.

--- src/aarch64/init.cpp
+++ src/aarch64/init.cpp
@@ -17,9 +17,16 @@
 
 #include "acpi.hpp"
 #include "buddy.hpp"
+#include "cmdline.hpp"
 #include "console.hpp"
 #include "extern.hpp"
 #include "patch.hpp"
+
+extern "C" void preinit()
+{
+    if (!Acpi::resume)
+        Cmdline::init();
+}
 
 extern "C" void init()
 {
