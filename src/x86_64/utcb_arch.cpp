76fdb4ba896d48ce195f5e90baab3ee5def8c82c
Udo Steinberg <udo@hypervisor.org>
2022-05-19 15:28:28 +0200

    x86: Context-switch XSV state between host and guest.

--- src/x86_64/utcb_arch.cpp
+++ src/x86_64/utcb_arch.cpp
@@ -182,6 +182,11 @@ void Utcb_arch::load_vmx (Mtd_arch const m, Cpu_regs const &c)
     if (m & Mtd_arch::Item::DR)
         dr7 = Vmcs::read<uintptr_t> (Vmcs::Encoding::GUEST_DR7);
 
+    if (m & Mtd_arch::Item::XSAVE) {
+        xcr0 = c.gst_xsv.xcr;
+        xss  = c.gst_xsv.xss;
+    }
+
     if (m & Mtd_arch::Item::SYSCALL) {
         star  = c.gst_sys.star;
         lstar = c.gst_sys.lstar;
@@ -368,6 +373,11 @@ bool Utcb_arch::save_vmx (Mtd_arch const m, Cpu_regs &c, Space_obj const *obj) c
     if (m & Mtd_arch::Item::DR)
         Vmcs::write (Vmcs::Encoding::GUEST_DR7, dr7);
 
+    if (m & Mtd_arch::Item::XSAVE) {
+        c.gst_xsv.xcr = Fpu::State_xsv::constrain_xcr (xcr0);
+        c.gst_xsv.xss = Fpu::State_xsv::constrain_xss (xss);
+    }
+
     if (m & Mtd_arch::Item::SYSCALL) {
         c.gst_sys.star  = Cpu::State_sys::constrain_star  (star);
         c.gst_sys.lstar = Cpu::State_sys::constrain_canon (lstar);
@@ -520,6 +530,11 @@ void Utcb_arch::load_svm (Mtd_arch const m, Cpu_regs const &c)
     if (m & Mtd_arch::Item::DR)
         dr7 = v->dr7;
 
+    if (m & Mtd_arch::Item::XSAVE) {
+        xcr0 = c.gst_xsv.xcr;
+        xss  = c.gst_xsv.xss;
+    }
+
     if (m & Mtd_arch::Item::SYSCALL) {
         star  = v->star;
         lstar = v->lstar;
@@ -636,6 +651,11 @@ bool Utcb_arch::save_svm (Mtd_arch const m, Cpu_regs &c, Space_obj const *obj) c
 
     if (m & Mtd_arch::Item::DR)
         v->dr7 = dr7;
+
+    if (m & Mtd_arch::Item::XSAVE) {
+        c.gst_xsv.xcr = Fpu::State_xsv::constrain_xcr (xcr0);
+        c.gst_xsv.xss = Fpu::State_xsv::constrain_xss (xss);
+    }
 
     if (m & Mtd_arch::Item::SYSCALL) {
         v->star   = Cpu::State_sys::constrain_star  (star);
