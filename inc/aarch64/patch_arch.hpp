20238402280564ea1e7c80a7b30cd7d593fe57c5
Udo Steinberg <udo@hypervisor.org>
2022-05-27 14:27:10 +0200

    ARM: Added patch definitions.

---
+++ inc/aarch64/patch_arch.hpp
@@ -0,0 +1,21 @@
+/*
+ * Instruction Patching: Architecture-Specific Part (ARM)
+ *
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#define NOP_LEN         4
+#define NOP_OPC         0xd503201f
