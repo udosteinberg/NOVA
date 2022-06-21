118bbf532b085d2e512f4e965ae4bd8df1b79cf6
Udo Steinberg <udo@hypervisor.org>
2022-06-21 15:18:24 +0200

    ARM: Added PIO space.

---
+++ inc/aarch64/space_pio.hpp
@@ -0,0 +1,35 @@
+/*
+ * PIO Space
+ *
+ * Copyright (C) 2019-2023 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "space.hpp"
+
+class Space_pio final : public Space
+{
+    public:
+        [[nodiscard]] auto delegate (Space_pio const *, unsigned long, unsigned long, unsigned, unsigned) { return Status::BAD_FTR; };
+
+        [[nodiscard]] static Space_pio *create (Status &s, Slab_cache &, Pd *, bool)
+        {
+            s = Status::BAD_FTR;
+
+            return nullptr;
+        }
+
+        void destroy() {}
+};
