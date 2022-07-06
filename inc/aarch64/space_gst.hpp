52d0fc5e6c461000e79c3a419b52dcf128bf0fcd
Udo Steinberg <udo@hypervisor.org>
2022-07-06 13:29:52 +0200

    Arm: Added GST space.

---
+++ inc/aarch64/space_gst.hpp
@@ -0,0 +1,51 @@
+/*
+ * Guest Memory Space
+ *
+ * Copyright (C) 2019-2026 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "ptab_npt.hpp"
+#include "space_mem.hpp"
+
+class Space_gst final : public Space_mem<Space_gst>
+{
+    private:
+        Vmid const  vmid;
+        Nptp        nptp;
+
+        Space_gst (Refptr<Pd> &ref_pd) : Space_mem { Kobject::Subtype::GST, ref_pd } {}
+
+        void collect() override final
+        {
+            trace (TRACE_DESTROY, "KOBJ: GST %p collected", static_cast<void *>(this));
+        }
+
+    public:
+        static constexpr uint8_t  sbw       { Npt::ibits - PAGE_BITS };
+        static constexpr uint64_t selectors { BIT64 (sbw) };
+
+        static auto mco() { return static_cast<uint8_t>(Npt::lev_ord()); }
+
+        [[nodiscard]] static Space_gst *create (Status &, Pd *);
+
+        void destroy() override final;
+
+        auto update (uint64_t v, uint64_t p, unsigned o, Paging::Permissions pm, Memattr ma) { return nptp.update (v, p, o, pm, ma); }
+
+        void sync() { nptp.invalidate (vmid); }
+
+        void make_current() { nptp.make_current (vmid); }
+};
---
+++ src/aarch64/space_gst.cpp
@@ -0,0 +1,59 @@
+/*
+ * Guest Memory Space
+ *
+ * Copyright (C) 2019-2026 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "space_gst.hpp"
+
+Space_gst *Space_gst::create (Status &s, Pd *pd)
+{
+    // Acquire reference
+    Refptr<Pd> ref_pd { pd };
+
+    // Failed to acquire reference
+    if (!ref_pd) [[unlikely]]
+        s = Status::ABORTED;
+
+    else {
+
+        // Create new GST object
+        auto const obj { new (ref_pd->gst_cache) Space_gst { ref_pd } };
+
+        // If creation succeeded, then reference must have been consumed
+        if (obj) [[likely]] {
+
+            assert (!ref_pd);
+
+            if (obj->nptp.root_init()) [[likely]]
+                return obj;
+
+            operator delete (obj, ref_pd->gst_cache);
+        }
+
+        // Failed to create GST object
+        s = Status::MEM_OBJ;
+    }
+
+    return nullptr;
+}
+
+void Space_gst::destroy()
+{
+    auto &cache { get_pd()->gst_cache };
+
+    this->~Space_gst();
+
+    operator delete (this, cache);
+}
