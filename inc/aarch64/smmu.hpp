26558dbbf2780b2213774a1be430b6fa25067525
Udo Steinberg <udo@hypervisor.org>
2022-07-25 19:21:48 +0200

    Arm: Unified PD,EC,SC,PT,SM,HIP,MBUF class.

--- inc/aarch64/smmu.hpp
+++ inc/aarch64/smmu.hpp
@@ -60,6 +60,9 @@ class Smmu : public List<Smmu>, protected Mmio
                 if (!smmu->init()) [[unlikely]]
                     return false;
 
+            if (list) [[likely]]
+                Hip::set_feature (Hip_arch::Feature::SMMU);
+
             return true;
         }
 
