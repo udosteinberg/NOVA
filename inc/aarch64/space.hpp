c484f88ca5dce88538b889dfa8ce0f634eb8ac6f
Udo Steinberg <udo@hypervisor.org>
2022-07-25 17:32:19 +0200

    ARM: Added Space class.

---
+++ inc/aarch64/space.hpp
@@ -0,0 +1,37 @@
+/*
+ * Space
+ *
+ * Copyright (C) 2019-2022 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "atomic.hpp"
+#include "kobject.hpp"
+#include "status.hpp"
+
+class Pd;
+class Space_hst;
+
+class Space : public Kobject
+{
+    private:
+        Pd *const pd;
+
+    protected:
+        inline Space (Kobject::Subtype s, Pd *p) : Kobject (Kobject::Type::PD, s), pd (p) {}
+
+    public:
+        inline auto get_pd() const { return pd; }
+};
