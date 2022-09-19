88180a452007d11b0d4818bcbfd117d4e978ef8c
Udo Steinberg <udo@hypervisor.org>
2022-09-19 12:54:02 +0200

    x86: Added UART console (HSU).

---
+++ src/x86_64/console_uart_pio_hsu.cpp
@@ -0,0 +1,41 @@
+/*
+ * Console: High-Speed UART (HSU)
+ *
+ * Copyright (C) 2019-2022 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#include "console_uart_pio_hsu.hpp"
+#include "pci.hpp"
+
+INIT_PRIORITY (PRIO_CONSOLE) Console_uart_pio_hsu Console_uart_pio_hsu::singleton { 1'843'200 };
+
+uint16 Console_uart_pio_hsu::probe()
+{
+    constexpr auto bdf { Pci::bdf (0, 26, 0) };
+
+    // Probe for Intel 16550-Compatible Serial Controller
+    if (Pci::cfg_read (bdf, Pci::Register16::VID) != 0x8086 || Pci::cfg_read (bdf, Pci::Register32::CCP_RID) >> 8 != 0x70002)
+        return 0;
+
+    auto const bar { Pci::cfg_read (bdf, Pci::Register32::BAR0) };
+
+    // Probe for I/O resources
+    if (bar == BIT (0))
+        return 0;
+
+    // Enable I/O space
+    Pci::cfg_rd_wr (bdf, Pci::Register16::CMD, BIT (0));
+
+    return bar & BIT_RANGE (15, 3);
+}
