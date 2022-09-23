981cfa62f0767488b53c8dad2c95b027d87b43b5
Udo Steinberg <udo@hypervisor.org>
2022-09-23 10:56:27 +0200

    x86: Added UART console (SOL).

---
+++ inc/x86_64/console_uart_pio_sol.hpp
@@ -0,0 +1,32 @@
+/*
+ * Console: Serial-Over-LAN (SOL)
+ *
+ * Copyright (C) 2019-2022 Udo Steinberg, BedRock Systems, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "console_uart_pio_ns16550.hpp"
+
+class Console_uart_pio_sol final : private Console_uart_pio_ns16550
+{
+    private:
+        static uint16 probe();
+
+    protected:
+        explicit Console_uart_pio_sol() : Console_uart_pio_ns16550 (probe()) {}
+
+    public:
+        static Console_uart_pio_sol singleton;
+};
