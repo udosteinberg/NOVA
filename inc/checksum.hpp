9cb0cadd4b8a85f0b140a99e0598097ac08f2cdd
Udo Steinberg <udo@hypervisor.org>
2022-12-03 13:55:17 +0100

    GEN: Added checksum functions.

---
+++ inc/checksum.hpp
@@ -0,0 +1,43 @@
+/*
+ * Checksum Functions
+ *
+ * Copyright (C) 2019-2026 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "std.hpp"
+
+class Checksum
+{
+    public:
+        /*
+         * Compute additive checksum
+         *
+         * @param p     Pointer to region of n T-sized values
+         * @param n     Number of T values to checksum
+         * @return      Sum of T values, trimmed to T
+         */
+        template<typename T> static auto additive (void const *p, unsigned n)
+        {
+            auto ptr { std::start_lifetime_as_array<T> (p, n) };
+
+            T val { 0 };
+
+            while (n--)
+                val += *ptr++;
+
+            return val;
+        }
+};
