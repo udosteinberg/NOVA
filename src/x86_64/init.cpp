cdd823839e2904d6b221edb8dced2a9fee08ffbb
Udo Steinberg <udo@hypervisor.org>
2023-02-22 13:36:30 +0100

    x86: Added TXT support.

--- src/x86_64/init.cpp
+++ src/x86_64/init.cpp
@@ -28,6 +28,7 @@
 #include "patch.hpp"
 #include "pic.hpp"
 #include "string.hpp"
+#include "txt.hpp"
 
 extern "C" uintptr_t kern_ptab_setup (apic_t t)
 {
@@ -67,7 +68,12 @@ extern "C" uintptr_t kern_ptab_setup (apic_t t)
 
 extern "C" void preinit()
 {
+    if (!Acpi::resume && !Txt::launched)
+        Cmdline::init();
+
     Patch::detect();
+
+    Txt::launch();
 }
 
 extern "C" void init()
@@ -79,8 +85,6 @@ extern "C" void init()
 
         for (auto func { CTORS_S }; func != CTORS_E; (*func++)()) ;
 
-        Cmdline::init();
-
         for (auto func { CTORS_C }; func != CTORS_S; (*func++)()) ;
 
         // Now we're ready to talk to the world
@@ -88,6 +92,8 @@ extern "C" void init()
 
         Interrupt::setup();
     }
+
+    Txt::init();
 
     Acpi::init();
 
