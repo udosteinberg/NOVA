b3aee45138e111de12d6b5f8bae29c4b98c37392
Udo Steinberg <udo@hypervisor.org>
2023-11-06 19:36:59 +0100

    GEN: Added endian functions.

---
+++ inc/endian.hpp
@@ -0,0 +1,64 @@
+/*
+ * Endianness
+ *
+ * Copyright (C) 2019-2026 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "types.hpp"
+
+/*
+ * Aligned Big/Little-Endian Integral Type T
+ */
+template<typename T, bool B> class Aligned
+{
+    private:
+        T val { 0 };
+
+        static constexpr auto bswap (uint128_t v) { return B ? __builtin_bswap128 (v) : v; }
+        static constexpr auto bswap (uint64_t  v) { return B ? __builtin_bswap64  (v) : v; }
+        static constexpr auto bswap (uint32_t  v) { return B ? __builtin_bswap32  (v) : v; }
+        static constexpr auto bswap (uint16_t  v) { return B ? __builtin_bswap16  (v) : v; }
+        static constexpr auto bswap (uint8_t   v) { return v; }
+
+    public:
+        constexpr Aligned() = default;
+
+        constexpr Aligned (T v) : val { bswap (v) } {}
+
+        constexpr operator T() const { return bswap (val); }
+};
+
+template<typename T> using Aligned_be = Aligned<T, __BYTE_ORDER__ != __ORDER_BIG_ENDIAN__>;
+template<typename T> using Aligned_le = Aligned<T, __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__>;
+
+/*
+ * Unaligned Big/Little-Endian Integral Type T
+ */
+template<typename T, bool B> class Unaligned
+{
+    private:
+        char val[sizeof (T)];
+
+    public:
+        constexpr Unaligned() = default;
+
+        constexpr Unaligned (T v) : Unaligned { __builtin_bit_cast (Unaligned, Aligned<T, B> { v }) } {}
+
+        constexpr operator T() const { return __builtin_bit_cast (Aligned<T, B>, *this); }
+};
+
+template<typename T> using Unaligned_be = Unaligned<T, __BYTE_ORDER__ != __ORDER_BIG_ENDIAN__>;
+template<typename T> using Unaligned_le = Unaligned<T, __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__>;
