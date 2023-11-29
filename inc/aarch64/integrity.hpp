8e62f6f7c6671c31cbcaab09bf5a22fc9cb80e2f
Udo Steinberg <udo@hypervisor.org>
2023-11-29 17:40:11 +0100

    Arm: Unified Integrity class.

---
+++ inc/aarch64/integrity.hpp
@@ -0,0 +1,29 @@
+/*
+ * Integrity Measurement
+ *
+ * Copyright (C) 2019-2026 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "hash.hpp"
+
+class Integrity final
+{
+    private:
+        [[nodiscard]] static bool extend (Hash_sha1_160 const &, Hash_sha2_256 const &, Hash_sha2_384 const &, Hash_sha2_512 const &) { return false; }
+
+    public:
+        [[nodiscard]] static bool measure (uint64_t, size_t);
+};
