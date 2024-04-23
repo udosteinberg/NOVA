c1412d51aa2353b208995b88ca3be9274cd368a7
Udo Steinberg <udo@hypervisor.org>
2024-04-23 14:41:28 +0200

    x86: Added NMI class.

---
+++ inc/x86_64/nmi.hpp
@@ -0,0 +1,24 @@
+/*
+ * Non-Maskable Interrupt Handling
+ *
+ * Copyright (C) 2019-2024 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+class Nmi final
+{
+    public:
+        static void handler() {}
+};
