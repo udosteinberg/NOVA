986fbf0d58048e2fb14bd60be9bf6cd385a2903e
Udo Steinberg <udo@hypervisor.org>
2024-12-02 07:30:53 +0100

    x86: Added Intid class.

---
+++ inc/x86_64/intid.hpp
@@ -0,0 +1,27 @@
+/*
+ * Interrupt Identifier
+ *
+ * Copyright (C) 2019-2024 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "types.hpp"
+
+class Intid
+{
+    public:
+        static constexpr auto to_seg (iid_t i) { return static_cast<uint16_t>(i >> 16); }
+        static constexpr auto to_gsi (iid_t i) { return static_cast<uint16_t>(i); }
+};
