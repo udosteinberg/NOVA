c33fa2d51d96c53c47de03baa0542476b14ff035
Udo Steinberg <udo@hypervisor.org>
2025-04-02 12:52:51 +0200

    Arm: Added Coherency class.

---
+++ inc/aarch64/coherence.hpp
@@ -0,0 +1,72 @@
+/*
+ * Coherence Management
+ *
+ * Copyright (C) 2019-2026 Udo Steinberg, BlueRock Security, Inc.
+ *
+ * This file is part of the NOVA microhypervisor.
+ *
+ * NOVA is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NOVA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "barrier.hpp"
+#include "cache.hpp"
+
+struct Coherence final
+{
+    /*
+     * Consumer-Side Coherence Management (Cacheline)
+     */
+    static void consumer (bool noncoherent, void const *ptr)
+    {
+        if (noncoherent) [[unlikely]]
+            return Cache::data_inval (ptr);
+
+        // DSB ISHLD (DDI0487 K11.5.4)
+        Barrier::rsb (Barrier::Domain::ISH);
+    }
+
+    /*
+     * Consumer-Side Coherence Management (Region)
+     */
+    static void consumer (bool noncoherent, void const *ptr, size_t size)
+    {
+        if (noncoherent) [[unlikely]]
+            return Cache::data_inval (ptr, size);
+
+        // DSB ISHLD (DDI0487 K11.5.4)
+        Barrier::rsb (Barrier::Domain::ISH);
+    }
+
+    /*
+     * Producer-Side Coherence Management (Cacheline)
+     */
+    static void producer (bool noncoherent, void const *ptr)
+    {
+        if (noncoherent) [[unlikely]]
+            return Cache::data_clean (ptr);
+
+        // DSB ISHST (DDI0487 K11.5.4)
+        Barrier::wsb (Barrier::Domain::ISH);
+    }
+
+    /*
+     * Producer-Side Coherence Management (Region)
+     */
+    static void producer (bool noncoherent, void const *ptr, size_t size)
+    {
+        if (noncoherent) [[unlikely]]
+            return Cache::data_clean (ptr, size);
+
+        // DSB ISHST (DDI0487 K11.5.4)
+        Barrier::wsb (Barrier::Domain::ISH);
+    }
+};
