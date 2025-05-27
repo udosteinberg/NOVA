0b52463ecdbd2e99cee0d2d54b0c5f3e11eed39e
Udo Steinberg <udo@hypervisor.org>
2025-05-27 11:58:53 +0200

    Arm: Added type definitions.

--- inc/types.hpp
+++ inc/types.hpp
@@ -37,6 +37,12 @@ using uintptr_t     = __UINTPTR_TYPE__;
 // Size Types
 using size_t        = __SIZE_TYPE__;
 
+// Arm Types
+using arm_colid_t   = uint16_t;             // Collection ID
+using arm_devid_t   = uint32_t;             // Device ID
+using arm_evtid_t   = uint32_t;             // Event ID
+using arm_intid_t   = uint32_t;             // Interrupt ID
+
 // NOVA Types
 using apic_t        = uint32_t;
 using iid_t         = uint32_t;
